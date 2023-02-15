79c2755681a3134361df70feb94e87affdccb2d8
Fidelis <fideliofidel9@gmail.com>
2023-02-15 14:19:03 +0300

    link to dashboard

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -9,12 +9,17 @@ const SignUp = ({setUser}) => {
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('')
+  const [formData, setFormData] = useState({
+    password: '',
+    confirmPswd: ''
+  })
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if(!name || !email) return;
     setUser({name: name, email: email})
     navigate('/')
+
   }
 
   return (
