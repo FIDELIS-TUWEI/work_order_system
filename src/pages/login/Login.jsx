8ca0794461dbc742b09cbe2d7bb1519428167650
Fidelis <fideliofidel9@gmail.com>
2023-02-15 09:42:54 +0300

    login form created

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,7 +3,18 @@ import './login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" placeholder='Enter your email' required />
+
+        <label htmlFor='password'>password</label>
+        <input type="password" name="password" placeholder='Enter your password' required />
+
+        <button type='submit'>Login</button>
+      </form>
+    </div>
   )
 }
 
