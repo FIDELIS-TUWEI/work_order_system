26d99d4962d7fffd82c251a355f0a0e2003fa03e
Fidelis <fideliofidel9@gmail.com>
2023-02-15 09:49:33 +0300

    signup form created

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -3,7 +3,24 @@ import './signup.css'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div>
+      <h2>Sign Up</h2>
+      <form>
+        <label htmlFor="create account">Create new account</label>
+        
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name"placeholder='Enter your name' required />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" name='email' placeholder='Enter your email' required />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" placeholder='Enter new password' required />
+
+        <label htmlFor="confirmPswd">Confirm Password</label>
+        <input type="password" name="confirmPswd" placeholder='Confirm Password' required />
+      </form>
+    </div>
   )
 }
 
