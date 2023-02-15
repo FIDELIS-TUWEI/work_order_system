558e6748ac36400dbbe4a86c6bbf8b688af2b5c5
Fidelis <fideliofidel9@gmail.com>
2023-02-15 09:35:54 +0300

    componets/pages setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp'
 
 function App() {
   return (
     <div className="App">
-     
+     <Login />
+     <SignUp />
     </div>
   );
 }
---
+++ src/pages/signup/SignUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './signup.css'
+
+const SignUp = () => {
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default SignUp
