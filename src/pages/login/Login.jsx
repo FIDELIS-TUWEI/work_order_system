b89572a10530a2967248f55f30c4a7e6f8b715e8
Fidelis <114683429+FIDELIS-TUWEI@users.noreply.github.com>
2023-02-23 09:07:31 +0300

    imported auth

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { auth } from '../Firebase
 import { signInWithEmailAndPassword } from 'firebase/auth'
 
 
