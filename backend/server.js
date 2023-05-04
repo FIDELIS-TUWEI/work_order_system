41b87ab217f8cd23e369145c2b8ba6f2c3df80a5
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-05 01:33:19 +0300

    mongodb db connected

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+// init express
+const app = express();
+
+// port for local
+const PORT = process.env.PORT || 5000;
+
+// connect to mongodb DB and start Server
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        
+        app.listen(PORT, () => {
+            console.log(`Server Running on port ${PORT}`)
+        })
+    })
+    .catch((error) => console.log(error))
