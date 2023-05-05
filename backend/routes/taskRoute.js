1abeec7dc5f6ef5dcc5b7f25d2ede08b577b4cd9
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-06 00:31:25 +0300

    separation of concerns

--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -3,7 +3,7 @@ const Task = require("../models/taskModel");
 const router = express.Router();
 
 // Create a Task
-router.get("/api/tasks", async(req, res) => {
+router.post("/api/tasks", async(req, res) => {
     try {
         const task = await Task.create(req.body);
         res.status(200).json(task);
@@ -13,7 +13,7 @@ router.get("/api/tasks", async(req, res) => {
 });
 
 // Get/ Read Tasks
-router.post("/api/tasks", async(req, res) => {
+router.get("/api/tasks", async(req, res) => {
     try {
         const tasks = await Task.find();
         res.status(200).json(tasks);
