aca0b607ef4ad27af49220c173edce42eb2331f0
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-05 13:48:45 +0300

    timestamp added

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -2,18 +2,22 @@ const mongoose = require("mongoose")
 
 // database schema & model
 const taskSchema = mongoose.Schema({
-    name : {
+    name: {
         type: String,
-        required: [true, "Please add a Task"]
+        required: [true, "Please add a Task"],
+    },
+    employee: {
+        type: String,
+        required: [true, "Please enter name"],
     },
     completed: {
         type: Boolean,
         required: true,
-        default: false
+        default: false,
     }
 }, {
     timestamps: true
-})
+});
 
 const Task = mongoose.model("Task", taskSchema);
 module.exports = Task;
