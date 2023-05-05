498973c0803b9dd0e1ee55ac150411f32f2cfdc8
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-05 03:57:56 +0300

    userController imported

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
-const registerUser = () => {
-
-}
+const { registerUser } = require("../controllers/userController");
 
 // user route
 router.post("/register", registerUser);
