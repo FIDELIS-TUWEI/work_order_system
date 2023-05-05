37cb1064124f6cab495a4cba02080a51076c1cbd
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-06 01:03:37 +0300

    separation of concerns controllers

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,6 +1,7 @@
 const Task = require("../models/taskModel");
 
-const CreateTask = async (req, res) => {
+// createTask Controller
+const createTask = async (req, res) => {
     try {
         const task = await Task.create(req.body);
         res.status(200).json(task)
@@ -9,7 +10,18 @@ const CreateTask = async (req, res) => {
     }
 };
 
+// GetTasks Controller
+const getTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.status(200).json(tasks);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+}
+
 
 module.exports = {
-    CreateTask,
+    createTask,
+    getTasks,
 }
