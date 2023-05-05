50b08492f9a8865dbf1958e332a22ca49003a364
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-06 02:17:18 +0300

    deleteTask controller imported to routes

--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const Task = require("../models/taskModel");
 const router = express.Router();
-const { createTask, getTasks, getTask } = require("../controllers/taskController");
+const { createTask, getTasks, getTask, deleteTask } = require("../controllers/taskController");
 
 // routes
 router.post("/api/tasks", createTask);
 router.get("/api/tasks", getTasks);
 router.get("/api/tasks/:id", getTask);
+router.delete("/api/tasks/:id", deleteTask);
 
 module.exports = router;
