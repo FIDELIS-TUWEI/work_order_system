9ae9dcece31e833708778c589caf8dbc6b0c8137
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-06 02:12:31 +0300

    getTask with Id

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -22,7 +22,17 @@ const getTasks = async (req, res) => {
 
 // Get a singleTask controller
 const getTask = async (req, res) => {
-
+    try {
+        const {id} = req.params
+        const task = await Task.findById(id);
+        if (!task) {
+            return res.status(404).json(`No task with id: ${id} found`);
+        };
+        
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
 
 module.exports = {
