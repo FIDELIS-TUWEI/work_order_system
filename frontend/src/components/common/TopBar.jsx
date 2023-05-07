4630148fbdbe6dd5b24f3cf72132ea55d6101c6b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-07 10:36:22 +0300

    TopBar complete

--- frontend/src/components/common/TopBar.jsx
+++ frontend/src/components/common/TopBar.jsx
@@ -1,6 +1,17 @@
+import { AppBar, Box, Toolbar, Typography, Button}from '@mui/material';
+
 const TopBar = () => {
     return ( 
-        <>TopBar</>
+        <Box sx={{ flexGrow: 1}}>
+            <AppBar position='static'>
+                <Toolbar>
+                    <Typography variant='h6' component="div" sx={{flexGrow: 1}}>
+                        Work Order System
+                    </Typography>
+                    <Button color='inherit'>Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
      );
 }
  
