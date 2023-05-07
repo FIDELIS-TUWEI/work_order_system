15f42d3155daf8141b1947871495d171e09e2ec7
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-07 12:54:21 +0300

    function to handle register route

--- frontend/src/layouts/HomeLayout.jsx
+++ frontend/src/layouts/HomeLayout.jsx
@@ -1,11 +1,32 @@
 import TopBar from "../components/common/TopBar";
+import CssBaseline from '@mui/material/CssBaseline';
+import { Container, Box, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const HomeLayout = () => {
+    // navigate 
+    const navigate = useNavigate();
+
+    // function to navigate to register page
+    const handleRegister = () => {
+        navigate('/register');
+    }
+
     return ( 
         <>
             <TopBar />
+
+            <CssBaseline />
+            <Container fixed>
+                <Box sx={{ height: '50vh' }}>
+                    <Typography>
+                        Ultimate Work Order system for Holiday Inn Maintenace Department
+                    </Typography>
+                </Box>
+                
+                <Button variant="contained" color="error" onClick={handleRegister}>Register</Button>
+            </Container>
         </>
-     );
+    );
 }
- 
 export default HomeLayout;
