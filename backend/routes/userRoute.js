373961269d3df8c2e2a64326f7563326c9a1bf75
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-08 22:21:05 +0300

    getUserData route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, logOut } = require("../controllers/userController");
+const { registerUser, loginUser, logOut, getUser } = require("../controllers/userController");
 
 // user route
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logOut);
+router.get("/getuser", getUser);
 
 module.exports = router;
