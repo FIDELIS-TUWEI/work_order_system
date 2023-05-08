1e6d175bbaab0da148a87754d7d30772108a264e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-08 21:58:47 +0300

    logout route created

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/userController");
+const { registerUser, loginUser, logOut } = require("../controllers/userController");
 
 // user route
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/logout", logOut);
 
 module.exports = router;
