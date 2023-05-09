0c265175425b70c55499a5555c3e8e19ce8b2ac3
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-09 10:14:51 +0300

    getUser data route modified

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -138,7 +138,22 @@ const logOut = asyncHandler( async(req, res) => {
 
 // get user Data
 const getUser = asyncHandler( async(req, res) => {
-    res.send("get user data");
+    const user = await User.findById(req.user._id);
+
+    if(user) {
+        const { _id, name, email, photo, phone, bio } = user;
+        res.status(200).json({
+            _id,
+            name,
+            email,
+            photo,
+            phone,
+            bio,
+        });
+    } else {
+        res.status(400);
+        throw new Error("User not Found!")
+    }
 });
 
 module.exports = {
