6286cd6082e64a2d4863985f8bfcbfc8902a748a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-09 12:21:50 +0300

    update user route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, logOut, getUser, loginStatus } = require("../controllers/userController");
+const { registerUser, loginUser, logOut, getUser, loginStatus, updateUser } = require("../controllers/userController");
 const protect = require("../middleWare/authMiddleware");
 
 // user route
@@ -8,6 +8,7 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logOut);
 router.get("/getuser", protect, getUser);
-router.get("/loggedin", loginStatus)
+router.get("/loggedin", loginStatus);
+router.patch("/updateuser", updateUser);
 
 module.exports = router;
