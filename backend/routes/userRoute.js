f022c65edf0ad5c1028177a4b9be95c23e800189
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-09 19:52:58 +0300

    changePassword route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, logOut, getUser, loginStatus, updateUser } = require("../controllers/userController");
+const { registerUser, loginUser, logOut, getUser, loginStatus, updateUser, changePassword } = require("../controllers/userController");
 const protect = require("../middleWare/authMiddleware");
 
 // user route
@@ -10,5 +10,6 @@ router.get("/logout", logOut);
 router.get("/getuser", protect, getUser);
 router.get("/loggedin", loginStatus);
 router.patch("/updateuser", protect, updateUser);
+router.patch("/changepassword", protect, changePassword);
 
 module.exports = router;
