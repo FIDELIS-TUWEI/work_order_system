bbd7756bc70a3b9128b9f59e509a584dc9b63345
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-09 19:49:51 +0300

    update user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -177,13 +177,28 @@ const updateUser = asyncHandler( async(req, res) => {
     const user = await User.findById(req.user._id);
 
     if (user) {
-        const { _id, name, email, photo, phone, bio } = user;
+        const { name, email, photo, phone, bio } = user;
         user.email = email;
         user.name = req.body.name || name;
         user.photo = req.body.photo || photo;
         user.phone = req.body.phone || phone;
         user.bio = req.body.bio || bio;
+
+        const updatedUser = await user.save();
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            photo: updatedUser.photo,
+            phone: updatedUser.phone,
+            bio: updatedUser.bio,
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found");
     }
+
+    
 });
 
 module.exports = {
