c6e2a92dfbbbc031a4b19db7fe38575cbfb26d31
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-09 10:01:02 +0300

    route protected

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser, logOut, getUser } = require("../controllers/userController");
+const protect = require("../middleWare/authMiddleware");
 
 // user route
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logOut);
-router.get("/getuser", getUser);
+router.get("/getuser", protect, getUser);
 
 module.exports = router;
