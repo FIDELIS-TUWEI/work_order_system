241608f7c7d9b598a4c3c1851804c6e2c1fc98d7
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-10 16:44:30 +0300

    Forgot password controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -229,6 +229,11 @@ const changePassword = asyncHandler( async(req, res) => {
     }
 });
 
+// Forgot password controller
+const forgotPassword = asyncHandler( async(req, res) => {
+    res.send("Forgot password");
+});
+
 module.exports = {
     registerUser,
     loginUser,
@@ -237,4 +242,5 @@ module.exports = {
     loginStatus,
     updateUser,
     changePassword,
+    forgotPassword,
 };
