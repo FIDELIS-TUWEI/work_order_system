c5ad7dd1cfc0eadf13fb9d78121350d4319aeec9
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-11 21:48:52 +0300

    Profile Logo added

--- frontend/src/components/ProfileComponent.jsx
+++ frontend/src/components/ProfileComponent.jsx
@@ -1,6 +1,21 @@
+import { Card, CardMedia, Container, Typography } from "@mui/material";
+import HolidayLogo from "../assets/HolidayLogo.png";
+
 const ProfileComponent = () => {
     return ( 
-        <>ProfileComponent</>
+        <>
+
+            <Container>
+                <Typography>Profile</Typography>
+                <Card sx={{ maxWidth: 400 }}>
+                    <CardMedia
+                    sx={{ height: 350 }}
+                        image={HolidayLogo}
+                        title="Holiday Inn Logo"
+                    />
+                </Card>
+            </Container>
+        </>
      );
 }
  
