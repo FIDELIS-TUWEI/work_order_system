e84ca88c6610b4285289c573fc2a5e38e5a29c6f
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-12 10:54:26 +0300

    profile modifications

--- frontend/src/components/ProfileComponent.jsx
+++ frontend/src/components/ProfileComponent.jsx
@@ -44,32 +44,36 @@ const ProfileComponent = () => {
                             Role: admin
                         </Typography>
                         
-                        <Box>
+                        <Box sx={{ m: '1rem'}}>
                             <TextField 
                                 type="file"
                                 accept="image/*"
                                 name="image"
                                 onChange={handleImageChange}
                             />
-
+                        </Box>
+                        <Box sx={{ m: '1rem'}}>
                             <TextField 
                                 label="Name"
                                 onChange={handleInputChange}
                                 value={name}
                             />
-
+                        </Box>
+                        <Box sx={{ m: '1rem'}}>
                             <TextField 
                                 label="Email"
                                 onChange={handleInputChange}
                                 value={email}
                             />
-
+                        </Box>
+                        <Box sx={{ m: '1rem'}}>
                             <TextField 
                                 label="Phone"
                                 onChange={handleInputChange}
                                 value={phone}
                             />
-
+                        </Box>
+                        <Box sx={{ m: '1rem'}}>
                             <TextField 
                                 label="Bio"
                                 onChange={handleInputChange}
