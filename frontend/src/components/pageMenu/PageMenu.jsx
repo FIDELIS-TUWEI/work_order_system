398be4871e72dd182f1bfbf5c0c566d036e83f6e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-12 10:46:07 +0300

    mui page menu

--- frontend/src/components/pageMenu/PageMenu.jsx
+++ frontend/src/components/pageMenu/PageMenu.jsx
@@ -1,6 +1,27 @@
+import { Box, Button, Container } from '@mui/material'
+import { useNavigate } from 'react-router-dom';
+
 const PageMenu = () => {
+    // function to navigate
+    const navigate = useNavigate();
+
     return ( 
-        <>PageMenu</>
+        <Container maxWidth='md'>
+            <Box 
+                sx={{ 
+                    m: '2rem',
+                    bgcolor: 'success.main',
+                    display: 'flex', 
+                    justifyContent: 'center', 
+                    alignItems: 'center', 
+                    textAlign: 'center' 
+                    }}
+            >
+                <Button color='inherit' onClick={() => navigate('/profile')}>Profile</Button>
+                <Button color='inherit' onClick={() => navigate('/changepassword')}>Change password</Button>
+                <Button color='inherit' onClick={() => navigate('/users')}>Users</Button>
+            </Box>
+        </Container>
      );
 }
  
