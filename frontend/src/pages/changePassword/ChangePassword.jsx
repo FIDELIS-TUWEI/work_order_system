a104711ac5ff4726d74df4d9c6fb1822b0a84caa
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-12 11:01:44 +0300

    ChangePassword page

---
+++ frontend/src/pages/changePassword/ChangePassword.jsx
@@ -0,0 +1,15 @@
+import TopBar from "../../components/common/TopBar";
+import PageMenu from "../../components/pageMenu/PageMenu";
+
+const changepassword = () => {
+    return ( 
+        <>
+            <TopBar />
+            <PageMenu />
+
+            ChangePassword
+        </>
+     );
+}
+ 
+export default changepassword;
