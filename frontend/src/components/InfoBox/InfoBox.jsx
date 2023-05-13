58c639238d54eb599db00b8a25ae3615b7cf465a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-13 07:52:25 +0300

    userstats card setup

--- frontend/src/components/InfoBox/InfoBox.jsx
+++ frontend/src/components/InfoBox/InfoBox.jsx
@@ -1,10 +1,17 @@
-import { Container } from "@mui/material";
+import { Card, Container, Box, Typography } from "@mui/material";
 
-const InfoBox = () => {
+
+const InfoBox = ({ icon, title, count}) => {
     return ( 
         <>
             <Container>
-                
+                <Card sx={{ m: '1rem', p: '1rem' }}>
+                    {icon}
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                        <Typography variant="p">{title}</Typography>      
+                        <Typography variant="h4">{count}</Typography>
+                    </Box>
+                </Card>
             </Container>
         </>
      );
--- frontend/src/components/userStats/UserStats.jsx
+++ frontend/src/components/userStats/UserStats.jsx
@@ -1,11 +1,49 @@
-import { Typography } from "@mui/material";
+import { Box, Container, CssBaseline, Typography } from "@mui/material";
 import InfoBox from "../InfoBox/InfoBox";
+import GroupIcon from '@mui/icons-material/Group';
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
+import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
+import PersonOffIcon from '@mui/icons-material/PersonOff';
+
+
+// Icons 
+const groupIcon = <GroupIcon />
+const verifiedUser = <VerifiedUserIcon />
+const unverifiedUser = <PersonRemoveIcon />
+const suspendedUser = <PersonOffIcon />
 
 const UserStats = () => {
     return ( 
         <>
-            <Typography variant="h5">UserStats</Typography>
-            <InfoBox />
+            <Container>
+            <CssBaseline />
+            <Box>
+                <Typography variant="h5">UserStats</Typography>
+                <InfoBox 
+                    icon={groupIcon}
+                    title={"Total Users"}
+                    count={"3"}
+                />
+
+                <InfoBox 
+                    icon={verifiedUser}
+                    title={"Verified Users"}
+                    count={"2"}
+                />
+
+                <InfoBox 
+                    icon={unverifiedUser}
+                    title={"Unverified User"}
+                    count={"1"}
+                />
+
+                <InfoBox 
+                    icon={suspendedUser}
+                    title={"Suspended User"}
+                    count={"0"}
+                />
+            </Box>
+            </Container>
         </>
      );
 }
