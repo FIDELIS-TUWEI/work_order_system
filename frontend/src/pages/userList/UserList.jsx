a8b1dfbda1ec61edabe4b75210fd98ea4a210b91
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-13 10:04:23 +0300

    datagrid

--- frontend/src/pages/userList/UserList.jsx
+++ frontend/src/pages/userList/UserList.jsx
@@ -2,6 +2,7 @@ import { Box, Container, Typography } from "@mui/material";
 import PageMenu from "../../components/pageMenu/PageMenu";
 import UserStats from "../../components/userStats/UserStats";
 import SearchBar from "../../components/search/SearchBar";
+import UserTable from "../../components/table/UserTable";
 
 const UserList = () => {
     return ( 
@@ -9,10 +10,14 @@ const UserList = () => {
             <PageMenu />
             <UserStats />
 
-            <Box component="div">
+            <Box component="div" sx={{ display: 'flex', flexDirection: 'row' }}>
                 <Typography variant="h5">All Users</Typography>
+                <SearchBar />
+
+                <Box>
+                    <UserTable />
+                </Box>
             </Box>
-            <SearchBar />
         </Container>
      );
 }
