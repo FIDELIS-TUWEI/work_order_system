6b4dacf27ab3cc7cbb350fa6e14ff259cab3151f
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-14 07:37:35 +0300

    userTable modified

--- frontend/src/components/table/UserTable.jsx
+++ frontend/src/components/table/UserTable.jsx
@@ -5,32 +5,40 @@ const columns = [
   { field: 'firstName', headerName: 'First name', width: 130 },
   { field: 'lastName', headerName: 'Last name', width: 130 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
+    field: 'email',
+    headerName: 'Email',
+    type: 'text',
+    width: 130,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    field: 'role',
+    headerName: 'Role',
+    type: 'text',
+    width: 130,
   },
+  {
+    field: 'change role',
+    headerName: 'Change Role',
+    type: 'text',
+    width: 130,
+  },
+  {
+    field: 'action',
+    headerName: 'Action',
+    width: 130,
+  }
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, lastName: 'Snow', firstName: 'Jon' },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei' },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime' },
+  { id: 4, lastName: 'Stark', firstName: 'Arya' },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys'},
+  { id: 6, lastName: 'Melisandre', firstName: null },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara'},
+  { id: 8, lastName: 'Frances', firstName: 'Rossini' },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey' },
 ];
 
 export default function UserTable() {
