1b63512a5441eff6532b2d1fe4253fa714b37fa5
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-15 09:34:21 +0300

    create Task

--- frontend/src/components/taskComponent/TaskForm.jsx
+++ frontend/src/components/taskComponent/TaskForm.jsx
@@ -1,6 +1,34 @@
+import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
+
 const TaskForm = () => {
+    // state
+    const [work, setWork] = useState();
+
+    // function to create Task
+    const createTask = (e) => {
+        e.preventDefault()
+    }
     return ( 
-        <>TaskForm</>
+        <>
+            <Box component="div">
+                <form action="" onSubmit={createTask}>
+                    <Box>
+                        <TextField 
+                            name="addWork"
+                            label="Add Work"
+                            margin="normal"
+                            variant="outlined"
+                            required
+                            id="addWork"
+                            onChange={(e) => setWork({...work, work: e.target.value})}
+                        />
+
+                        <Button variant="contained" color="success">Add</Button>
+                    </Box>
+                </form>
+            </Box>
+        </>
      );
 }
  
--- frontend/src/components/taskComponent/TaskList.jsx
+++ frontend/src/components/taskComponent/TaskList.jsx
@@ -1,6 +1,13 @@
+import { Container, Typography } from "@mui/material";
+import TaskForm from "./TaskForm";
+
 const TaskList = () => {
     return ( 
         <>
+            <Container>
+                <Typography variant="h5">Work Order Manager</Typography>
+                <TaskForm />
+            </Container>
         </>
      );
 }
