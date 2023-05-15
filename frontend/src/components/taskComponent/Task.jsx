e38e11b5cd1d48f1b20b24b450d73bc498af689a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-15 10:45:14 +0300

    Task setup

--- frontend/src/components/taskComponent/Task.jsx
+++ frontend/src/components/taskComponent/Task.jsx
@@ -1,6 +1,35 @@
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }));
+
 const Task = () => {
     return ( 
-        <>Task</>
+        <>
+        <Box sx={{ width: '100%' }}>
+            <Stack spacing={2}>
+                <Item>1. Door Lock 
+                    <Box>
+                        <EditIcon color='success' /> 
+                        <DeleteIcon color='error' />
+                    </Box>
+                </Item>
+            </Stack>
+        </Box>
+        </>
      );
 }
  
