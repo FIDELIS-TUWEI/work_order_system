de185c0603f63e335d9695ea418aeea3fd800f94
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-17 09:23:55 +0300

    userModel changes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,20 +3,10 @@ const bcrypt = require("bcryptjs");
 
 
 // schema for storing & validating user data in mongoDB
-const userSchema = mongoose.Schema({
-    name: {
+const userSchema = new mongoose.Schema({
+    username: {
         type: String,
         required: [true, "Please add a name"],
-    },
-    email: {
-        type: String,
-        required: [true, "Please add a email"],
-        unique: true,
-        trim: true,
-        match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            "Please Enter a valid email"
-        ],
     },
     password:{
         type: String,
@@ -24,6 +14,14 @@ const userSchema = mongoose.Schema({
         minLength: [6, "Password must be 6 characters"],
         //maxLength: [23, "Password must not be more than 23  characters"],
     },
+    roles: [{
+        type: String,
+        default: "Employee",
+    }],
+    active: {
+        type: Boolean,
+        default: true,
+    }
 }, {
     timestamps: true
 });
