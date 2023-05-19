18c316007e670ada4f05a14675657cf05f0580e2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-19 10:11:57 +0300

    UsersList

--- frontend/src/features/users/UserList.jsx
+++ frontend/src/features/users/UserList.jsx
@@ -1,9 +1,48 @@
+/* eslint-disable react/jsx-no-undef */
 import { useGetUsersQuery } from "./usersApiSlice";
 
 const UsersList = () => {
-    return ( 
-        <>UsersList</>
-    );
+
+    const {
+        data: users,
+        isLoading,
+        isSuccess,
+        isError,
+        error,
+    } = useGetUsersQuery()
+
+    let content
+
+    if (isLoading) content = <p>Loading...</p>
+
+    if (isError) {
+        content = <p>{error?.data?.message}</p>
+    }
+
+    if (isSuccess) {
+        const { ids } = users;
+
+        const tableContent = ids?.length
+            ? ids.map(userId => <User key={userId} userId={userId} />)
+            : null
+
+        content = (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th>Roles</th>
+                        <th>Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableContent}
+                </tbody>
+            </table>
+        )
+    }
+
+    return content
 }
 
 export default UsersList;
