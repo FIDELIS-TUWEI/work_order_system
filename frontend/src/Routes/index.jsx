6ab98449d087f31b5e1606150fed7d27dde158d1
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-19 09:49:04 +0300

    routes refactor

--- frontend/src/Routes/index.jsx
+++ frontend/src/Routes/index.jsx
@@ -2,10 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Login from '../pages/auth/Login';
 import Register from '../pages/auth/Register';
 import Layout from '../layouts/Layout';
-import Forgot from '../pages/auth/Forgot';
-import Reset from '../pages/auth/Reset';
 import Profile from '../pages/profile/Profile';
-import ChangePassword from '../pages/changePassword/ChangePassword';
 import UserList from '../pages/userList/UserList';
 import Tasks from '../pages/tasks/Tasks';
 
@@ -23,20 +20,8 @@ export const router = createBrowserRouter([
         element: <Layout />,
     },
     {
-        path: "/forgot",
-        element: <Forgot />,
-    },
-    {
-        path: "/reset/:resetToken",
-        element: <Reset />,
-    },
-    {
         path: "/profile",
         element: <Profile />
-    },
-    {
-        path: "/changepassword",
-        element: <ChangePassword />
     },
     {
         path: "/users",
