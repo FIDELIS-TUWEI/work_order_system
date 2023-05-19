49c37b8e3809cc042ea7c8242008c97f61e81bf7
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-19 08:55:56 +0300

    redux config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,14 @@ import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { store } from './app/store'
+import { Provider } from 'react-redux';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <ToastContainer />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </Provider>
   </React.StrictMode>,
 )
