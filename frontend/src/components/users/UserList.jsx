db66dd993abe0cededd60c61e15be617fea0201c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-20 19:00:14 +0300

    Userlist table

--- frontend/src/components/users/UserList.jsx
+++ frontend/src/components/users/UserList.jsx
@@ -1,14 +1,27 @@
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+
 import AddUser from "./AddUser";
 import DeleteUser from "./DeleteUsers";
 import UpdateUser from "./UpdateUsers";
 
 const UserList = () => {
     return ( 
-        <>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Username</TableCell>
+
+                </TableRow>
+            </TableHead>
+            <TableBody>
+
+            </TableBody>
             <AddUser />
             <UpdateUser />
             <DeleteUser />
-        </>
+        </Table>
      );
 }
  
