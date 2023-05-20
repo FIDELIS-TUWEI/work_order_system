8a0082e03c6492ab209fae79de229c7992f10612
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-20 19:15:11 +0300

    get users api request

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -10,3 +10,12 @@ export const addUser = async (data) => {
         console.log('Error while calling Add User Api', error)
     }
 }
+
+// get users request
+export const getUsers = async () => {
+    try {
+        return await axios.get(`${URL}/all`)
+    } catch (error) {
+        console.log('Error while calling getUsers Api', error);
+    }
+}
