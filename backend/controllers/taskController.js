76a54031e39e616d15927e6493ed2a34e39ce817
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-21 09:30:34 +0300

    Add task backend config operations / Api

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,78 +1,21 @@
 const Task = require("../models/taskModel");
 
-// createTask Controller
-const createTask = async (req, res) => {
+// addTask Controller
+const addTask = async (req, res) => {
+    const task = req.body;
+
+    const newTask = new Task(task);
+
     try {
-        const task = await Task.create(req.body);
-        res.status(200).json(task)
+        await newTask.save();
+        res.status(200).json(newTask);
     } catch (error) {
         res.status(500).json({msg: error.message})
     }
 };
 
-// GetTasks Controller
-const getTasks = async (req, res) => {
-    try {
-        const tasks = await Task.find();
-        res.status(200).json(tasks);
-    } catch (error) {
-        res.status(500).json({msg: error.message});
-    }
-};
 
-// Get a singleTask controller
-const getTask = async (req, res) => {
-    try {
-        const {id} = req.params
-        const task = await Task.findById(id);
-        if (!task) {
-            return res.status(404).json(`No task with id: ${id} found`);
-        };
-
-        res.status(200).json(task);
-    } catch (error) {
-        res.status(500).json({msg: error.message});
-    }
-};
-
-// Delete Task controller
-const deleteTask = async (req, res) => {
-    try {
-        const {id} = req.params;
-        const task = await Task.findByIdAndDelete(id);
-        if (!task) {
-            return res.status(404).json(`No task with id: ${id} found`);
-        }
-
-        res.status(200).send("Task deleted");
-    } catch (error) {
-        res.status(500).json({msg: error.message});
-    }
-};
-
-// update a task controller
-const updateTask = async (req, res) => {
-    try {
-        const {id} = req.params;
-        const task = await Task.findByIdAndUpdate(
-            {_id: id}, req.body, {
-                new: true,
-                runValidators: true,
-            });
-        
-        if (!task) {
-            return res.status(404).json(`No task with id: ${id} found`)
-        }
-        res.status(200).json(task);
-    } catch (error) {
-        res.status(500).json({msg: error.message})
-    }
-}
 
 module.exports = {
-    createTask,
-    getTasks,
-    getTask,
-    deleteTask,
-    updateTask,
+    addTask,
 }
--- frontend/src/api/taskApi.js
+++ frontend/src/api/taskApi.js
@@ -5,7 +5,7 @@ const URL = 'http://localhost:5000'
 
 export const addTask = async (data) => {
     try {
-        return await axios.post(`${URL}/add`, data)
+        return await axios.post(`${URL}/addtask`, data)
     } catch (error) {
         console.log('Error while calling Add Task Api', error)
     }
