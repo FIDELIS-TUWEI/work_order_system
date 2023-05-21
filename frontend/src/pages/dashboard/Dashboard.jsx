f6ff12e752428db818b47b1295e70da188d474f1
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-21 20:03:44 +0300

    useMemo hook for darkmode theme

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { styled } from '@mui/material/styles';
+import { ThemeProvider, styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,8 +7,11 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import SideBar from './SideBar';
+import { Tooltip, createTheme } from '@mui/material';
+import { Brightness4, Brightness7, Home } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 const drawerWidth = 240;
 
@@ -35,35 +38,54 @@ const AppBar = styled(MuiAppBar, {
 
 export default function Dashboard() {
   const [open, setOpen] = useState(false);
+  const [dark, setDark] = useState(true);
+
+  // hook to handle dark mode state
+  const darkTheme = useMemo(() => createTheme({
+    palette:{
+        mode: dark ? 'dark' : 'light'
+    }
+  }), [dark])
 
   const handleDrawerOpen = () => {
     setOpen(true);
   };
 
+  const navigate = useNavigate();
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{
-              marginRight: 5,
-              ...(open && { display: 'none' }),
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Mini variant drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <SideBar {...{open, setOpen}} />
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+        <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="fixed" open={open}>
+            <Toolbar>
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                sx={{
+                marginRight: 5,
+                ...(open && { display: 'none' }),
+                }}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Tooltip title='Go back to Home Page'>
+                <IconButton sx={{ mr: 1 }} onClick={() => navigate("/")}>
+                    <Home />
+                </IconButton>
+            </Tooltip>
+            <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+                Dashboard
+            </Typography>
+            <IconButton onClick={() => setDark(!dark)}>
+                { dark ? <Brightness7 /> : <Brightness4 /> }
+            </IconButton>
+            </Toolbar>
+        </AppBar>
+        <SideBar {...{open, setOpen}} />
+        </Box>
+    </ThemeProvider>
   );
 }
