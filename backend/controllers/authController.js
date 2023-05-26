e6dc09f2ce751accc2ff20e48152718374f90e37
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-26 17:08:41 +0300

    register auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,7 +7,13 @@ export const register = asyncHandler( async (req, res, next) => {
         const salt = bcrypt.genSalt(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
 
-        const newUser = new User){}
+        const newUser = new User({
+            ...req.body,
+            password: hash,
+        });
+
+        await newUser.save();
+        res.status(200).send("User has been created")
     } catch (err) {
         next(err);
     }
