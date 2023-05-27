f916c5020ecee8281431819d301f0bbe9a23cf0b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-27 16:44:16 +0300

    routes refactor

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { 
-    addUser, getUsers, getUser, editUser, deleteUser
+    getUsers, getUser, editUser, deleteUser
 } = require("../controllers/userController");
 const protect = require("../middleWare/authMiddleware");
 
 // user route
-router.post("/add", addUser);
 router.get("/all", getUsers);
 router.get("/getuser/:id", getUser);
 router.put("/edituser/:id", editUser);
