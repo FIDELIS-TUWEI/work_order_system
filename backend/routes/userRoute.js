89d7e459cd75b4666303c5b3d04b71e66bef6517
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-27 17:08:53 +0300

    backend authenticated routes update

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const { 
     getUsers, getUser, editUser, deleteUser
 } = require("../controllers/userController");
-const protect = require("../middleWare/authMiddleware");
-
+const { verifyAdmin, verifyToken, verifyUser } = require("../utils/verifyToken")
 // user route
-router.get("/all", getUsers);
-router.get("/getuser/:id", getUser);
-router.put("/edituser/:id", editUser);
-router.delete("/delete/:id", deleteUser);
+router.get("/all", verifyAdmin, verifyToken, getUsers);
+router.get("/getuser/:id", verifyAdmin, verifyUser, verifyToken, getUser);
+router.put("/edituser/:id", verifyAdmin, verifyUser, verifyToken, editUser);
+router.delete("/delete/:id", verifyAdmin, verifyUser, verifyToken, deleteUser);
 
 module.exports = router;
