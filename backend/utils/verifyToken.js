3e1acbd6a116cc6df99ed0c5ab2e687c7158e8f3
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-27 16:58:46 +0300

    Verify user

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -11,5 +11,16 @@ export const verifyToken = (req, res, next) => {
         if (err) return next(createError(403, "Token is Invalid"));
         req.user = user;
         next();
-    })
+    });
 }
+
+// Verify user
+export const verifyUser = (req, res, next) => {
+    verifyToken(req, res, next, () => {
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            next();
+        } else {
+            return next(createError(403, "You are not authorised!"))
+        }
+    });
+};
