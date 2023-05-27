dd2268e1fd8a161c7735064d412cd2bbcc30d2ee
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-27 16:37:52 +0300

    find and delete user with id backedend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,38 +1,24 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 
-// Create New User
-const addUser = asyncHandler( async (req, res) => {
-    const user = req.body;
-    
-    const newUser = new User(user);
-
-    try {
-        await newUser.save();
-        res.status(201).json(newUser);
-    } catch (error) {
-        res.status(409).json({ message: error.message });
-    }
-});
-
 // get All users
-const getUsers = asyncHandler( async (req, res) => {
+const getUsers = asyncHandler( async (req, res, next) => {
     try {
         const users = await User.find({});
         res.status(200).json(users);
     } catch (error) {
-        res.status(404).json({ message: error.message });
+        next(err);
     }
 });
 
 // Get single User with unique Id
-const getUser = asyncHandler( async (req, res) => {
+const getUser = asyncHandler( async (req, res, next) => {
     try {
         //const user = await User.find({ _id: req.params.id })
         const user = await User.findById(req.params.id);
         res.status(200).json(user);
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        next(err);
     }
 });
 
@@ -52,15 +38,14 @@ const editUser = asyncHandler( async (req, res) => {
 // Delete User
 const deleteUser = asyncHandler( async (req, res) => {
     try {
-        await User.deleteOne({ _id: req.params.id });
+        await User.findByIdAndDelete({ _id: req.params.id });
         res.status(200).json({ message: "User deleted Succesfully" });
     } catch (error) {
-        res.status(409).json({ message: error.message });
+        next(err);
     }
 })
 
 module.exports = {
-    addUser,
     getUsers,
     getUser,
     editUser,
