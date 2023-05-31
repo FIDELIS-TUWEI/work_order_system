a66116a34868e04f71660145b431817a76bf2508
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-31 16:33:22 +0300

    protect getTask route functionality

--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { addTask, getTasks, getTask, editTask, deleteTask } = require("../controllers/taskController");
+const { protect } = require("../controllers/authController");
 
 // routes
 router.post("/addtask", addTask);
-router.get("/gettasks", getTasks);
+router.get("/gettasks", protect, getTasks);
 router.get("/gettask/:id", getTask);
 router.put("/edittask/:id", editTask);
 router.delete("/:id", deleteTask)
