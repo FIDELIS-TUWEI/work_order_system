8ff750f52bd76f3a3778c419e7504aaf3cf2a7e3
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-06 13:40:16 +0300

    Tasks Stats model schema

---
+++ backend/models/TasksStats.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const tasksStatSchema = new mongoose.Schema({
+    taskId: String,
+    monthlyData: [
+        {
+            month: String,
+            totalTasks: Number,
+            totalPendingTasks: Number,
+            totalCompletedTasks: Number,
+        }
+    ],
+    dailyData: [
+        {
+            date: String,
+            totalTasks: Number,
+            totalPendingTasks: Number,
+            totalCompletedTasks: Number,
+        }
+    ],
+},
+{ timestamps: true }
+);
+
+const TasksStats = mongoose.model("TasksStats", tasksStatSchema);
+
+module.exports = TasksStats;
