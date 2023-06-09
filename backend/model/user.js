d114d79ce3531798a53f11969b93397a8958b81d
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-09 14:25:38 +0300

    user model / validator added

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,19 +1,41 @@
 const mongoose = require("mongoose");
+const validate  = require("validatorjs");
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, "Please fill your name"]
     },
     username: {
         type: String,
-        required: true,
+        required: [true, "Please enter a Username"],
         uppercase: true
     },
     password: {
         type: String,
-        required: true,
-        minLength: 8
+        required: [true, "Please enter a password"],
+        minLength: 6,
+        select: false
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, "Please fill your password to confirm"],
+        validate: {
+            validator: function(el) {
+                return el === this.password
+            },
+            message: "Your password and confirmation password are not the same!",
+        },
+    },
+    role: {
+        type: String,
+        enum: ["user", "hod", "admin"],
+        default: "hod"
+    },
+    active: {
+        type: Boolean,
+        default: true,
+        select: false
     }
 }, { timestamps: true }
 );
