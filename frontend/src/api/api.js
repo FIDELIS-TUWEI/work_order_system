6fbe9bf14ae4feb5a4021e1f9278d715b70e8e8b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-12 12:58:30 +0300

    frontend code refactor

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,14 +3,6 @@ import axios from "axios";
 // backend url endpoint
 const URL = 'http://localhost:5000/hin'
 
-export const registerUser = async (data) => {
-    try {
-        return await axios.post(`${URL}/register`, data)
-    } catch (error) {
-        console.log('Error while calling Add User Api', error)
-    }
-}
-
 // get users request
 export const getUsers = async () => {
     try {
--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -3,6 +3,14 @@ import axios from "axios";
 // backend url endpoint
 const URL = 'http://localhost:5000/hin'
 
+// Register
+export const registerUser = async (data) => {
+    try {
+        return await axios.post(`${URL}/register`, data)
+    } catch (error) {
+        console.log('Error while calling Add User Api', error)
+    }
+}
 // Login 
 export const loginUser = async(data) => {
     try {
