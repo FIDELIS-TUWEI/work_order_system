1bf7da367988753e467e9c7aa5f48b8da7885b0c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-14 17:47:08 +0300

    frontend code refactor

--- backend/routes/category-routes.js
+++ backend/routes/category-routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { createCategory, getCategories } = require('../controllers/category-controller');
-const { isAuthenticated } = require("../middleware/authMiddleware");
+const { isAuthenticate } = require("../middleware/authMiddleware");
 
 
 router.post("/category/create", isAuthenticated, createCategory);
