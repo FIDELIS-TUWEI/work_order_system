bbd469fdc3f84eca6a9f4daac5affee5b58d4df9
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-17 15:21:29 +0300

    changes

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { signUp, signIn, singleUser, userProfile, logout, getAllUsers } = require('../controllers/auth');
-const { isAuthenticated, isAdmin } = require("../middleware/authMiddleware");
+const { isAuthenticated } = require("../middleware/authMiddleware");
 
-router.post('/signup', isAuthenticated, isAdmin(["admin"]), signUp);
+router.post('/signup', isAuthenticated, signUp);
 router.post('/signin', signIn);
 router.get("/logout", logout);
 router.get("/all-users", getAllUsers);
