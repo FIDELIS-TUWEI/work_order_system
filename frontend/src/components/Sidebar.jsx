723515e5d5a05ec3e16199fdd9be3db7935515fc
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-18 17:19:41 +0300

    frontend mode bug fix

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -8,13 +8,68 @@ import {
     SettingsOutlined, ChevronLeft,
     ChevronRightOutlined, HomeOutlined,
     ShoppingCartOutlined, Groups2Outlined,
-    ReceiptLongOutlined, PublicOutlined,
+    ReceiptLongOutlined, TableViewOutlined,
     TodayOutlined, CalendarMonthOutlined,
-    AdminPanelSettingsOutlined, TrendingUpOutlined, PieChartOutlined
+    AdminPanelSettingsOutlined, TrendingUpOutlined, PieChartOutlined,
 } from "@mui/icons-material";
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import FlexBetween from "./FlexBetween";
+
+const navItems = [
+    {
+        text: "Dashboard",
+        icon: <HomeOutlined />
+    },
+    {
+        text: "Client Facing",
+        icon: null,
+    },
+    {
+        text: "Tasks",
+        icon: <ShoppingCartOutlined />
+    },
+    {
+        text: "Users",
+        icon: <Groups2Outlined />
+    },
+    {
+        text: "Assigned Work",
+        icon: <ReceiptLongOutlined />
+    },
+    {
+        text: "Categories",
+        icon: null,
+    },
+    {
+        text: "Overview",
+        icon: <TableViewOutlined />
+    },
+    {
+        text: "Daily",
+        icon: <TodayOutlined />
+    },
+    {
+        text: "Monthly",
+        icon: <CalendarMonthOutlined />
+    },
+    {
+        text: "Breakdown",
+        icon: <PieChartOutlined/>
+    },
+    {
+        text: "Management",
+        icon: null,
+    },
+    {
+        text: "Admin",
+        icon: <AdminPanelSettingsOutlined />
+    },
+    {
+        text: "Performance",
+        icon: <TrendingUpOutlined />
+    },
+]
 
 const Sidebar = ({
     isNonMobile,
@@ -48,7 +103,73 @@ const Sidebar = ({
                     width: drawerWidth
                 }
             }}
-        ></Drawer>
+        >
+            <Box width="100%">
+                <Box m="1.5rem 2rem 2rem 3rem">
+                    <FlexBetween color={theme.palette.primary.main}>
+                        <Box display="flex" alignItems="center" gap="0.5rem">
+                            <Typography variant="h4" fontWeight="bold">
+                                HIN
+                            </Typography>
+                        </Box>
+                        {!isNonMobile && (
+                            <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                                <ChevronLeft />
+                            </IconButton>
+                        )}
+                    </FlexBetween>
+                </Box>
+                <List>
+                    {navItems.map(({text, icon}) => {
+                        if (!icon) {
+                            return (
+                                <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
+                                    {text}
+                                </Typography>
+                            )
+                        }
+                        const lcText = text.toLowerCase();
+
+                        return (
+                            <ListItem key={text} disablePadding>
+                                <ListItemButton
+                                    onClick={() => {
+                                        navigate(`/${lcText}`);
+                                        setActive(lcText);
+                                    }}
+                                    sx={{
+                                        backgroundColor: 
+                                            active === lcText 
+                                                ? theme.palette.secondary[300] 
+                                                : "transparent",
+                                        color:
+                                            active === lcText
+                                                ? theme.palette.primary[600]
+                                                : theme.palette.secondary[100],
+                                    }}
+                                >
+                                    <ListItemIcon
+                                        sx={{
+                                            ml: "2rem",
+                                            color:
+                                                active === lcText
+                                                    ? theme.palette.primary[600]
+                                                    : theme.palette.secondary[200],
+                                        }}
+                                    >
+                                        {icon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={text} />
+                                    {active === lcText && (
+                                        <ChevronRightOutlined sx={{ ml: "auto" }} />
+                                    )}
+                                </ListItemButton>
+                            </ListItem>
+                        )
+                    })}
+                </List>
+            </Box>
+        </Drawer>
     )}
   </Box>
 }
