e4940ce105088eb05e5421d136515a0a47a5df41
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-18 07:12:37 +0300

    MUI redux theme setup config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,22 @@ import "./index.css";
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/Routes';
 
+import { configureStore } from "@reduxjs/toolkit"
+import globalReducer from "state";
+import { Provider } from 'react-redux';
+import App from './App';
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
