6a399b029faa940c63596bb1db3d648506e6394e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-19 09:25:27 +0300

    baseUrl added

--- frontend/src/state/api.js
+++ frontend/src/state/api.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
-const baseUrl = `http://localhost:5000/hin`
+const baseUrl = `http://localhost:5000`
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl }),
@@ -8,7 +8,7 @@ export const api = createApi({
     tagTypes: ["User"],
     endpoints: (build) => ({
         singleUser: build.query({
-            query: (id) => `/user/${id}`,
+            query: (id) => `/hin/user/${id}`,
             providesTags: ["User"]
         })
     })
