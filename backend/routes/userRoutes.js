69bc9911948657cba7020498d6ec0da3ac6bce32
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-20 15:49:18 +0300

    user router update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { signUp, signIn, logout, singleUser } = require("../controllers/user-controller");
+const { signUp, signIn, logout, singleUser, updateUser, deleteUser } = require('../controllers/userController');
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 router.get("/logout", logout);
 router.get("/user/:id", singleUser);
+router.patch("/update-user", updateUser);
+router.delete("delete-user", deleteUser);
 
 module.exports = router;
