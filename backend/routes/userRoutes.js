3a7d27923693617381f5784b1058ecc6680762b0
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-21 10:10:15 +0300

    user routes updated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { signUp, signIn, logout, singleUser, updateUser, deleteUser } = require('../controllers/userController');
+const { createNewUser, updateUser, getAllUsers, deleteUser } = require('../controllers/userController');
 
-router.post("/signup", signUp);
-router.post("/signin", signIn);
-router.get("/logout", logout);
-router.get("/user/:id", singleUser);
+router.post("/register", createNewUser);
+router.get("/all-users", getAllUsers);
 router.patch("/update-user", updateUser);
 router.delete("delete-user", deleteUser);
 
