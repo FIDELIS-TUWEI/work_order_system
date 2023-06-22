38a8ee88200846d519d39a33ad80c75ebd6bb9a8
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-22 13:18:25 +0300

    create user logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,33 @@
 const User = require("../model/user");
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const createNewUser = asyncHandler (async (req, res, next) => {
-    res.send("New User")
+    try {
+        const { name, username, password } = req.body;
+
+        // check if user exists
+        const userExists = await User.findOne({ username });
+        if (userExists) {
+            return res.status(400).json({ message: "Username already exists" });
+        }
+
+        // Hash password
+        const hashedPswd = await bcrypt.hash(password, 10);
+
+        // Create new user
+        const user = new User({ name, username, password: hashedPswd });
+        await user.save();
+
+        // Generate Token
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+        // Return Token
+        res.status(201).json({token});
+    } catch (error) {
+        
+    }
 });
 
 const getAllUsers = asyncHandler (async (req, res, next) => {
