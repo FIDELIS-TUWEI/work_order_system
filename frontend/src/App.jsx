0ee95cb86ba169744d3a45971d17bec2ca84b0a8
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-25 00:53:39 +0300

    main app setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,30 @@
+import { colorModeContext, useMode } from "./theme"
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
-import { useMemo } from "react";
-import { useSelector } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import { themeSettings } from "./theme";
+import { colorModeContext, themeSettings } from "./theme";
 import { router } from "./routes/Routes";
+import TopBar from "scenes/global/TopBar";
 
 function App() {
-    // redux state
-    const mode = useSelector((state) => state.global.mode);
-    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+    // hook
+    const [theme, colorMode] = useMode();
 
   
-  return <div className="app">
-    <RouterProvider router={router}>
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-        </ThemeProvider>
-    </RouterProvider>
-  </div>
+  return ( 
+  <colorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="context">
+            <TopBar />
+          </main>
+          <RouterProvider router={router}>
+          </RouterProvider>
+        </div>
+    </ThemeProvider>
+
+  </colorModeContext.Provider>
+  )
 }
 
 export default App;
