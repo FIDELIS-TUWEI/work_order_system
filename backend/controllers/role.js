9decd2f027878a90729bd2610914f8208273cbb3
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-24 13:29:03 +0300

    update role bug fix

--- backend/controllers/role.js
+++ backend/controllers/role.js
@@ -7,7 +7,7 @@ const assignRole = asyncHandler (async (req, res, next) => {
         if (req.body.role && req.body !== "") {
             const newRole = new Role(req.body);
             await newRole.save();
-            return res.status(201).json({ message: "Role created succesfully" });
+            return res.status(200).json({ message: "Role created succesfully" });
         } else {
             return res.status(400).json({ message: "Bad request" });
         }
@@ -27,10 +27,26 @@ const updateRole = asyncHandler (async (req, res, next) => {
             return res.status(404).json({ message: "No role found" })
         }
 
-        res.status(200).json({ message: "Role Updated" });
+        res.status(200).json({
+            success: true,
+            message: "Role Updated" 
+            });
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Get All Roles
+const getAllRoles = asyncHandler (async (req, res, next) => {
+    try {
+        const role = await Role.find({});
+        res.status(200).json({
+            success: true,
+            role
+        });
     } catch (error) {
         next(error);
     }
 })
 
-module.exports = {assignRole, updateRole};
+module.exports = {assignRole, updateRole, getAllRoles};
--- backend/routes/roleRoutes.js
+++ backend/routes/roleRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { assignRole, updateRole } = require("../controllers/role");
+const { assignRole, updateRole, getAllRoles } = require("../controllers/role");
 
 router.post("/role", assignRole);
+router.get("getAll", getAllRoles);
 router.put("/update/:id", updateRole);
 
 module.exports = router;
