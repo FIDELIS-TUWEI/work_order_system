1d9a4431d072860db964bfd6326c385456d950ca
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-24 20:18:05 +0300

    update Task logic

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -5,12 +5,35 @@ const asyncHandler = require("express-async-handler");
 const newTask = asyncHandler (async (req, res, next) => {
     try {
         const task = await Task.create(req.body);
-        res.status(200).json(task);
+        res.status(200).json({
+            success: true,
+            data: {
+                task
+            }
+        });
     } catch (error) {
         next(error);
     }
 });
 
+// update Task logic
+const updateTask = asyncHandler (async (req, res, next) => {
+    try {
+       const {id} = req.params
+       const task = await Task.findByIdAndUpdate({ _id: id }, req.body, {new: true});
+       res.status(200).json({
+        success: true,
+        data: {
+            task
+        },
+        message: `Task updated succesfully `
+       }) 
+    } catch (error) {
+        next(error);
+    }
+})
+
 module.exports = {
     newTask,
+    updateTask,
 }
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { newTask } = require('../controllers/taskController');
+const { newTask, updateTask } = require('../controllers/taskController');
 const router = express.Router();
 
 router.post("/newtask", newTask);
+router.put("/tasks/update/:id", updateTask);
 
 module.exports = router;
