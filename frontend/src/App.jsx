ecd6a091f524347f697f3e20e28d10825c9181d0
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-26 18:00:35 +0300

    ThemeProvider wrapped

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
 
 function App() {
   return (
     <>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
+        </ThemeProvider>
     </>
   )
 }
