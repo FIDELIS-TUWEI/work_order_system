78076a852b83392d48b3364fe75cc2cf881f1658
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-26 19:16:17 +0300

    Navbar components created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,124 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import WorkIcon from "@mui/icons-material/Work"
+import { useState } from 'react';
+import { useTheme } from '@emotion/react';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function Navbar() {
+    const { palette } = useTheme();
+    const [anchorElNav, setAnchorElNav] = useState(null);
+    const [anchorElUser, setAnchorElUser] = useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <AppBar position="static">
+      <Container>
+        <Toolbar disableGutters>
+          <WorkIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            WORK ORDER SYSTEM
+          </Typography>
 
-export default Navbar
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            {/*MENU DESKTOP ONLY */}
+
+            <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color:'white', display: 'block' }}
+            >
+                <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+                    Home
+                </Link>
+            </Button>
+
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="../assets/avatar.png" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Typography textAlign="center"><Link style={{ 
+                    textDecoration: "none",
+                    color: palette.primary.main
+                    }} to="/admin/dashboard">Dashboard</Link>
+                </Typography>
+                <Typography textAlign="center"><Link style={{ 
+                    textDecoration: "none",
+                    color: palette.primary.main
+                    }} to="/login">Log In</Link>
+                </Typography>
+                <Typography textAlign="center"><Link style={{ 
+                    textDecoration: "none",
+                    color: palette.primary.main
+                    }}>Log Out</Link>
+                </Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Navbar;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import Navbar from "../components/Navbar"
 
 const Home = () => {
   return (
     <>
+        <Navbar />
         <h1>Home Page</h1>
     </>
   )
