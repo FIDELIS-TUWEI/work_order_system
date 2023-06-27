9b69bbc6fc94d8615d0d2a04b6bab615c4b905dd
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-27 22:04:37 +0300

    Work action reducer code bug fixed

--- frontend/src/redux/actions/workAction.js
+++ frontend/src/redux/actions/workAction.js
@@ -1,18 +1,18 @@
 import axios from "axios";
-import { JOB_LOAD_REQUEST, JOB_LOAD_SUCCESS, JOB_LOAD_FAIL } from "../constants/workConstants";
+import { WORK_LOAD_REQUEST, WORK_LOAD_SUCCESS, WORK_LOAD_FAIL } from "../constants/workConstants";
 
 export const taskLoadAction = (pageNumber, keyword = '', cat = '', location = '') => async(dispatch) => {
-    dispatch({ type: JOB_LOAD_REQUEST })
+    dispatch({ type: WORK_LOAD_REQUEST })
 
     try {
         const { data } = await axios.get(`/hin/tasks/show/?pageNumber=${pageNumber}&keyword=${keyword}&cat=${cat}&location=${location}`)
         dispatch({
-            type: JOB_LOAD_SUCCESS,
+            type: WORK_LOAD_SUCCESS,
             payload: data
         })
     } catch (error) {
         dispatch({
-            type: JOB_LOAD_FAIL,
+            type: WORK_LOAD_FAIL,
             payload: error.response.data.error
         })
     }
