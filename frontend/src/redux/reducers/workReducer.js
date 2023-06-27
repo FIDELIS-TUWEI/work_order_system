62ba5878c7ef217cda4208d5fe47b5467228347b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-27 12:30:51 +0300

    work reducer

---
+++ frontend/src/redux/reducers/workReducer.js
@@ -0,0 +1,28 @@
+
+
+export const loadWorkReducer = (state = { tasks:[] }, action) => {
+    switch (action.type) {
+        case WORK_LOAD_REQUEST:
+            return {loading:true}
+        
+        case WORK_LOAD_SUCCESS: 
+        return {
+            loading: false,
+            success: action.payload.success,
+            page: action.payload.page,
+            pages: action.payload.pages,
+            count: action.payload.count,
+            setUniqueLocation: action.payload.setUniqueLocation,
+            tasks: action.payload.tasks
+        } 
+        case WORK_LOAD_FAIL: 
+        return {
+            loading: false,
+            error: action.payload
+        }
+        case WORK_LOAD_RESET: 
+        return {}
+        default:
+            return state;
+    }
+}
