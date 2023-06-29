ef89d015a1891750ffe5cce4f55c4be59a952e38
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-29 16:10:21 +0300

    search url params

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/search/location/:location" element={<Home />} />
+                <Route path="/search/:keyword" element={<Home />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </ThemeProvider>
