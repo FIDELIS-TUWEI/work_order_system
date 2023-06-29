2cc5b1d7e19d06e3ba76e62cc1b2a8f09e8981c7
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-29 16:16:41 +0300

    Not found component styled

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,16 @@
+import { Box } from "@mui/material"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 const NotFound = () => {
   return (
-    <h1>Page Not Found</h1>
+    <>
+      <Navbar />
+      <Box sx={{ height: '81vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <h1>Page Not Found!</h1>
+      </Box>
+      <Footer />
+    </>
   )
 }
 
