58833677330b8821ff7f1e37ceb7aa7df750bdba
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-06-29 16:35:41 +0300

    user signin reducer config

---
+++ frontend/src/redux/reducers/userReducer.js
@@ -0,0 +1,21 @@
+
+
+export const userReducerSignIn = (state = {}, action ) => {
+    switch (action.type) {
+        case USER_SIGNIN_REQUEST:
+            return { loading: true, userInfo: null, isAuthenticated: false }
+        case USER_SIGNIN_SUCCESS:
+            return {
+                loading: false,
+                userInfo: action.payload,
+                isAuthenticated: true
+            }
+        case USER_SIGNIN_FAIL:
+            return { loading: false, userInfo: null, isAuthenticated: false, error: action.payload }
+        case USER_SIGNIN_RESET:
+            return {}
+    
+        default:
+            return state;
+    }
+}
