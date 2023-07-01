69b8b7915a041a8a5817770540d8e7b6a3e0ee9a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-01 20:24:35 +0300

    Login form design

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box } from "@mui/material"
+import { Avatar, Box, Button, TextField } from "@mui/material"
 import LockClockOutlined from '@mui/icons-material/LockClockOutlined'
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
@@ -14,6 +14,42 @@ const LogIn = () => {
                     <Avatar sx={{ m: 1, bgcolor: "primary.main", mb: 3 }}>
                         <LockClockOutlined />
                     </Avatar>
+
+                    <TextField 
+                        sx={{ mb: 3 }}
+                        fullWidth
+                        id="username"
+                        label="Username"
+                        name="username"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        placeholder="Username"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.username && Boolean(formik.errors.username)}
+                        helperText={formik.touched.username && formik.errors.username}
+                    />
+
+                    <TextField 
+                        sx={{ mb: 3 }}
+                        fullWidth
+                        id="password"
+                        label="password"
+                        name="password"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        placeholder="Password"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
+                    />
+
+                    <Button fullWidth variant="contained" type="submit">Log In</Button>
                 </Box>
             </Box>
         </Box>
@@ -22,4 +58,4 @@ const LogIn = () => {
   )
 }
 
-export default LogIn
+export default LogIn;
