a0714cdaa17f561d7afdacc61cc3d4f5b1342187
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-02 16:10:06 +0300

    auth & admin middleware config

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,34 @@
 const jwt = require("jsonwebtoken");
+const ErrorResponse = require("../utils/errorResponse");
+const User = require("../model/user");
 
 // check if user is authenticated
-const auth = (req, res, next) => {
+const isAuthenticated = async (req, res, next) => {
+    const { token } = req.cookies;
+    // Make sure token exists
+    if (!token) {
+        return next(new ErrorResponse("Not authorized to access this route", 401));
+    }
+
+    try {
+        // Verify Token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id);
+        next();
+    } catch (error) {
+        return next(new ErrorResponse("Not authorized to access this route", 401));
+    }
+};
+
+// Middleware for Admin
+const isAdmin = (req, res, next) => {
+    if (req.user.role === 0) {
+        return next(new ErrorResponse("Access Denied! You must be an admin", 401));
+    }
+}
+
+// check if user is authenticated
+{/*const auth = (req, res, next) => {
     const { token }= req.cookies;
 
     if (!token) {
@@ -17,10 +44,10 @@ const auth = (req, res, next) => {
     } catch (error) {
         return res.status(400).send("Access Denied, Invalid auth token");
     }
-}
+}}*/}
 
 // user profile
-const isUser = (req, res, next) => {
+{/*const isUser = (req, res, next) => {
     auth(req, res, () => {
         if (req.user._id === req.params.id || req.user.isAdmin) {
             next();
@@ -28,10 +55,10 @@ const isUser = (req, res, next) => {
             res.status(403).send("Access Denied! You are not authorized ")
         }
     });
-};
+};*/}
 
 // Admin
-const isAdmin = (req, res, next) => {
+{/*const isAdmin = (req, res, next) => {
     auth(req, res, () => {
         if (req.user.isAdmin) {
             next();
@@ -39,10 +66,12 @@ const isAdmin = (req, res, next) => {
             res.status(403).send("Forbidden! Not Authorized")
         }
     })
-}
+}*/}
 
 module.exports = {
-    auth,
-    isUser,
+    isAuthenticated,
     isAdmin,
+    //auth,
+    //isUser,
+    //isAdmin,
 }
