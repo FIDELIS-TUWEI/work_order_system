c0bd855605840be730f3065b824d1f6c5fdc92d0
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-02 11:49:45 +0300

    User Logout Action

--- frontend/src/redux/actions/userAction.js
+++ frontend/src/redux/actions/userAction.js
@@ -1,7 +1,11 @@
 import axios from "axios";
-import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS } from "../constants/userConstant";
 import { toast } from 'react-toastify';
+import { USER_LOGOUT_FAIL, USER_LOGOUT_REQUEST, USER_LOGOUT_SUCCESS, USER_SIGNIN_FAIL, 
+    USER_SIGNIN_REQUEST, 
+    USER_SIGNIN_SUCCESS 
+} from "../constants/userConstant";
 
+// User Login Action
 export const userSignInAction = (user) => async(dispatch) => {
     dispatch({ type: USER_SIGNIN_REQUEST })
 
@@ -18,5 +22,26 @@ export const userSignInAction = (user) => async(dispatch) => {
             payload: error.response.data.error
         });
         toast.error(error.response.data.error)
+    }
+}
+
+// User Logout Action
+export const userLogoutAction = () => async(dispatch) => {
+    dispatch({ type: USER_LOGOUT_REQUEST });
+
+    try {
+        const { data } = await axios.get("/hin/logout");
+        localStorage.removeItem("userInfo");
+        dispatch({
+            type: USER_LOGOUT_SUCCESS,
+            payload: data
+        });
+        toast.success("Log Out Succesfully!");
+    } catch (error) {
+        dispatch({
+            type: USER_LOGOUT_FAIL,
+            payload: error.response.data.error
+        });
+        toast.error(error.response.data.error);
     }
 }
