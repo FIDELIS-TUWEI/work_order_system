619761b6c9df0a7351d9247364b02abb9c3a651c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-03 05:51:43 +0300

    task type Action url created

--- frontend/src/redux/actions/taskTypeAction.js
+++ frontend/src/redux/actions/taskTypeAction.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { WORK_TYPE_LOAD_FAIL, WORK_TYPE_LOAD_REQUEST, WORK_TYPE_LOAD_SUCCESS } from "../constants/taskTypeConstants";
 
+const url = "http://localhost:5000"
+
 export const taskTypeLoadAction = () => async(dispatch) => {
     dispatch({ type: WORK_TYPE_LOAD_REQUEST })
 
     try {
-        const { data } = await axios.get('/hin/type/tasks')
+        const { data } = await axios.get(`${url}/hin/type/tasks`)
         dispatch({
             type: WORK_TYPE_LOAD_SUCCESS,
             payload: data
