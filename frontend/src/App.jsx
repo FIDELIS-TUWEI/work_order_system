8104a7561826241dcfce49187d3fbaa1a9fe43f6
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-03 05:28:57 +0300

    User TasksHistory Higher Order Component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,16 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { ProSidebarProvider } from "react-pro-sidebar";
 import LogIn from "./pages/LogIn";
 import UserDashboard from "./pages/user/UserDashboard";
 import UserRoute from "./components/UserRoute";
 import Layout from "./pages/global/Layout";
-import { ProSidebarProvider } from "react-pro-sidebar";
+import UserTasksHistory from "./pages/user/UserTasksHistory";
 
 // Higher Order Component
 const UserDashboardHOC = Layout(UserDashboard);
+const UserTasksHistoryHOC = Layout(UserTasksHistory);
 
 function App() {
   return (
@@ -28,6 +30,7 @@ function App() {
                   <Route path="/search/:keyword" element={<Home />} />
                   <Route path="/login" element={<LogIn />} />
                   <Route path="/user/dashboard" element={<UserRoute><UserDashboardHOC /></UserRoute>} />
+                  <Route path="/user/tasks" element={<UserRoute><UserTasksHistoryHOC /></UserRoute>} />
                   <Route path="*" element={<NotFound />} />
               </Routes>
               </ProSidebarProvider>
