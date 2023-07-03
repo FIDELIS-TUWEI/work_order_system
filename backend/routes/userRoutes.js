9fc0c4dd8f165f8d5912dc9be7c31725e5fbfb79
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-03 18:33:01 +0300

    delete user with admin role route protected

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, singleUser, editUser } = require('../controllers/userController');
+const { getAllUsers, singleUser, editUser, deleteUser } = require('../controllers/userController');
 const { isAuthenticated, isAdmin } = require('../middleware/authMiddleware');
 
 router.get("/all-users", isAuthenticated, isAdmin, getAllUsers);
 router.get("/user/:id",isAuthenticated, singleUser);
 router.put("/edit/:id", isAuthenticated, isAdmin, editUser);
+router.delete("/admin/user/delete/:id", isAuthenticated, isAdmin, deleteUser);
 
 
 module.exports = router;
