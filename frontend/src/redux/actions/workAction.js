b202a0420ceb097cd55e973cdadf361c2dcab03f
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-03 05:50:53 +0300

    work Action url created

--- frontend/src/redux/actions/workAction.js
+++ frontend/src/redux/actions/workAction.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { WORK_LOAD_REQUEST, WORK_LOAD_SUCCESS, WORK_LOAD_FAIL } from "../constants/workConstants";
 
+const url = "http://localhost:5000"
+
 export const taskLoadAction = (pageNumber, keyword = '', cat = '', location = '') => async(dispatch) => {
     dispatch({ type: WORK_LOAD_REQUEST })
 
     try {
-        const { data } = await axios.get(`/hin/tasks/show/?pageNumber=${pageNumber}&keyword=${keyword}&cat=${cat}&location=${location}`)
+        const { data } = await axios.get(`${url}/hin/tasks/show/?pageNumber=${pageNumber}&keyword=${keyword}&cat=${cat}&location=${location}`)
         dispatch({
             type: WORK_LOAD_SUCCESS,
             payload: data
