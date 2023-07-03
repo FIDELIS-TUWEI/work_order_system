5131551ea340aea6c24d01c6494d398279d3928d
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-03 17:17:52 +0300

    get all users route protected

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { updateUser, getAllUsers, singleUser } = require('../controllers/userController');
+const { isAuthenticated, isAdmin } = require('../middleware/authMiddleware');
 
-router.get("/all-users", getAllUsers);
+router.get("/all-users", isAuthenticated, isAdmin, getAllUsers);
 router.get("/user/:id", singleUser);
 router.put("/update-user/:id", updateUser);
 
