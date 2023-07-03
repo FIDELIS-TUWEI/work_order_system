31e049df089ec388fb106f53a97c9cc9c65f1609
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-03 05:48:40 +0300

    User TasksHistory

--- frontend/src/pages/user/UserTasksHistory.jsx
+++ frontend/src/pages/user/UserTasksHistory.jsx
@@ -1,9 +1,38 @@
+import { Box, Typography } from "@mui/material";
+import { useEffect } from "react";
+import { userProfileAction } from "../../redux/actions/userAction";
+import { useDispatch, useSelector } from "react-redux";
 
 
 const UserTasksHistory = () => {
+    const { user } = useSelector(state => state.userProfile);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(userProfileAction());
+    }, []);
+     
   return (
     <>
-        <h1>User TasksHistory</h1>
+        <Box>
+            <Typography variant="h4" sx={{ color: "#fafafa" }}> Tasks History </Typography>
+
+            <Box>
+                {
+                    user && user.tasksHistory.map((history, i) => (
+                        <CardElement
+                            key={i}
+                            id={history._id}
+                            taskTitle={history.title}
+                            description={history.description}
+                            category=""
+                            location={history.location}
+                        />
+                    ))
+                }
+            </Box>
+        </Box>
+        
     </>
   )
 }
