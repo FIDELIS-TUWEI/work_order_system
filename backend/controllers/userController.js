dafe99c4c52f09218882ac42eb3824b6608ab327
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-03 16:23:45 +0300

    Get all  users with pagination

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,52 +1,25 @@
 const User = require("../model/user");
 const asyncHandler = require("express-async-handler");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
-const createNewUser = asyncHandler (async (req, res, next) => {
-    try {
-        const { name, username, password } = req.body;
-
-        // Validate request
-        if (!name || !username || !password) {
-            return res.status(400).json({ message: "All fields are required" })
-        }
-
-        // check if user exists
-        const userExists = await User.findOne({ username });
-        if (userExists) {
-            return res.status(400).json({ message: "Username already exists" });
-        }
-
-        // Hash password
-        const hashedPswd = await bcrypt.hash(password, 10);
-
-        // Create new user
-        const user = await User.create({name, username, password: hashedPswd });
-
-        // Generate Token
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-            expiresIn: process.env.LOGIN_EXPIRES
-        });
-
-        // Return Token
-        res.status(201).json({
-            success: true,
-            user,
-            token,
-        });
-    } catch (error) {
-        next(error);
-    }
-});
 
 const getAllUsers = asyncHandler (async (req, res, next) => {
+    // Enable Pagination
+    const pageSize = 10;
+    const page = Number(req.query.pageNumber) || 1;
+    const count = await User.find({}).estimatedDocumentCount();
+
     try {
-        const users = await User.find({}).select("-password");
+        const users = await User.find({}).sort({ createdAt: -1 }).select("-password")
+            .skip(pageSize * (page -1))
+            .limit(pageSize)
         res.status(200).json({
             success: true,
-            data: users
+            data: users,
+            page,
+            pages: Math.ceil(count / pageSize),
+            count
         });
+        next();
     } catch (error) {
         next(error);
     }
@@ -89,7 +62,6 @@ const updateUser = asyncHandler (async (req, res, next) => {
 
 
 module.exports = {
-    createNewUser,
     getAllUsers,
     singleUser,
     updateUser,
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { createNewUser, updateUser, getAllUsers, singleUser } = require('../controllers/userController');
-const { login, logout } = require('../controllers/authController');
+const { updateUser, getAllUsers, singleUser } = require('../controllers/userController');
 
-router.post("/register", createNewUser);
-router.post("/login", login);
-router.post("/logout", logout);
 router.get("/all-users", getAllUsers);
 router.get("/user/:id", singleUser);
 router.put("/update-user/:id", updateUser);
