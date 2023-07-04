786ed25517f19b629cdd031e03bdb2bdea236a1b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-04 12:33:54 +0300

    all task type route

--- backend/routes/taskTypeRoutes.js
+++ backend/routes/taskTypeRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createTasktype } = require("../controllers/taskTypeController");
+const { createTasktype, allTaskType } = require("../controllers/taskTypeController");
 const { isAuthenticated } = require("../middleware/authMiddleware");
 
 router.post("/type/create", isAuthenticated, createTasktype);
+router.get("/type/tasks", allTaskType);
 
 module.exports = router;
