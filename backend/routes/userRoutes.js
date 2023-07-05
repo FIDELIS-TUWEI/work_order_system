835775002936c54e100bebce17eed525546ba545
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-05 20:17:09 +0300

    Task History route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, singleUser, editUser, deleteUser } = require('../controllers/userController');
+const { getAllUsers, singleUser, editUser, deleteUser, createTaskHistory } = require('../controllers/userController');
 const { isAuthenticated, isAdmin } = require('../middleware/authMiddleware');
 
 router.get("/all-users", isAuthenticated, isAdmin, getAllUsers);
 router.get("/user/:id",isAuthenticated, singleUser);
 router.put("/edit/:id",  editUser);
 router.delete("/admin/user/delete/:id", isAuthenticated, isAdmin, deleteUser);
+router.post("/user/taskHistory", isAuthenticated, createTaskHistory);
 
 
 module.exports = router;
