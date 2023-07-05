156cb58044d5b2ea161d97ec94d42112510af764
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-05 12:22:09 +0300

    Show taskType route update

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createTask, updateTask, deleteTask, getTasks, singleTask, showTasks } = require('../controllers/taskController');
+const { createTask, updateTask, deleteTask, getTasks, singleTask, showTaskType } = require('../controllers/taskController');
 const { isAuthenticated, isAdmin } = require('../middleware/authMiddleware');
 
 router.post("/task/create", isAuthenticated, isAdmin, createTask);
 router.get("/tasks/getall", getTasks);
 router.get("/task/:id", singleTask);
-router.get("/tasks/show", showTasks);
+router.get("/tasks/show", showTaskType);
 router.put("/task/update/:task_id", isAuthenticated, isAdmin, updateTask);
 router.delete("/tasks/delete/:id", deleteTask);
 
