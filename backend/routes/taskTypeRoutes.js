5a44d5a8e3a6ef5883df72a785307497c673e36d
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-05 17:17:10 +0300

    Update Task Type route

--- backend/routes/taskTypeRoutes.js
+++ backend/routes/taskTypeRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createTasktype, allTaskType } = require("../controllers/taskTypeController");
-const { isAuthenticated } = require("../middleware/authMiddleware");
+const { createTasktype, allTaskType, updateTaskType } = require("../controllers/taskTypeController");
+const { isAuthenticated, isAdmin } = require("../middleware/authMiddleware");
 
-router.post("/type/create", isAuthenticated, createTasktype);
+router.post("/type/create", isAuthenticated, isAdmin, createTasktype);
 router.get("/type/tasks", allTaskType);
+router.put("/type/update/:type_id", isAuthenticated, isAdmin, updateTaskType);
 
 module.exports = router;
