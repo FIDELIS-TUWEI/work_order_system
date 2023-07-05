308fdc507d02a5f0be1dcd264293132898c7c280
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-05 05:01:14 +0300

    updatetask by Id route update

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { createTask, updateTask, deleteTask, getTasks, singleTask } = require('../controllers/taskController');
-const { isAuthenticated } = require('../middleware/authMiddleware');
+const { isAuthenticated, isAdmin } = require('../middleware/authMiddleware');
 
-router.post("/task/create", isAuthenticated, createTask);
+router.post("/task/create", isAuthenticated, isAdmin, createTask);
 router.get("/tasks/getall", getTasks);
 router.get("/task/:id", singleTask);
-router.put("/task/update/:task_id", isAuthenticated, updateTask);
+router.put("/task/update/:task_id", isAuthenticated, isAdmin, updateTask);
 router.delete("/tasks/delete/:id", deleteTask);
 
 module.exports = router;
