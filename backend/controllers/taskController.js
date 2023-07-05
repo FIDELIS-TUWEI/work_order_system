d687c7dcbdb32a5cd2c783993dfd6b045a8280fb
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-05 04:56:34 +0300

    updatetask by Id controller update

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,4 +1,5 @@
 const Task = require("../model/task");
+const TaskType = require("../model/taskType");
 const asyncHandler = require("express-async-handler");
 
 // create Task logic
@@ -25,6 +26,41 @@ const createTask = asyncHandler (async (req, res, next) => {
     }
 });
 
+// get Task logic
+const singleTask = asyncHandler (async (req, res, next) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) {
+            return res.status(404).json({ message: `No task with ID: ${id} found` });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: {
+                task
+            }
+        })
+    } catch (error) {
+        next(error);
+    }
+});
+
+// update Task logic
+const updateTask = asyncHandler (async (req, res, next) => {
+    try {
+       const task = await Task.findByIdAndUpdate(req.params.task_id, req.body, {new: true, runValidators: true}).populate("taskType", "taskTypeName").populate("user", "name");
+       res.status(200).json({
+        success: true,
+        data: {
+            task
+        },
+        message: `Task updated succesfully `
+       }) 
+    } catch (error) {
+        next(error);
+    }
+});
+
 // get Tasks Logic
 const getTasks = asyncHandler (async (req, res, next) => {
     try {
@@ -34,23 +70,6 @@ const getTasks = asyncHandler (async (req, res, next) => {
         data: {
             tasks
         }
-       }) 
-    } catch (error) {
-        next(error);
-    }
-})
-
-// update Task logic
-const updateTask = asyncHandler (async (req, res, next) => {
-    try {
-       const {id} = req.params;
-       const task = await Task.findByIdAndUpdate({ _id: id }, req.body, {new: true, runValidators: true});
-       res.status(200).json({
-        success: true,
-        data: {
-            task
-        },
-        message: `Task updated succesfully `
        }) 
     } catch (error) {
         next(error);
@@ -69,25 +88,6 @@ const deleteTask = asyncHandler (async (req, res, next) => {
         res.status(200).json({
             success: true,
             message: "Task deleted"
-        })
-    } catch (error) {
-        next(error);
-    }
-});
-
-// get Task logic
-const singleTask = asyncHandler (async (req, res, next) => {
-    try {
-        const task = await Task.findById(req.params.id);
-        if (!task) {
-            return res.status(404).json({ message: `No task with ID: ${id} found` });
-        }
-
-        res.status(200).json({
-            success: true,
-            data: {
-                task
-            }
         })
     } catch (error) {
         next(error);
