575c84d7dea5b458df8f89d8271c47572b0d9e6c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-10 20:31:32 +0300

    Stat component

---
+++ frontend/src/components/StatComponent.jsx
@@ -0,0 +1,23 @@
+import { Card, CardContent, IconButton, Typography, useTheme } from "@mui/material";
+
+
+const StatComponent = ({ icon, description }) => {
+    const palette = useTheme();
+
+  return (
+    <>
+        <Card sx={{ bgcolor: palette.secondary.midNightBlue, width: "100%" }}>
+            <CardContent>
+                <IconButton sx={{ bgcolor: palette.primary.main, mb: 2 }}>
+                    {icon}
+                </IconButton>
+                <Typography variant="body2" sx={{ color: palette.primary.main, mb: 0 }}>
+                    {description}
+                </Typography>
+            </CardContent>
+        </Card>
+    </>
+  )
+}
+
+export default StatComponent
