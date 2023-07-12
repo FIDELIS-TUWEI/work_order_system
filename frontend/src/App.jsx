3071c07c80e6252d81bb46d138ae6f7dff82513a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-12 12:58:21 +0300

    frontend code refactor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,9 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { ProSidebarProvider } from "react-pro-sidebar";
 import LogIn from "./features/LogIn";
 import UserDashboard from "./pages/user/UserDashboard";
 import UserRoute from "./components/UserRoute";
-import Layout from "./pages/global/Layout";
 import UserTasksHistory from "./pages/user/UserTasksHistory";
 import UserInfoDashboard from "./pages/user/UserInfoDashboard";
 import DashCreateTask from "./pages/admin/tasks/DashCreateTask";
@@ -32,23 +30,17 @@ function App() {
         <ToastContainer />
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <ProSidebarProvider>
               <Routes>
                   <Route path="/" element={<Home />} />
                   <Route path="/dashboard" element={<Dashboard />} />
                   <Route path="/search/:keyword" element={<Home />} />
                   <Route path="/login" element={<LogIn />} />
-                  <Route path="/admin/dashboard" element={<AdminDashboard />} />
-                  <Route path="/user/dashboard" element={<UserRoute><UserDashboard /></UserRoute>} />
-                  <Route path="/user/tasks" element={<UserRoute><UserTasksHistory /></UserRoute>} />
-                  <Route path="/user/info" element={<UserRoute><UserInfoDashboard /></UserRoute>} />
                   <Route path="/tasks/create" element={<DashCreateTask />} />
                   <Route path="/tasks/list" element={<DashTasks />} />
                   <Route path="/users/create" element={<DashCreateUsers />} />
                   <Route path="/users/list" element={<DashUsers />} />
                   <Route path="*" element={<NotFound />} />
               </Routes>
-              </ProSidebarProvider>
         </ThemeProvider>
     </>
   )
