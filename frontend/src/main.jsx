07e352d9e3da34e0680ef49f87ff5fe73635dc2f
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-13 04:31:22 +0300

    Router provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App';
 
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './utils/redux/store';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <RouterProvider router={router} />
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
   </React.StrictMode>,
 )
