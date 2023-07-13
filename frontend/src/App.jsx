560a1aa469ea9274dfb1238ff975f2ce73146802
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-13 08:49:50 +0300

    frontend code refactor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,13 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import LogIn from "./features/LogIn";
+import Dashboard from "./pages/global/Dashboard";
+import NotFound from "./pages/NotFound";
+import Tasks from "./pages/admin/Tasks";
+import Users from "./pages/admin/Users";
 
 
 function App() {
@@ -10,7 +17,15 @@ function App() {
     <>
         <ToastContainer />
         <ThemeProvider theme={theme}>
-            <CssBaseline />  
+            <CssBaseline />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/login" element={<LogIn />} />
+              <Route path="/tasks/list" element={<Tasks />} />
+              <Route path="/users/list" element={<Users />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
         </ThemeProvider>
     </>
   )
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import App from "./App"
-import router from './routes';
-import {RouterProvider} from "react-router-dom"
 import { Provider } from 'react-redux';
 import store from './utils/redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </RouterProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
