58b48f55aa1afcdf90cfe5d131bd40d2b0fc1179
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-17 10:17:16 +0300

    edit user slice refactor

--- frontend/src/utils/redux/slice/userSlice.js
+++ frontend/src/utils/redux/slice/userSlice.js
@@ -7,34 +7,35 @@ const userSlice = createSlice({
         users: []
     },
     reducers: {
-        getUser : (state, action) => {
-            state.users = action.payload.data.map((user) => {
-                return { _id: user._id, 
-                    name: user.name, 
-                    username: user.username,
-                    role: user.role,
-                    active: user.active,
-                    date: user.date 
-                }
-            })
-        },
-        addUser : (state, action) => {
-            state.users.push(action.payload)
-        },
-editUser: (state, action) => {
-  const updatedUser = {
-    ...action.payload,
-  };
+getUser: (state, action) => {
+    const { data } = action.payload;
+    state.users = data.map(user => ({
+        _id: user._id,
+        name: user.name,
+        username: user.username,
+        role: user.role,
+        active: user.active,
+        date: user.date
+    }));
+},
+        addUser: (state, action) => ({
+        ...state,
+        users: [...state.users, action.payload],
+        }),
+        editUser: (state, action) => {
+        const updatedUser = {
+            ...action.payload,
+        };
 
-  const updatedUsers = state.users.map(user =>
-    user._id === action.payload._id ? updatedUser : user
-  );
+        const updatedUsers = state.users.map(user =>
+            user._id === action.payload._id ? updatedUser : user
+        );
 
-  return {
-    ...state,
-    users: updatedUsers,
-  };
-}
+        return {
+            ...state,
+            users: updatedUsers,
+        };
+        }
     }
 })
 
