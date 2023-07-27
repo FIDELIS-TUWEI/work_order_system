67d07090199711bb401977f26f841ee42bda9c8e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-27 12:08:43 +0300

    Protect middleware

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { createTask, updateTask, deleteTask, getTasks, singleTask } = require('../controllers/taskController');
-const { isAuthenticated, isAdmin } = require('../middleware/authMiddleware');
+const { protect, isAdmin } = require('../middleware/authMiddleware');
 
 
-router.post("/tasks/create", createTask);
-router.get("/tasks/getall",  getTasks);
-router.get("/task/:id", isAdmin, singleTask);
-router.put("/task/update/:task_id", isAdmin, updateTask);
-router.delete("/tasks/delete/:id", isAdmin, deleteTask);
+router.post("/tasks/create", protect, createTask);
+router.get("/tasks/getall", protect,  getTasks);
+router.get("/task/:id", protect, isAdmin, singleTask);
+router.put("/task/update/:task_id", protect, isAdmin, updateTask);
+router.delete("/tasks/delete/:id", protect, isAdmin, deleteTask);
 
 module.exports = router;
