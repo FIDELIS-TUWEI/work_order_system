51cf9230467f17f72b217470cc17d873058cd515
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-27 10:04:08 +0300

    code refactor

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,7 +6,5 @@ const { isAuthenticated, isAdmin } = require("../middleware/authMiddleware");
 router.post("/register", isAuthenticated, isAdmin, register);
 router.post("/login",  login);
 router.get("/logout", logout);
-router.get("/refresh", refresh);
-router.get("/:id/info", isAuthenticated, userProfile);
 
 module.exports = router;
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { createTask, updateTask, deleteTask, getTasks, singleTask } = require('../controllers/taskController');
 const { isAuthenticated, isAdmin } = require('../middleware/authMiddleware');
-//const verifyJWT = require('../middleware/verifyJWT');
 
-//router.use(verifyJWT);
 
 router.post("/tasks/create", createTask);
 router.get("/tasks/getall",  getTasks);
