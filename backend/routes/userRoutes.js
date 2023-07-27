38cfadee6f2a385566f755713d8b44568725e760
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-27 12:07:50 +0300

    Protect middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getAllUsers, singleUser, editUser, deleteUser} = require('../controllers/userController');
 const { protect, isAdmin } = require('../middleware/authMiddleware');
 
-router.get("/all-users", protect, isAdmin, getAllUsers);
+router.get("/all-users", protect,  getAllUsers);
 router.get("/user/:id", protect, isAdmin, singleUser);
 router.put("/edit/:id", protect, isAdmin,  editUser);
 router.delete("/admin/user/delete/:id", protect, isAdmin, deleteUser);
