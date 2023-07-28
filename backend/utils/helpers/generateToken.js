4374b246c11cd662c4321791e446190167ef6d3a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-28 09:59:46 +0300

    generate token and cookie

--- backend/utils/helpers/generateToken.js
+++ backend/utils/helpers/generateToken.js
@@ -1,17 +1,20 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET, NODE_ENV } = require("../env");
 
-const generateToken = (res, id) => {
-    const token = jwt.sign({ id }, JWT_SECRET, {
-        expiresIn: "30d",
+const generateToken = (res, userId) => {
+    const token = jwt.sign({ userId }, JWT_SECRET, {
+        expiresIn: "7d",
     });
 
     res.cookie("jwt", token, {
-        httpOnly: true,
+        httpOnly: true, // more secure
         secure: NODE_ENV !== "development", // Use secure cookies in production
         sameSite: 'strict', // Prevent CSRF attacks
-        expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
+        //expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     })
+
+    return token;
 }
 
 module.exports = {
