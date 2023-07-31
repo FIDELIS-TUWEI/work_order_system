1e54f9cccdff1b27f82d423fb45d206f57439408
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-31 12:13:51 +0300

    api slice config

--- frontend/src/utils/redux/store.js
+++ frontend/src/utils/redux/store.js
@@ -3,10 +3,12 @@ import authSlice from "./slices/authSlice";
 import userSlice from "./slices/userSlice";
 import taskSlice from "./slices/taskSlice";
 import loadingSlice from "./slices/loadingSlice";
+import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
     reducer: {
         auth: authSlice,
+        [apiSlice.reducerPath]: apiSlice.reducer,
         user: userSlice,
         task: taskSlice,
         loading: loadingSlice,
