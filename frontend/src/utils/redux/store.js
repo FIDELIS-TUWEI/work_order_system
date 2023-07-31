ce41bb3711814a22864c1aea984ea355e07fe911
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-31 05:01:24 +0300

    Redux Store configs refactor

--- frontend/src/utils/redux/store.js
+++ frontend/src/utils/redux/store.js
@@ -1,27 +1,18 @@
-import { configureStore, combineReducers, applyMiddleware } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "@redux-devtools/extension";
-import { userReducerLogout, userReducerSignIn } from "./reducers/userReducer";
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./slices/authSlice";
 import userSlice from "./slices/userSlice";
 import taskSlice from "./slices/taskSlice";
 import loadingSlice from "./slices/loadingSlice";
 
-// combine reducers
-const reducers = combineReducers({
-    loading: loadingSlice,
-    signIn: userReducerSignIn,
-    logOut: userReducerLogout,
-    users: userSlice,
-    tasks: taskSlice,
-});
-
-// Initial State
-let initialState = {
-    signIn: {
-        userInfo: localStorage.getItem("userInfo") ? JSON.parse(localStorage.getItem("userInfo")) : null
-    }
-};
-const middleware = [thunk];
-const store = configureStore({reducer:reducers}, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+const store = configureStore({
+    reducer: {
+        auth: authSlice,
+        user: userSlice,
+        task: taskSlice,
+        loading: loadingSlice,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+    devTools: true
+})
 
 export default store;
