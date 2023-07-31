75a9fe031b167b77c3e6b5b708eafaaf05c7997e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-07-31 04:26:44 +0300

    React router refactor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,8 @@
 import "./App.css";
-import { CssBaseline } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import LogIn from "./features/LogIn";
-import Dashboard from "./pages/global/Dashboard";
-import NotFound from "./pages/NotFound";
-import Tasks from "./pages/admin/Tasks";
-import Users from "./pages/admin/Users";
-import CreateUsers from "./pages/admin/CreateUsers";
-import CreateTask from "./pages/admin/CreateTask";
-import EditUser from "./pages/admin/EditUser";
-import EditTask from "./pages/admin/EditTask";
+import { Outlet } from "react-router-dom";
 
 import { useSelector } from "react-redux";
 import LoadingBox from "./components/LoadingBox";
@@ -28,18 +18,9 @@ function App() {
       {loading ? (
         <LoadingBox />
       ) : (
-        <Routes>
-          <Route path="/" element={ <Home /> } />
-          <Route path="/login" element={ <LogIn /> } />
-          <Route path="/dashboard" element={ <Dashboard /> } />
-          <Route path="/tasks/list" element={ <Tasks /> } />
-          <Route path="/users/list" element={ <Users /> } />
-          <Route path="/users/create" element={ <CreateUsers /> } />
-          <Route path="/tasks/create" element={ <CreateTask /> } />
-          <Route path="/users/edit/:id" element={ <EditUser /> } />
-          <Route path="/tasks/edit/:id" element={ <EditTask /> } />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Box sx={{ width: "100%", height: "100vh" }}>
+          <Outlet />
+        </Box>
       )}
     </>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,45 @@ import ReactDOM from 'react-dom/client';
 import App from "./App"
 import { Provider } from 'react-redux';
 import store from './utils/redux/store';
-import { BrowserRouter } from 'react-router-dom';
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  RouterProvider,  
+} from 'react-router-dom';
+import Home from './pages/Home';
+import LogIn from './features/LogIn';
+import Dashboard from './pages/global/Dashboard';
+import Tasks from './pages/admin/Tasks';
+import Users from './pages/admin/Users';
+import CreateUsers from './pages/admin/CreateUsers';
+import CreateTask from './pages/admin/CreateTask';
+import EditUser from './pages/admin/EditUser';
+import EditTask from './pages/admin/EditTask';
+import NotFound from './pages/NotFound';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} >
+      <Route index={true} path="/" element={ <Home /> } />
+      <Route path="/login" element={ <LogIn /> } />
+      <Route path="/dashboard" element={ <Dashboard /> } />
+      <Route path="/tasks/list" element={ <Tasks /> } />
+      <Route path="/users/list" element={ <Users /> } />
+      <Route path="/users/create" element={ <CreateUsers /> } />
+      <Route path="/tasks/create" element={ <CreateTask /> } />
+      <Route path="/users/edit/:id" element={ <EditUser /> } />
+      <Route path="/tasks/edit/:id" element={ <EditTask /> } />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+          <RouterProvider router={router} />
+    </React.StrictMode>,
+  </Provider>
+
 )
