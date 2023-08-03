41417925ab9948be8632771dd306493042dfbab5
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-03 16:07:01 +0300

    code refactor

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,11 @@
-import { Box
-} from "@mui/material";
-import Header from "../components/Header"
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer";
+import Layout from "../components/Layout"
 
-const Home = () => {
+const PrivateHome = () => {
   return (
-    <Box sx={{ bgcolor: "#fafafa", minHeight: "100vh" }}>
-      <Navbar />
-      <Header />
-      <Footer />
-    </Box>
-  );
-};
+    <Layout>
+      <h1>Private Home</h1>
+    </Layout>
+  )
+}
 
-export default Home;
+export default PrivateHome
