47552768f9558841d168123d15f5c97eed0fe0ca
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-03 08:28:58 +0300

    Header Update data update

--- frontend/src/Data/data.jsx
+++ frontend/src/Data/data.jsx
@@ -4,7 +4,51 @@ import {MdPeople} from "react-icons/md";
 import {TbReportAnalytics} from "react-icons/tb";
 import {AiOutlineLogout} from "react-icons/ai";
 
+// Users Menu
 export const SidebarMenu = [
+    {
+        name: "Home",
+        path: "/private",
+        icon: <AiOutlineHome />
+    },
+    {
+        name: "Work",
+        path: "/work/list",
+        icon: <MdOutlineWork />
+    },
+    {
+        name: "Logout",
+        path: "/logout",
+        icon: <AiOutlineLogout />
+    }
+]
+
+// HoD Menu
+export const hodMenu = [
+    {
+        name: "Home",
+        path: "/private",
+        icon: <AiOutlineHome />
+    },
+    {
+        name: "Work",
+        path: "/work/list",
+        icon: <MdOutlineWork />
+    },
+    {
+        name: "Analytics",
+        path: "/analytics",
+        icon: <TbReportAnalytics />
+    },
+    {
+        name: "Logout",
+        path: "/logout",
+        icon: <AiOutlineLogout />
+    }
+]
+
+// Admin Menu
+export const adminMenu = [
     {
         name: "Home",
         path: "/private",
