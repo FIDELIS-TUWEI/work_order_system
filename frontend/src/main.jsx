de1d617a3f74b0f63dd61c3df3b21f28db8e3037
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-03 05:09:11 +0300

    Code refactor

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,12 +12,6 @@ import {
 import Home from './pages/Home';
 import LogIn from './features/LogIn';
 import Dashboard from './pages/global/Dashboard';
-import Tasks from './pages/admin/Tasks';
-import Users from './pages/admin/Users';
-import CreateUsers from './pages/admin/CreateUsers';
-import CreateTask from './pages/admin/CreateTask';
-import EditUser from './pages/admin/EditUser';
-import EditTask from './pages/admin/EditTask';
 import NotFound from './pages/NotFound';
 import PublicRoute from './components/PublicRoute';
 import PrivateRoute from './components/PrivateRoute';
@@ -30,12 +24,6 @@ const router = createBrowserRouter(
       <Route path="/login" element={ <PublicRoute> <LogIn /> </PublicRoute>} />
       <Route path="/dashboard" element={ <PrivateRoute> <Dashboard /> </PrivateRoute> } />
       <Route path="/private" element={ <PrivateHome /> } />
-      <Route path="/tasks/list" element={ <Tasks /> } />
-      <Route path="/users/list" element={ <Users /> } />
-      <Route path="/users/create" element={ <CreateUsers /> } />
-      <Route path="/tasks/create" element={ <CreateTask /> } />
-      <Route path="/users/edit/:id" element={ <EditUser /> } />
-      <Route path="/tasks/edit/:id" element={ <EditTask /> } />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
