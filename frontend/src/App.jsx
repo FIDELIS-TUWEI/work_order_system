ec205c28f6ad33aa2796e6a433196f4cdb55bbd3
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-05 09:19:52 +0300

    code cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,24 +4,18 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Outlet } from "react-router-dom";
 
-import { useSelector } from "react-redux";
-import LoadingBox from "./components/LoadingBox";
+
 
 
 function App() {
-  const { loading } = useSelector(state => state.loading);
 
   return (
     <>
       <ToastContainer />
       <CssBaseline />
-      {loading ? (
-        <LoadingBox />
-      ) : (
         <Box sx={{ width: "100%", height: "100vh" }}>
           <Outlet />
         </Box>
-      )}
     </>
   );
 }
--- frontend/src/utils/redux/store.js
+++ frontend/src/utils/redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../redux/slices/authSlice";
-import loadingSlice from "../redux/slices/loadingSlice";
 import { apiSlice } from "../redux/slices/apiSlice";
 
 const store = configureStore({
     reducer: {
         auth: authSlice,
         [apiSlice.reducerPath]: apiSlice.reducer,
-        loading: loadingSlice,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true
