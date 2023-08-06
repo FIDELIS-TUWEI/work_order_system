d7b7d584093db0b9faa2fcddbb02eac10daedc72
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-06 10:43:30 +0300

    code cleanup

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const { getAllUsers, singleUser, editUser, deleteUser} = require('../controllers/userController');
 const { protect, restrict } = require('../middleware/authMiddleware');
 
-router.get("/all-users", protect, restrict('admin'), getAllUsers);
-router.get("/user/:id", protect, restrict('admin'), singleUser);
-router.put("/edit/:id", protect, restrict('admin'),  editUser);
-router.delete("/admin/user/delete/:id", protect, restrict('admin'), deleteUser);
+router.get("/all-users", protect, restrict(['admin']), getAllUsers);
+router.get("/user/:id", protect, restrict(['admin']), singleUser);
+router.put("/edit/:id", protect, restrict(['admin']),  editUser);
+router.delete("/admin/user/delete/:id", protect, restrict(['admin']), deleteUser);
 
 
 module.exports = router;
