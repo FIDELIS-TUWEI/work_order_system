4d4ad6ab476d8b561842212f69e56c516bf84ebc
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-07 12:39:52 +0300

    redux setup

--- frontend/src/utils/redux/slices/apiSlice.js
+++ frontend/src/utils/redux/slices/apiSlice.js
@@ -14,6 +14,6 @@ const baseQuery = fetchBaseQuery({
 
 export const apiSlice = createApi({
     baseQuery,
-    tagTypes: ["Users", "Tasks", "WorkOrders"],
+    tagTypes: ["Users"],
     endpoints: (builder) => ({}),
 })
