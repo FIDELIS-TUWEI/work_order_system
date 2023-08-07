7a8d9d8c8a0de1e24ec8f18a6c1790d05f3ba591
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-07 05:18:49 +0300

    authSlice refactor

--- frontend/src/utils/redux/slices/apiSlice.js
+++ frontend/src/utils/redux/slices/apiSlice.js
@@ -3,13 +3,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseQuery = fetchBaseQuery({ 
     baseUrl: "",
     credentials: "include",
-    prepareHeaders: (headers, { getState }) => {
-        const token = getState().auth.token;
-        if (token) {
-            headers.set("authorization", `Bearer ${token}`);
-        }
-        return headers;
-    } 
+    //prepareHeaders: (headers, { getState }) => {
+    //    const token = getState().auth.token;
+    //    if (token) {
+    //        headers.set("authorization", `Bearer ${token}`);
+    //    }
+    //    return headers;
+    //} 
 });
 
 export const apiSlice = createApi({
