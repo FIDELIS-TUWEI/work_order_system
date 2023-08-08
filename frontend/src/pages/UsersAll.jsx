4babc5ccffb8c2aab67d625c219801bc2b87cddd
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-08 16:01:25 +0300

    authSlice refactor

--- frontend/src/pages/UsersAll.jsx
+++ frontend/src/pages/UsersAll.jsx
@@ -1,28 +1,30 @@
 import { useEffect, useState } from "react";
 import Layout from "../components/Layout"
-import { useGetUsersQuery } from "../utils/redux/slices/usersApiSlice";
+//import { useGetUsersQuery } from "../utils/redux/slices/usersApiSlice";
 import { useSelector } from "react-redux";
 import { selectToken } from "../utils/redux/slices/authSlice";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 import { Table, Typography } from "antd";
-import LoadingBox from "../components/LoadingBox";
 import axios from "axios";
 
 const UsersAll = () => {
   const token = useSelector(selectToken);
   const [allUsers, setAllUsers] = useState([]);
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
 
   //const { data: users } = useGetUsersQuery();
 
 const getUsers = async () => {
   try {
+    setLoading(true);
     const response = await axios.get("http://localhost:5000/hin/all-users", {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
+    //setAllUsers([...allUsers, response.data]);
+    //setLoading(false);
     const data = response.data;
     setAllUsers(data);
   } catch (error) {
@@ -35,47 +37,31 @@ const getUsers = async () => {
   }, []);
 
 
-  const data = [allUsers.map((user) => {
-    return {
-      key: user._id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      username: user.username,
-    };
-  })]
+  //const data = [allUsers.map((user) => {
+  //  return {
+  //    key: user.id,
+  //    firstName: user.firstName,
+  //    lastName: user.lastName,
+  //    username: user.username,
+  //  };
+  //})]
   // antD table
 
   const columns = [
     {
-      title: "ID",
-      dataIndex: "_id",
-    },
-    {
-      title: "First Name",
-      dataIndex: "firstName",
-      align: "center",
-    },
-    {
-      title: "Last Name",
-      dataIndex: "lastName",
-      align: "center",
-    },
-    {
-      title: "Username",
-      dataIndex: "username",
-      align: "center",
-    },
-    {
-      title: "Action",
-      dataIndex: "action",
-      align: "center",
+      title: "Name",
+      dataIndex: "name",
+      render: (text, record) => {
+        <span>{record.firstName} {record.lastName}</span>
+      }
     }
   ]
 
   return (
     <Layout>
       <Typography>Users</Typography>
-      <Table columns={columns} dataSource={data} rowKey="_id" />
+      <Table dataSource={allUsers} columns={columns} rowKey={"id"} />
+      {loading && <p>Loading...</p>}
     </Layout>
   )
 }
