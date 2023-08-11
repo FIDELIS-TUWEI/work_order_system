e6395c375a2f89c9edaa0b0fbc06a88415fdf0b8
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-11 07:50:54 +0300

    backend refactor

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -13,31 +13,30 @@ const signToken = (id) => {
 // @desc Register User
 const signupUser = asyncHandler (async (req, res) => {
     try {
-        const { username } = req.body;
-        const user = await User.findOne({ $or: [{ username }] });
-
-        // Check for user in database
-        if (user) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-
-        // Create new user
-        const newUser = await User.create(req.body)
-
-        if (user) {
-            const {_id, firstName, lastName, role} = newUser;
-            res.status(201).json({
-                success: true,
-                message: "User created successfully",
-                _id,
-                firstName,
-                lastName,
-                role,
+        // check for existing user
+        const existingUser = await User.findOne({ username: req.body.username });
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exists",
             });
         }
+        // create new user
+        const user = await User.create(req.body);
+        res.status(201).json({
+            success: true,
+            message: "User created successfully",
+            firstName: user.firstName,
+            lastName: user.lastName,
+            username: user.username,
+            role: user.role
+        })
 
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
     }
 });
 
