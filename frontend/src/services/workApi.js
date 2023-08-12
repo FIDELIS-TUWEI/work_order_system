cc380abc252204d608672e82ef98f9951041f579
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-12 20:46:47 +0300

    new work order setup

--- frontend/src/services/workApi.js
+++ frontend/src/services/workApi.js
@@ -1,2 +1,12 @@
 import axios from 'axios';
 const USERS_URL = "/hin";
+
+// create new work
+export const createWorkOrder = async (values) => {
+    try {
+        const res = await axios.post(`${USERS_URL}/new/work`, values);
+        console.log(res);
+    } catch (error) {
+        console.log("Error while creating work", error);
+    }
+}
