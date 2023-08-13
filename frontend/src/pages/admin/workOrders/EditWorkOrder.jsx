3f530aac51942d8a14b57a11e5d0caddd54f8625
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-13 08:04:47 +0300

    edit work setup

---
+++ frontend/src/pages/admin/workOrders/EditWorkOrder.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Layout from '../../../components/Layout'
+import { Col, Form, Row, Typography } from 'antd'
+
+const EditWorkOrder = () => {
+  return (
+    <Layout>
+        <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>
+            Edit Work Order
+        </Typography>
+        <Form layout='vertical' style={{ margin: '18px' }}>
+            <Typography style={{ fontSize: '1rem', fontWeight: '500' }}>Work Details: </Typography>
+            <Row gutter={20}>
+                <Col xs={24} md={24} lg={8}></Col>
+                <Col xs={24} md={24} lg={8}></Col>
+                <Col xs={24} md={24} lg={8}></Col>
+                <Col xs={24} md={24} lg={8}></Col>
+            </Row>
+        </Form>
+    </Layout>
+  )
+}
+
+export default EditWorkOrder
