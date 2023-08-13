341bffa1096a38a8cc4ac7b6b3fead9cfbf20208
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-13 15:31:15 +0300

    app separation of concerns

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,49 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import App from "./App"
 import { Provider } from 'react-redux';
 import store from './utils/redux/store';
-import { 
-  createBrowserRouter, 
-  createRoutesFromElements, 
-  Route, 
-  RouterProvider,  
-} from 'react-router-dom';
-import Home from './pages/home/Home';
-import LogIn from './pages/LogIn';
-import NotFound from './pages/NotFound';
-import PublicRoute from './components/PublicRoute';
-import PrivateRoute from './components/PrivateRoute';
-import Dashboard from './pages/dashboard/Dashboard';
-import Profile from './pages/admin/users/Profile';
-import Analytics from './pages/admin/reports/Analytics';
-import Register from './pages/admin/users/Register';
-import UsersAll from './pages/admin/users/UsersAll';
-import EditUser from './pages/admin/users/EditUser';
-import AllWorkOrders from './pages/admin/workOrders/AllWorkOrders';
-import CreateWorkOrder from './pages/admin/workOrders/CreateWorkOrder';
-import EditWorkOrder from './pages/admin/workOrders/EditWorkOrder';
-import WorkDetails from './pages/admin/workOrders/workDetails';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/route';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />} >
-      <Route index={true} path="/" element={<PublicRoute> <Home /> </PublicRoute>} />
-      <Route path="/login" element={ <PublicRoute> <LogIn /> </PublicRoute>} />
-      <Route path="/private" element={ <PrivateRoute> <Dashboard /> </PrivateRoute> } />
-      <Route path="/work/list" element={ <PrivateRoute> <AllWorkOrders /> </PrivateRoute> } />
-      <Route path="/new/work" element={ <PrivateRoute> <CreateWorkOrder /> </PrivateRoute> } />
-      <Route path="/work/details/:id" element={ <PrivateRoute> <WorkDetails /> </PrivateRoute> } />
-      <Route path="/edit/work/:id" element={ <PrivateRoute> <EditWorkOrder /> </PrivateRoute> } />
-      <Route path="/users/all" element={ <PrivateRoute> <UsersAll/> </PrivateRoute> } />
-      <Route path="/users/register" element={ <PrivateRoute> <Register/> </PrivateRoute> } />
-      <Route path="/work/analytics" element={ <PrivateRoute> <Analytics /> </PrivateRoute> } />
-      <Route path="/profile" element={ <PrivateRoute> <Profile /> </PrivateRoute> } />
-      <Route path="/edit/user/:id" element={ <PrivateRoute> <EditUser /> </PrivateRoute> } />
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
---
+++ frontend/src/routes/route.jsx
@@ -0,0 +1,41 @@
+import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import App from "../App";
+
+import PrivateRoute from '../components/PrivateRoute';
+import PublicRoute from '../components/PublicRoute';
+import Home from '../pages/home/Home';
+import LogIn from '../pages/LogIn';
+import NotFound from '../pages/NotFound';
+import Dashboard from '../pages/dashboard/Dashboard';
+import AllWorkOrders from '../pages/admin/workOrders/AllWorkOrders';
+import CreateWorkOrder from '../pages/admin/workOrders/CreateWorkOrder';
+import WorkDetails from '../pages/admin/workOrders/workDetails';
+import EditWorkOrder from '../pages/admin/workOrders/EditWorkOrder';
+import Analytics from '../pages/admin/reports/Analytics';
+import Profile from '../pages/admin/users/Profile';
+import EditUser from '../pages/admin/users/EditUser';
+import UsersAll from '../pages/admin/users/UsersAll';
+import Register from '../pages/admin/users/Register';
+
+
+
+
+export const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<App />} >
+        <Route index={true} path="/" element={<PublicRoute> <Home /> </PublicRoute>} />
+        <Route path="/login" element={ <PublicRoute> <LogIn /> </PublicRoute>} />
+        <Route path="/private" element={ <PrivateRoute> <Dashboard /> </PrivateRoute> } />
+        <Route path="/work/list" element={ <PrivateRoute> <AllWorkOrders /> </PrivateRoute> } />
+        <Route path="/new/work" element={ <PrivateRoute> <CreateWorkOrder /> </PrivateRoute> } />
+        <Route path="/work/details/:id" element={ <PrivateRoute> <WorkDetails /> </PrivateRoute> } />
+        <Route path="/edit/work/:id" element={ <PrivateRoute> <EditWorkOrder /> </PrivateRoute> } />
+        <Route path="/users/all" element={ <PrivateRoute> <UsersAll/> </PrivateRoute> } />
+        <Route path="/users/register" element={ <PrivateRoute> <Register/> </PrivateRoute> } />
+        <Route path="/work/analytics" element={ <PrivateRoute> <Analytics /> </PrivateRoute> } />
+        <Route path="/profile" element={ <PrivateRoute> <Profile /> </PrivateRoute> } />
+        <Route path="/edit/user/:id" element={ <PrivateRoute> <EditUser /> </PrivateRoute> } />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    )
+  )
