a35a68824c7f2373a5bc1144209de2ccc87f1852
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-13 08:34:41 +0300

    work details setup

---
+++ frontend/src/pages/admin/workOrders/workDetails.jsx
@@ -0,0 +1,14 @@
+import { Typography } from "antd"
+import Layout from "../../../components/Layout"
+
+const workDetails = () => {
+  return (
+    <Layout>
+        <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>
+            Work Details
+        </Typography>
+    </Layout>
+  )
+}
+
+export default workDetails
