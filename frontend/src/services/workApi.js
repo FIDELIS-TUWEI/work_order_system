4dadfce3f8911b07174d71c1e05179e1346d1fd9
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-13 08:08:45 +0300

    Get all work api service config

--- frontend/src/services/workApi.js
+++ frontend/src/services/workApi.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
-const USERS_URL = "/hin";
+const WORK_URL = "/hin";
 
 // create new work
 export const createWorkOrder = async (values) => {
     try {
-        const res = await axios.post(`${USERS_URL}/create/work`, values);
+        const res = await axios.post(`${WORK_URL}/create/work`, values);
         console.log(res);
     } catch (error) {
         console.log("Error while creating work", error);
+    }
+}
+
+// Get all work Orders
+export const getAllWorkOrders = async () => {
+    try {
+        const res = await axios.get(`${WORK_URL}/getall/work`);
+        const data = res.data;
+        return data;
+    } catch (error) {
+        console.log("Error while fetching all work orders", error);
     }
 }
