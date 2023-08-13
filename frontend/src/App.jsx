5ae4001e1cfe741fb4ef411ed73a8b6396877211
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-13 19:49:37 +0300

    loading spinner styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,32 @@
 import "./App.css";
 import "antd/dist/reset.css"
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import LoadingBox from "./components/LoadingBox";
 
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000)
+  }, []);
 
   return (
     <>
-        <div>
+    { loading ? (
+      <LoadingBox loading={loading} />
+    ) : (
+      
+  
+        <div className="App">
           <Outlet />
         </div>
+
+    )}
     </>
   );
 }
