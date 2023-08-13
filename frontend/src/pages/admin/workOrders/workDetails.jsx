54a078743d0839b2205727f539b2e354f0670d98
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-13 14:27:42 +0300

    code refactor

--- frontend/src/pages/admin/workOrders/workDetails.jsx
+++ frontend/src/pages/admin/workOrders/workDetails.jsx
@@ -1,16 +1,14 @@
-import { Button, Card, Col, DatePicker, Form, Input, Row, Typography } from "antd"
+import { Button, Typography } from "antd"
 import Layout from "../../../components/Layout"
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import { useEffect, useState } from "react";
 import { selectToken } from "../../../utils/redux/slices/authSlice";
-import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import { getSingleWorkOrder } from "../../../services/workApi";
-const WORK_URL = "/hin";
 
 
 const WorkDetails = () => {
-    const [workDetails, setWorkDetails] = useState(null);
+    const [workDetails, setWorkDetails] = useState({});
     const token = useSelector(selectToken);
     const navigate = useNavigate();
     const {id} = useParams();
@@ -46,6 +44,7 @@ const WorkDetails = () => {
             <p>
                 Title: {workDetails && workDetails.title}
             </p>
+            <Button onClick={() => navigate(-1)}>Back</Button>
         </div>
     </Layout>
   )
