4eb93a1b74500ebc3b4b39a286a195902e153eae
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-14 05:08:27 +0300

    user rights to edit work order

--- frontend/src/pages/admin/workOrders/EditWorkOrder.jsx
+++ frontend/src/pages/admin/workOrders/EditWorkOrder.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import Layout from '../../../components/Layout'
-import { Col, DatePicker, Form, Input, Row, Select, Typography } from 'antd'
+import { Button, Col, DatePicker, Form, Input, Row, Select, Typography } from 'antd'
 import { useSelector } from 'react-redux'
 import { selectUserInfo } from '../../../utils/redux/slices/authSlice'
+import { useNavigate } from 'react-router-dom'
 
 const EditWorkOrder = () => {
   const user = useSelector(selectUserInfo);
+  const navigate = useNavigate();
+
+  // Function to handle form submit
+  const onFinishHandler = async (values) => {
+    console.log(values);
+  }
   
   return (
     <Layout>
         <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>
             Edit Work Order
         </Typography>
-        <Form layout='vertical' style={{ margin: '18px' }}>
-            <Typography style={{ fontSize: '1rem', fontWeight: '500' }}>Work Details: </Typography>
+        <Form onFinish={onFinishHandler} layout='vertical' style={{ margin: '18px' }}>
+            <Typography style={{ fontSize: '1rem', fontWeight: '500', marginBottom: '10px', textAlign: 'center' }}>Work Details: </Typography>
             <Row gutter={20}>
                 <Col xs={24} md={24} lg={8}>
                   <Form.Item
@@ -24,7 +31,10 @@ const EditWorkOrder = () => {
                   </Form.Item>
                 </Col>
                 <Col xs={24} md={24} lg={8}>
-                  <Form.Item>
+                  <Form.Item
+                    label="Status"
+                    name="status"
+                  >
                     <Select 
                       placeholder='Select Status'
                       allowClear
@@ -52,46 +62,57 @@ const EditWorkOrder = () => {
                     <Input type='text' placeholder='Enter comments' />
                   </Form.Item>
                 </Col>
-                <Col xs={24} md={24} lg={8}>
-                  <Form.Item
-                    label="Reviewed"
-                    name="reviewed"
-                  >
-                    <Select 
-                      placeholder='Select Review Status'
-                      allowClear
-                      style={{ width: '100%' }}
-                      options={[
-                        { value: true, label: 'Reviewed' }, { value: false, label: 'Not Reviewed' }
-                      ]}
-                    />
-                  </Form.Item>
-                </Col>
-                <Col xs={24} md={24} lg={8}>
-                  <Form.Item
-                    label="Reviewed By"
-                    name="reviewedBy"
-                  >
-                    <Input type='text' placeholder='Enter employee name to assign' />
-                  </Form.Item>
-                </Col>
-                <Col xs={24} md={24} lg={8}>
-                  <Form.Item
-                    label="Date Reviewed"
-                    name="dateReviewed"
-                  >
-                    <DatePicker format='YYYY-MM-DD' />
-                  </Form.Item>
-                </Col>
-                <Col xs={24} md={24} lg={8}>
-                  <Form.Item
-                    label="Review Comments"
-                    name="reviewComments"
-                  >
-                    <Input type='text' placeholder='Enter comments' />
-                  </Form.Item>
-                </Col>
+                {
+                  user && user.role === 'admin' || user && user.role === 'reviewer' ? (
+                    <>
+                      <Col xs={24} md={24} lg={8}>
+                        <Form.Item
+                          label="Reviewed"
+                          name="reviewed"
+                        >
+                          <Select 
+                            placeholder='Select Review Status'
+                            allowClear
+                            style={{ width: '100%' }}
+                            options={[
+                              { value: true, label: 'Reviewed' }, { value: false, label: 'Not Reviewed' }
+                            ]}
+                          />
+                        </Form.Item>
+                      </Col>
+                      <Col xs={24} md={24} lg={8}>
+                        <Form.Item
+                          label="Reviewed By"
+                          name="reviewedBy"
+                        >
+                          <Input type='text' placeholder='Enter employee name to assign' />
+                        </Form.Item>
+                      </Col>
+                      <Col xs={24} md={24} lg={8}>
+                        <Form.Item
+                          label="Review Comments"
+                          name="reviewComments"
+                        >
+                          <Input type='text' placeholder='Enter comments' />
+                        </Form.Item>
+                      </Col>
+                      <Col xs={24} md={24} lg={8}>
+                        <Form.Item
+                          label="Date Reviewed"
+                          name="dateReviewed"
+                        >
+                          <DatePicker format='YYYY-MM-DD' />
+                        </Form.Item>
+                      </Col>
+                    </>
+                  ) : ''}
             </Row>
+            <div>
+              <Button type="primary" htmlType="submit" onClick={() => {navigate(-1)}}>Go Back</Button>
+            </div>
+            <div className="user_submit">
+              <Button type="primary" htmlType="submit">Update</Button>
+            </div>
         </Form>
     </Layout>
   )
