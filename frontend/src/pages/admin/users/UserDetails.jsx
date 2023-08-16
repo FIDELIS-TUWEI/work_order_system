d4069df3d043f97c293f225abb7c76c754dfcfa6
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-16 09:12:23 +0300

    user details page complete

--- frontend/src/pages/admin/users/UserDetails.jsx
+++ frontend/src/pages/admin/users/UserDetails.jsx
@@ -1,8 +1,57 @@
+import { Button, Card } from 'antd';
 import Layout from '../../../components/Layout';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getUserInfo } from '../../../services/usersApi';
+import { useSelector } from 'react-redux';
+import { selectToken } from '../../../utils/redux/slices/authSlice';
 
 const UserDetails = () => {
+  const token = useSelector(selectToken);
+  const [userDetails, setUserDetails] = useState([]);
+  const navigate = useNavigate();
+  const {id} = useParams();
+
+  useEffect(() => {
+    if (id) {
+      getUserDetails(id);
+    }
+  }, [id]);
+
+  // function to get user details
+  const getUserDetails = async (id) => {
+    const res = await getUserInfo(id, {
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    setUserDetails({...res.data});
+  }
+
   return (
-    <Layout>UserDetails</Layout>
+    <Layout>
+      <Card title="User Details" style={{ margin: 'auto', width: '300px' }}>
+        <p>User Id: {id}</p>
+        <p>
+          First Name: {userDetails && userDetails.firstName}
+        </p>
+        <p>
+          Last Name: {userDetails && userDetails.lastName}
+        </p>
+        <p>
+          Username: {userDetails && userDetails.username}
+        </p>
+        <p>
+          Role: {userDetails && userDetails.role}
+        </p>
+        <p>
+          Status: {userDetails && userDetails.active === true ? "Active" : "Not Active"}
+        </p>
+
+        <Button onClick={() => {navigate(-1)}}>Back</Button>
+      </Card>
+    </Layout>
   )
 }
 
