f190462c933228461d1eafb443e7520f226747a5
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-16 04:29:10 +0300

    all users code refactor

--- frontend/src/pages/admin/users/UsersAll.jsx
+++ frontend/src/pages/admin/users/UsersAll.jsx
@@ -3,7 +3,7 @@ import Layout from "../../../components/Layout";
 import { useSelector } from "react-redux";
 import { selectToken, selectUserInfo } from "../../../utils/redux/slices/authSlice";
 import { useNavigate } from "react-router-dom";
-import { Table, Typography, Button } from "antd";
+import { Typography, Button } from "antd";
 import { getAllUsers } from "../../../services/usersApi";
 
 
@@ -32,40 +32,6 @@ const getUsers = async () => {
   setAllUsers(response.data);
   setLoading(false);
 };
-
-
-  // antD table
-
-  const columns = [
-    {
-      title: "First Name",
-      dataIndex: "firstName",
-    },
-    {
-      title: "Last Name",
-      dataIndex: "lastName",
-    },
-    {
-      title: "Username",
-      dataIndex: "username",
-    },
-    {
-      title: "Role",
-      dataIndex: "role",
-    },
-    {
-      title: "Actions",
-      dataIndex: "actions",
-      render: () => {
-        return (
-          <div className="actions__btn">
-            <Button onClick={() => {navigate(`/edit/user/${user._id}`)}}>Edit</Button>
-            <Button danger>Delete</Button>
-          </div>
-        );
-      }
-    }
-  ]
 
   return (
     <Layout>
