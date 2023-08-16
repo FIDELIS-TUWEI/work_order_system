04d6fffcfe30022b6b4bb8c7ac873c77de9a66f2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-16 05:05:01 +0300

    Edit user frontend logic

--- frontend/src/pages/admin/users/UsersAll.jsx
+++ frontend/src/pages/admin/users/UsersAll.jsx
@@ -59,7 +59,7 @@ const getUsers = async () => {
               <td>{user.role}</td>
               <td className="actions__btn">
                 <Button onClick={() => {navigate(`/edit/user/${user._id}`)}}>Edit</Button>
-                <Button danger>Delete</Button>
+                <Button danger>View Details</Button>
               </td>
             </tr>
           ))}
