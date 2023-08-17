280a679acbabc411bc7a53517175df78e459bcd2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-17 19:34:27 +0300

    create locaion logic

--- backend/controllers/locationController.js
+++ backend/controllers/locationController.js
@@ -26,6 +26,29 @@ const createLocation = asyncHandler(async (req, res) => {
     }
 });
 
+// @desc Get all locations
+// @route GET /all-locations
+// @access Private
+const getAllLocations = asyncHandler(async (req, res) => {
+    try {
+        const locations = await Location.find({});
+
+        if (!locations) {
+            return res.status(400).json({ message: "No locations found" });
+        }
+        res.status(200).json({
+            success: true,
+            data: locations
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+})
+
 module.exports = {
-    createLocation
+    createLocation,
+    getAllLocations
 }
