08c6f2b688c377cb4f80f53fe379abb9357f39db
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-17 19:40:37 +0300

    location routes

---
+++ backend/routes/locationRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { createLocation, getAllLocations } = require('../controllers/locationController');
+const { protect, restrict } = require('../middleware/authMiddleware');
+
+
+router.post("/create/location", protect, restrict(["admin", "superadmin"]), createLocation);
+router.get("/all-locations", protect, restrict(["admin", "superadmin"]), getAllLocations);
+
+module.exports = router;
