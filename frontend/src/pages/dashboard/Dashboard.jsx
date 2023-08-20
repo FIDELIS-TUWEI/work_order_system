fd8d97e9e2bafa546e9aa3db484135373c40b473
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-20 13:13:47 +0300

    Dashboard details

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,22 @@
+import { useSelector } from "react-redux";
 import Layout from "../../components/Layout"
+import { Card, Typography } from "antd";
+import { selectUserInfo } from "../../utils/redux/slices/authSlice";
 
 const Dashboard = () => {
+  const user = useSelector(selectUserInfo);
+
   return (
     <Layout>
-      <h1>Dashboard</h1>
+      <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>
+          Welcome {user && user.firstName}, {user && user.lastName}
+        </Typography>
+      <Card style={{ margin: "auto", width: "300px" }}>
+        <Typography style={{ textAlign: 'center', fontSize: '1rem', fontWeight: 'bold' }}>
+          The ultimate Work Order Management System
+        </Typography>
+      </Card>
+      
     </Layout>
   )
 }
