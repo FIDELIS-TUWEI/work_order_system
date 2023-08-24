1eafde92dab4254fc02b2344b332506d9a2be5a5
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-24 05:05:03 +0300

    reports frontend setup

--- frontend/src/services/reportsApi.js
+++ frontend/src/services/reportsApi.js
@@ -1,2 +1,24 @@
 import axios from 'axios';
 const WORK_URL = "/hin";
+
+// Get pending work Orders
+export const pendingWorkOrders = async (page) => {
+    try {
+        const res = await axios.get(`${WORK_URL}/pending/work?page=${page}`);
+        const data = res.data;
+        return data;
+    } catch (error) {
+        console.log("Error while fetching all pending work orders", error);
+    }
+};
+
+// Get completed work Orders
+export const completedWorkOrders = async (page) => {
+    try {
+        const res = await axios.get(`${WORK_URL}/completed/work?page=${page}`);
+        const data = res.data;
+        return data;
+    } catch (error) {
+        console.log("Error while fetching all completed work orders", error);
+    }
+};
