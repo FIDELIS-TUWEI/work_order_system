bb73bb3698d0171044e9ffe329097c13e1c1fcc2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-24 05:18:02 +0300

    report controller

--- backend/controllers/report.js
+++ backend/controllers/report.js
@@ -4,14 +4,20 @@ const asyncHandler = require("express-async-handler");
 
 // Pending Work Order
 const pendingWorkOrder = asyncHandler (async (req, res, next) => {
+    const pageSize = 5;
+    const page = Number(req.query.pageNumber) || 1;
+    const count = await WorkOrder.find({status: "Pending"}).estimatedDocumentCount();
     try {
-        const workOrder = await WorkOrder.find({status: "Pending"}).populate().select("-completedWork");
-        if (!workOrder) {
+        const pending = await WorkOrder.find({status: "Pending"}).populate().select("-completedWork");
+        if (!pending) {
             return next(new ErrorResponse("Work Order not found", 404));
         }
         return res.status(200).json({
             success: true,
-            data: workOrder
+            data: pending,
+            page,
+            pages: Math.ceil(count / pageSize),
+            count
         })
     } catch (error) {
         return next(new ErrorResponse(error.message, 500));
@@ -19,8 +25,33 @@ const pendingWorkOrder = asyncHandler (async (req, res, next) => {
         
 });
 
+// In Progress Work Order
+const inProgressWorkOrder = asyncHandler (async (req, res, next) => {
+    const pageSize = 5;
+    const page = Number(req.query.pageNumber) || 1;
+    const count = await WorkOrder.find({status: "In_Progress"}).estimatedDocumentCount();
+    try {
+        const inProgress = await WorkOrder.find({status: "In_Progress"}).populate().select("-completedWork");
+        if (!inProgress) {
+            return next(new ErrorResponse("Work Order not found", 404));
+        }
+        return res.status(200).json({
+            success: true,
+            data: inProgress,
+            page,
+            pages: Math.ceil(count / pageSize),
+            count
+        })
+    } catch (error) {
+        return next(new ErrorResponse(error.message, 500));
+    }
+});
+
 
 const completedWorkOrder = asyncHandler (async (req, res, next) => {
+    const pageSize = 5;
+    const page = Number(req.query.pageNumber) || 1;
+    const count = await WorkOrder.find({status: "Complete"}).estimatedDocumentCount();
     try {
         const workOrder = await WorkOrder.find({status: "Complete"}).populate().select("-completedWork");
         if (!workOrder) {
@@ -28,7 +59,31 @@ const completedWorkOrder = asyncHandler (async (req, res, next) => {
         }
         return res.status(200).json({
             success: true,
-            data: workOrder
+            data: workOrder,
+            page,
+            pages: Math.ceil(count / pageSize),
+            count
+        })
+    } catch (error) {
+        return next(new ErrorResponse(error.message, 500));
+    }
+});
+
+const reviewWorkOrder = asyncHandler (async (req, res, next) => {
+    const pageSize = 5;
+    const page = Number(req.query.pageNumber) || 1;
+    const count = await WorkOrder.find({status: "Reviewed"}).estimatedDocumentCount();
+    try {
+        const review = await WorkOrder.find({status: "Reviewed"}).populate().select("-completedWork");
+        if (!review) {
+            return next(new ErrorResponse("Work Order not found", 404));
+        }
+        return res.status(200).json({
+            success: true,
+            data: review,
+            page,
+            pages: Math.ceil(count / pageSize),
+            count
         })
     } catch (error) {
         return next(new ErrorResponse(error.message, 500));
@@ -37,5 +92,7 @@ const completedWorkOrder = asyncHandler (async (req, res, next) => {
 
 module.exports = {
     pendingWorkOrder,
-    completedWorkOrder
+    inProgressWorkOrder,
+    completedWorkOrder,
+    reviewWorkOrder
 }
