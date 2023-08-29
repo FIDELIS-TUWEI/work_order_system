b1034c60cdcb766cf9ac18eb24bb43abd824c029
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-08-29 04:44:51 +0300

    backend status filter pdf generation router

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { completedWorkOrder, pendingWorkOrder, inProgressWorkOrder, reviewedWorkOrder } = require('../controllers/report');
 const { protect, restrict } = require('../middleware/authMiddleware');
+const { printWorkOrder, generatePDF } = require('../utils/helpers/pdf');
 
 router.get("/pending/work", protect, restrict(["admin", "hod", "supervisor", "superadmin"]), pendingWorkOrder);
 router.get("/inprogress/work", protect, restrict(["admin", "hod", "supervisor", "superadmin"]), inProgressWorkOrder);
 router.get("/work/complete", protect, restrict(["admin", "hod", "supervisor", "superadmin"]), completedWorkOrder);
 router.get("/work/review", protect, restrict(["admin", "hod", "supervisor", "superadmin"]), reviewedWorkOrder);
+router.get("/workorders/:status", protect, restrict(["admin", "hod", "supervisor", "superadmin"]), printWorkOrder);
+router.get("/generate-Pdf/:status", protect, restrict(["admin", "hod", "supervisor", "superadmin"]), generatePDF);
 
 
 module.exports = router;
