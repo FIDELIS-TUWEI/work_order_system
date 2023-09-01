fee5d8fcdfc65b187bc4f934aba1eb73ad123c6e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-01 21:22:33 +0300

    categories controller

---
+++ backend/controllers/categoryController.js
@@ -0,0 +1,54 @@
+const Category = require("../model/category");
+const asyncHandler = require("express-async-handler");
+
+
+// Create category
+const createCategory = asyncHandler(async (req, res) => {
+    try {
+        const duplicate = await Category.findOne({ categoryTitle: req.body.categoryTitle });
+        if (duplicate) {
+            return res.status(400).json({
+                success: false,
+                message: "Category already exists",
+            });
+        }
+
+        // create new category
+        const newCategory = new Category(req.body);
+        await newCategory.save();
+        res.status(201).json({
+            success: true,
+            data: newCategory
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+});
+
+const getAllCategories = asyncHandler(async (req, res) => {
+    try {
+        const categories = await Category.find({});
+
+        if (!categories) {
+            return res.status(400).json({ message: "No categories found" });
+        }
+        
+        res.status(200).json({
+            success: true,
+            data: categories
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+})
+
+module.exports = {
+    createCategory,
+    getAllCategories
+}
