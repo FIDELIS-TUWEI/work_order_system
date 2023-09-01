6a33337770e761a15692ed26aea5c542a4448109
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-01 21:37:13 +0300

    categories route

---
+++ backend/routes/categoryRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const { createCategory } = require("../controllers/categoryController");
+const { protect, restrict } = require("../middleware/authMiddleware");
+
+router.post("/new/category", protect, restrict(["admin", "superadmin"]), createCategory);
+router.get("/all/categories", protect, restrict(["admin", "superadmin", "hod", "reviewer", "supervisor", "user"]), getAllCategories);
+
+module.exports = router;
