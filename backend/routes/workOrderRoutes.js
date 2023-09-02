7c07ca58d33e5e107b66d2cde531e1ee69e41f6c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-02 05:53:57 +0300

    user roles update

--- backend/routes/workOrderRoutes.js
+++ backend/routes/workOrderRoutes.js
@@ -4,10 +4,10 @@ const { createWorkOrder, updateWorkOrder, getAllWorkOrders, getSingleWorkOrder, 
 const { protect,  restrict} = require('../middleware/authMiddleware');
 
 
-router.post("/create/work", protect, restrict(["admin", "hod", "user", "supervisor", "superadmin"]), createWorkOrder);
-router.get("/getall/work", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer"]), getAllWorkOrders);
-router.get("/single/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "hod", "reviewer"]), getSingleWorkOrder);
-router.put("/update/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "hod", "reviewer"]), updateWorkOrder);
+router.post("/create/work", protect, restrict(["admin", "hod", "user", "supervisor", "superadmin", "reviewer", "engineer"]), createWorkOrder);
+router.get("/getall/work", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer"]), getAllWorkOrders);
+router.get("/single/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "hod", "reviewer", "engineer"]), getSingleWorkOrder);
+router.put("/update/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "hod", "reviewer", "engineer"]), updateWorkOrder);
 router.delete("/delete/work/:id", protect, restrict(["admin"]), deleteWorkOrder);
 
 module.exports = router;
