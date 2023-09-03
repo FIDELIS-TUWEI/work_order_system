50cfdf2909d002655ca6601073baf9f31963f011
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-03 21:18:38 +0300

    frontend changes

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <h1>Home</h1>
       <Link to="/login">Login</Link>
-    </>
+    </div>
   )
 }
 
