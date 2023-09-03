0bb5c3d49193147b8720fc5d033b4c4fa32d8304
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-03 21:21:23 +0300

    frontend changes

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
+import { Card } from "antd";
 import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <div className="home">
+    <Card style={{ margin: "auto", width: "200px", display: "flex", justifyContent: "center" }}>
       <h1>Home</h1>
       <Link to="/login">Login</Link>
-    </div>
+    </Card>
   )
 }
 
