d36327c11e7c208602fb96ec0837abe40f04e0ef
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-04 14:26:06 +0300

    line chart package install

--- frontend/src/pages/admin/reports/BarGraph.jsx
+++ frontend/src/pages/admin/reports/BarGraph.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { selectToken } from "../../../utils/redux/slices/authSlice";
 import { getAllWorkOrders } from "../../../services/workApi";
 import { Card } from "antd";
+import { LineChart, Line, ResponsiveContainer, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const BarGraph = () => {
     const token = useSelector(selectToken);
@@ -29,9 +30,21 @@ const BarGraph = () => {
   return (
     <div>
         <Card title="Work Orders Assigned To Employees" style={{ margin: "15px 25px" }} loading={loading}>
+            <ResponsiveContainer width="100%" aspect={3}>
+                <LineChart width={500} height={300} data={workOrders}>
+                    <CartesianGrid />
+                    <XAxis dataKey="assignedTo" interval={'preserveStartEnd'} />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line dataKey="title" stroke="black" activeDot={{ r: 8 }} />
+                    <Line dataKey="serviceType" stroke="red" activeDot={{ r: 8 }} />
+                    <Line dataKey="status" stroke="green" activeDot={{ r: 8 }} />
+                </LineChart>
+            </ResponsiveContainer>
         </Card>
     </div>
   )
 }
 
-export default BarGraph
+export default BarGraph;
