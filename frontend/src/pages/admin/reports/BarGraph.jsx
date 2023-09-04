d3e8ab587a3a7f410d7b20be4716b4e14190d6c0
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-04 10:55:04 +0300

    Chartjs Configuration

--- frontend/src/pages/admin/reports/BarGraph.jsx
+++ frontend/src/pages/admin/reports/BarGraph.jsx
@@ -1,10 +1,8 @@
 import {useState, useEffect} from "react";
-import { Bar } from "react-chartjs-2";
 import { useSelector } from "react-redux";
 import { selectToken } from "../../../utils/redux/slices/authSlice";
 import { getAllWorkOrders } from "../../../services/workApi";
 import { Typography } from "antd";
-import { Chart } from "chart.js";
 
 const BarGraph = () => {
     const token = useSelector(selectToken);
@@ -24,16 +22,17 @@ const BarGraph = () => {
 
     useEffect(() => {
         allWork();
-
-        // Process the data to create the chart dataset
-        
     });
+
+    // callback to store the chart instance
+    const handleChartInstance = (instance) => {
+        setChartInstance(instance);
+    }
 
   return (
     <div>
         <Typography>Work Orders Assigned To Employees</Typography>
-        <div style={{ width: "80%", margin: "0 auto" }}>
-        </div>
+        
     </div>
   )
 }
