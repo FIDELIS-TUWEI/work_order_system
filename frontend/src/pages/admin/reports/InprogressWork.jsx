78ca235d174f19a4c34feb2b4024a54d55b0b696
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-04 16:55:51 +0300

    filter work by status success

--- frontend/src/pages/admin/reports/InprogressWork.jsx
+++ frontend/src/pages/admin/reports/InprogressWork.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 import moment from "moment";
-import { Card, Typography } from "antd";
+import { Button, Card, Typography, message } from "antd";
 import { useEffect, useState } from "react";
+import { jsPDF } from "jspdf";
+import "jspdf-autotable";
+import Logo from "../../../assets/logo.png";
 import LoadingBox from "../../../components/LoadingBox";
 
 const WORK_URL = "/hin";
@@ -28,6 +31,40 @@ const InprogressWork = () => {
 
     getWorkOrders();
   }, [filterStatus]);
+
+  // Function to generate and export pending work orders
+  const exportPDF = async () => {
+    const doc = new jsPDF({ orientation: "landscape" });
+
+    // Calculate the center position for the logo
+    const centerX = doc.internal.pageSize.width / 2;
+
+    // Add company logo centered
+    const imgData = Logo;
+    const logoWidth = 30;
+    const logoHeight = 30;
+    const logoX = centerX - logoWidth / 2;
+    doc.addImage(imgData, "PNG", logoX, 10, logoWidth, logoHeight);
+
+    // Add report generation date
+    const reportDate = moment().format("DD/MM/YYYY, hh:mm a");
+    doc.setFontSize(6);
+    doc.text(`${reportDate}`, 10, 20);
+
+    const initialTableY = 50;
+
+    doc.setFontSize(12);
+    doc.text("Work Orders Report", 15, initialTableY - 10);
+
+    doc.autoTable({
+      html: "#table",
+      startY: initialTableY,
+    });
+
+    doc.save("Work Orders.pdf");
+    message.success("Report Generated Successfully"); 
+  }
+
   return (
     <Card>
       <Typography>Pending Work Orders</Typography>
@@ -47,28 +84,29 @@ const InprogressWork = () => {
       ) : (
         <>
 
-          <table>
+          <table id="table">
             <thead>
               <tr>
-                <th>Assigned To</th>
                 <th>Title</th>
                 <th>Service Type</th>
                 <th>Status</th>
+                <th>Assigned To</th>
                 <th>Date Requested</th>
               </tr>
             </thead>
             <tbody>
               {workOrders.map((workOrder) => (
                 <tr key={workOrder._id}>
-                  <td>{workOrder.assignedTo}</td>
                   <td>{workOrder.title}</td>
                   <td>{workOrder.serviceType}</td>
                   <td>{workOrder.status}</td>
+                  <td>{workOrder.assignedTo}</td>
                   <td>{moment(workOrder.Date_Created).format("DD/MM/YYYY, hh:mm a")}</td>
                 </tr>
               ))}
             </tbody>
           </table>
+          <Button style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }} onClick={exportPDF}>Generate Report</Button>
         </>
         )}
     </Card>
