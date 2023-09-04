4e48bbf7cc8622b479ddf318366d3f5c7149ffb0
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-04 04:50:57 +0300

    Chartjs Configuration

--- frontend/src/pages/admin/reports/Analytics.jsx
+++ frontend/src/pages/admin/reports/Analytics.jsx
@@ -1,9 +1,11 @@
 import Layout from "../../../components/Layout"
+import BarGraph from "./BarGraph"
 
 const Analytics = () => {
   return (
     <Layout>
       <h1>Analytics</h1>
+      <BarGraph />
     </Layout>
   )
 }
