b9f7d5513dc596b4dffe9f8d7242e0908fca1448
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-04 14:07:08 +0300

    line chart package install

--- frontend/src/pages/admin/reports/BarGraph.jsx
+++ frontend/src/pages/admin/reports/BarGraph.jsx
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 import { selectToken } from "../../../utils/redux/slices/authSlice";
 import { getAllWorkOrders } from "../../../services/workApi";
 import { Card } from "antd";
-import { Bar, Line } from "react-chartjs-2";
 
 const BarGraph = () => {
     const token = useSelector(selectToken);
@@ -27,41 +26,9 @@ const BarGraph = () => {
         setLoading(false);
     }
 
-    // Extract data for the bar graph
-    const employeeWorkCounts = {};
-    workOrders.forEach(workOrder => {
-        const assignedTo = workOrder.assignedTo;
-
-        if (employeeWorkCounts[assignedTo]) {
-            employeeWorkCounts[assignedTo] = 1;
-        } else {
-            employeeWorkCounts[assignedTo] ++ ;
-        }
-    });
-
-    // Prepare data for the bar graph
-    const employeeNames = Object.keys(employeeWorkCounts);
-    const workCounts = Object.values(employeeWorkCounts);
-
-    const data = {
-        labels: employeeNames,
-        datasets: [{
-            label: "Work Orders",
-            data: workCounts,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-            ],
-            borderWidth: 1
-        }]
-    }
-
   return (
     <div>
         <Card title="Work Orders Assigned To Employees" style={{ margin: "15px 25px" }} loading={loading}>
-            <Line data={data} />
         </Card>
     </div>
   )
