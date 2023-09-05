bcb537897025e87f62ac4e72c16719a6599b6214
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-05 08:26:40 +0300

    query to generate report for work orders weekly and daily

--- backend/controllers/report.js
+++ backend/controllers/report.js
@@ -16,9 +16,48 @@ const filterWorkStatus = asyncHandler (async (req, res, next) => {
     } catch (error) {
         return next(new ErrorResponse(error.message, 500));
     }
-})
+});
+
+// Query to filter work orders by date
+const dailyWorkOrders = asyncHandler (async (req, res, next) => {
+    try {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        const dailyWork = await WorkOrder.find({
+            Date_Created: {$gte: today}
+        });
+
+        res.json(dailyWork);
+    } catch (error) {
+        return next(new ErrorResponse(error.message, 500));
+    } 
+});
+
+// Query to filter work orders by week
+const weeklyWorkOrders = asyncHandler (async (req, res, next) => {
+    try {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        // Calculate the start date for the current week
+        const currentWeekStartDate = new Date(today);
+        currentWeekStartDate.setDate(today.getDate() - today.getDay());
+
+        // Filter work orders for the created for the current week (Sunday)
+        const weeklyWork = await WorkOrder.find({
+            Date_Created: {$gte: currentWeekStartDate}
+        });
+
+        res.json(weeklyWork);
+    } catch (error) {
+        return next(new ErrorResponse(error.message, 500));
+    }
+});
 
 
 module.exports = {
-    filterWorkStatus
+    filterWorkStatus,
+    dailyWorkOrders,
+    weeklyWorkOrders
 }
