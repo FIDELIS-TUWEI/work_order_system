27a196181ccf702e3da619b36798c63e9c269b8f
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-05 06:59:48 +0300

    backend code cleanup

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { completedWorkOrder, pendingWorkOrder, inProgressWorkOrder, reviewedWorkOrder, filterWorkStatus } = require('../controllers/report');
+const { filterWorkStatus } = require('../controllers/report');
 const { protect, restrict } = require('../middleware/authMiddleware');
 
-router.get("/pending/work", protect, restrict(["admin", "hod", "supervisor", "superadmin"]), pendingWorkOrder);
-router.get("/inprogress/work", protect, restrict(["admin", "hod", "supervisor", "superadmin"]), inProgressWorkOrder);
-router.get("/work/complete", protect, restrict(["admin", "hod", "supervisor", "superadmin"]), completedWorkOrder);
-router.get("/work/review", protect, restrict(["admin", "hod", "supervisor", "superadmin"]), reviewedWorkOrder);
 router.get("/work", protect, restrict(["admin", "hod", "supervisor", "superadmin"]), filterWorkStatus);
 
 
