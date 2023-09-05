13677cb4008f94074cb89cc4fdfca77bf1512c8e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-05 06:57:35 +0300

    backend code cleanup

--- backend/controllers/report.js
+++ backend/controllers/report.js
@@ -1,23 +1,6 @@
 const WorkOrder = require("../model/workOrder");
 const asyncHandler = require("express-async-handler");
 
-// Filter Work Orders
-const filterWorkStatus = asyncHandler (async (req, res, next) => {
-    try {
-        const { status } = req.query;
-        let query = {};
-
-        if (status) {
-            query.status = status;
-        }
-
-        const workOrders = await WorkOrder.find(query);
-        res.json(workOrders);
-    } catch (error) {
-        return next(new ErrorResponse(error.message, 500));
-    }
-})
-
 
 // Pending Work Order
 const pendingWorkOrder = asyncHandler (async (req, res, next) => {
@@ -106,5 +89,4 @@ module.exports = {
     inProgressWorkOrder,
     completedWorkOrder,
     reviewedWorkOrder, 
-    filterWorkStatus
 }
