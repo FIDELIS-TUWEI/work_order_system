73b896e738f71d60c1b4088caeadc38dade2530c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-06 16:10:30 +0300

    dashboard changes

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -2,18 +2,25 @@ import { useSelector } from "react-redux";
 import Layout from "../../components/Layout"
 import { Card, Typography } from "antd";
 import { selectUserInfo } from "../../utils/redux/slices/authSlice";
+import Logo from "../../assets/logo.png"
 
 const Dashboard = () => {
   const user = useSelector(selectUserInfo);
 
   return (
     <Layout>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <img style={{ marginTop: "20px" }} src={Logo} alt="Company Logo" />
+      </div>
       <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>
-          Welcome {user && user.firstName}, {user && user.lastName}
+          Welcome To Holiday Inn Nairobi
         </Typography>
       <Card style={{ margin: "auto", width: "300px" }}>
         <Typography style={{ textAlign: 'center', fontSize: '1rem', fontWeight: 'bold' }}>
-          The ultimate Work Order Management System
+          User: {user && user.firstName}, {user && user.lastName}
+        </Typography>
+        <Typography style={{ textAlign: 'center', fontSize: '1rem', fontWeight: 'bold' }}>
+          Department: {user && user.department} 
         </Typography>
       </Card>
       
