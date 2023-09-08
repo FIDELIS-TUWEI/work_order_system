8497fed762e7a680491411e80a03c2be95f8d119
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-08 21:44:20 +0300

    data passed down as prop code review

---
+++ frontend/src/components/NewWork.jsx
@@ -0,0 +1,117 @@
+import { Card, Col, DatePicker, Form, Input, Row, Select, Typography } from "antd";
+import LoadingBox from "./LoadingBox";
+
+const { Option } = Select;
+const { RangePicker } = DatePicker;
+
+const NewWork = () => {
+  return (
+    <>
+    <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>New Work Order</Typography>
+      <Card style={{ margin: '15px' }}>
+      <Form onFinish={onFinishHandler} layout='vertical' style={{ margin: '18px' }}>
+        <Typography style={{ fontSize: '1rem', fontWeight: '500' }}>Work Details: </Typography>
+        <Row gutter={20}>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item
+                name="title" 
+                label="Title" 
+                required rules={[{ required: true, message: 'Please Enter Work Title!' }]}>
+              <Input type='text' placeholder='Work Title' />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item 
+                name="location" 
+                label="Work Location" 
+                required rules={[{ required: true, message: 'Please Select Work Location!' }]}>
+              <Select 
+                placeholder='Select Work Location'
+                onChange={handleLocationChange}
+                value={selectedLocation}
+                allowClear
+                style={{ width: '100%' }}
+              >
+                {location.map((location) => (
+                  <Option key={location._id} value={location._id}>{location.locationTitle}</Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item 
+                name="serviceType" 
+                label="Service Type" 
+                required rules={[{ required: true, message: 'Please Select a service type!' }]}>
+              <Select 
+                placeholder='Select Service Type' 
+                allowClear
+                style={{ width: '100%' }}
+                options={[
+                  { value: 'Fix', label: 'Fix' }, { value: 'Repair', label: 'Repair' }, 
+                  { value: 'Replace', label: 'Replace' }, { value: 'Install', label: 'Install' }
+                ]}
+              />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item 
+                name="category" 
+                label="Select Category" 
+                required rules={[{ required: true, message: 'Please Select a category!' }]}>
+              <Select 
+                placeholder='Select Category'
+                onChange={handleCategoryChange}
+                value={selectedCategory}
+                allowClear
+                style={{ width: '100%' }}
+              >
+                {category.map((category) => (
+                  <Option key={category._id} value={category._id}>{category.categoryTitle}</Option>
+                ))}
+              </Select>
+            </Form.Item>  
+          </Col>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item
+              name="priority"
+              label="Priority"
+              required
+              rules={[{ required: true, message: 'Please Select a priority!' }]} 
+            >
+              <Select 
+                  placeholder='Select Priority'
+                  allowClear
+                  style={{ width: '100%' }}
+                  options={[
+                    { value: 'Normal', label: 'Normal' }, { value: 'Urgent', label: 'Urgent' }
+                  ]}
+                />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item 
+                name="dueDate" 
+                label="Due Date" 
+                required rules={[{ required: true, message: 'Please Enter Due Date!' }]}>
+              <RangePicker format='YYYY-MM-DD' />
+            </Form.Item>
+          </Col>
+        </Row>
+        <div>
+          <Button style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }} htmlType="submit" onClick={() => {navigate(-1)}}>Go Back</Button>
+        </div>
+        <div className="user_submit">
+          <Button style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }} htmlType="submit">Submit</Button>
+        </div>
+        <Col xs={24} md={24} lg={8}></Col>
+        <div className="loader">
+          { loading && <LoadingBox /> }
+        </div>
+      </Form>
+      </Card>
+      </>
+  )
+}
+
+export default NewWork
--- frontend/src/pages/admin/workOrders/CreateWorkOrder.jsx
+++ frontend/src/pages/admin/workOrders/CreateWorkOrder.jsx
@@ -10,9 +10,9 @@ import LoadingBox from '../../../components/LoadingBox'
 import { useSelector } from 'react-redux'
 import { selectToken } from '../../../utils/redux/slices/authSlice'
 import { allWorkCategories } from '../../../services/categoryApi'
+import NewWork from '../../../components/NewWork';
 
-const { Option } = Select;
-const { RangePicker } = DatePicker;
+
 
 const CreateWorkOrder = () => {
   const token = useSelector(selectToken);
@@ -71,109 +71,16 @@ const CreateWorkOrder = () => {
 
   return (
     <Layout>
-      <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>New Work Order</Typography>
-      <Card style={{ margin: '15px' }}>
-      <Form onFinish={onFinishHandler} layout='vertical' style={{ margin: '18px' }}>
-        <Typography style={{ fontSize: '1rem', fontWeight: '500' }}>Work Details: </Typography>
-        <Row gutter={20}>
-          <Col xs={24} md={24} lg={8}>
-            <Form.Item 
-                name="title" 
-                label="Title" 
-                required rules={[{ required: true, message: 'Please Enter Work Title!' }]}>
-              <Input type='text' placeholder='Work Title' />
-            </Form.Item>
-          </Col>
-          <Col xs={24} md={24} lg={8}>
-            <Form.Item 
-                name="location" 
-                label="Work Location" 
-                required rules={[{ required: true, message: 'Please Select Work Location!' }]}>
-              <Select 
-                placeholder='Select Work Location'
-                onChange={handleLocationChange}
-                value={selectedLocation}
-                allowClear
-                style={{ width: '100%' }}
-              >
-                {location.map((location) => (
-                  <Option key={location._id} value={location._id}>{location.locationTitle}</Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col xs={24} md={24} lg={8}>
-            <Form.Item 
-                name="serviceType" 
-                label="Service Type" 
-                required rules={[{ required: true, message: 'Please Select a service type!' }]}>
-              <Select 
-                placeholder='Select Service Type' 
-                allowClear
-                style={{ width: '100%' }}
-                options={[
-                  { value: 'Fix', label: 'Fix' }, { value: 'Repair', label: 'Repair' }, 
-                  { value: 'Replace', label: 'Replace' }, { value: 'Install', label: 'Install' }
-                ]}
-              />
-            </Form.Item>
-          </Col>
-          <Col xs={24} md={24} lg={8}>
-            <Form.Item 
-                name="category" 
-                label="Select Category" 
-                required rules={[{ required: true, message: 'Please Select a category!' }]}>
-              <Select 
-                placeholder='Select Category'
-                onChange={handleCategoryChange}
-                value={selectedCategory}
-                allowClear
-                style={{ width: '100%' }}
-              >
-                {category.map((category) => (
-                  <Option key={category._id} value={category._id}>{category.categoryTitle}</Option>
-                ))}
-              </Select>
-            </Form.Item>  
-          </Col>
-          <Col xs={24} md={24} lg={8}>
-            <Form.Item
-              name="priority"
-              label="Priority"
-              required
-              rules={[{ required: true, message: 'Please Select a priority!' }]} 
-            >
-              <Select 
-                  placeholder='Select Priority'
-                  allowClear
-                  style={{ width: '100%' }}
-                  options={[
-                    { value: 'Normal', label: 'Normal' }, { value: 'Urgent', label: 'Urgent' }
-                  ]}
-                />
-            </Form.Item>
-          </Col>
-          <Col xs={24} md={24} lg={8}>
-            <Form.Item 
-                name="dueDate" 
-                label="Due Date" 
-                required rules={[{ required: true, message: 'Please Enter Due Date!' }]}>
-              <RangePicker format='YYYY-MM-DD' />
-            </Form.Item>
-          </Col>
-        </Row>
-        <div>
-          <Button style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }} htmlType="submit" onClick={() => {navigate(-1)}}>Go Back</Button>
-        </div>
-        <div className="user_submit">
-          <Button style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }} htmlType="submit">Submit</Button>
-        </div>
-        <Col xs={24} md={24} lg={8}></Col>
-        <div className="loader">
-          { loading && <LoadingBox /> }
-        </div>
-      </Form>
-      </Card>
+      <NewWork 
+        loading={loading} 
+        category={category} 
+        location={location} 
+        onFinish={onFinishHandler}
+        selectedLocation={selectedLocation}
+        selectedCategory={selectedCategory}
+        handleLocationChange={handleLocationChange}
+        handleCategoryChange={handleCategoryChange} 
+      />
     </Layout>
   )
 }
