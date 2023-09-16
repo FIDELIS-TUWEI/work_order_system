b5ab3c3372750e08980c37205c710d1f8cfa93d0
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-16 19:58:14 +0300

    Dashboard component

---
+++ frontend/src/components/DashboardComponent.jsx
@@ -0,0 +1,89 @@
+import { Card, Col, Row, Typography } from "antd";
+
+
+const DashboardComponent = ({ user }) => {
+  return (
+    <>
+        <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>
+            System Analytics
+        </Typography>
+        <Row gutter={16} style={{ marginTop: '20px' }}>
+            {
+            user && user.role === "admin" || user && user.role === "superadmin" ? (
+                <>
+                <Col span={8}>
+                <Card title="Total Work Orders" bordered={false} style={{ marginBottom: '16px'}} className="custom-card">
+                    4
+                </Card>
+                </Col>
+
+                <Col span={8}>
+                    <Card title="Pending Work Orders" bordered={false} className="custom-card">
+                    2
+                    </Card>
+                </Col>
+
+                <Col span={8}>
+                    <Card title="In Progress Work Orders" bordered={false} className="custom-card">
+                    1
+                    </Card>
+                </Col>
+
+                <Col span={8}>
+                    <Card title="Completed Work Orders" bordered={false} className="custom-card">
+                    2
+                    </Card>
+                </Col>
+
+                <Col span={8}>
+                    <Card title="Reviewed Work Orders" bordered={false} className="custom-card">
+                    0
+                    </Card>
+                </Col>
+
+                <Col span={8} style={{ marginBottom: '16px'}}>
+                <Card title="Total Users" bordered={false} className="custom-card">
+                    6
+                </Card>
+                </Col>
+
+                <Col span={8}>
+                <Card title="Active Users" bordered={false} className="custom-card">
+                    6
+                </Card>
+                </Col>
+                </>
+            ) : (
+                <>
+                <Col span={8}>
+                    <Card title="Pending Work Orders" bordered={false} style={{ marginBottom: '16px'}} className="custom-card">
+                    2
+                    </Card>
+                </Col>
+
+                <Col span={8}>
+                    <Card title="In Progress Work Orders" bordered={false} className="custom-card">
+                    1
+                    </Card>
+                </Col>
+
+                <Col span={8}>
+                    <Card title="Completed Work Orders" bordered={false} className="custom-card">
+                    2
+                    </Card>
+                </Col>
+
+                <Col span={8}>
+                    <Card title="Reviewed Work Orders" bordered={false} className="custom-card">
+                    0
+                    </Card>
+                </Col>
+                </>
+            )
+            }
+        </Row>
+    </>
+  )
+}
+
+export default DashboardComponent
--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,91 +1,14 @@
 import { useSelector } from "react-redux";
 import Layout from "../../components/Layout"
-import { Card, Col, Row, Typography } from "antd";
 import { selectUserInfo } from "../../utils/redux/slices/authSlice";
+import DashboardComponent from "../../components/DashboardComponent";
 
 const Dashboard = () => {
   const user = useSelector(selectUserInfo);
 
   return (
     <Layout>
-      <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>
-        System Analytics
-      </Typography>
-      <Row gutter={16} style={{ marginTop: '30px' }}>
-        {
-          user && user.role === "admin" || user && user.role === "superadmin" ? (
-            <>
-            <Col span={8}>
-              <Card title="Total Work Orders" bordered={false} style={{ marginBottom: '16px'}} className="custom-card">
-                4
-              </Card>
-            </Col>
-
-            <Col span={8}>
-                <Card title="Pending Work Orders" bordered={false} className="custom-card">
-                  2
-                </Card>
-              </Col>
-
-              <Col span={8}>
-                <Card title="In Progress Work Orders" bordered={false} className="custom-card">
-                  1
-                </Card>
-              </Col>
-
-              <Col span={8}>
-                <Card title="Completed Work Orders" bordered={false} className="custom-card">
-                  2
-                </Card>
-              </Col>
-
-              <Col span={8}>
-                <Card title="Reviewed Work Orders" bordered={false} className="custom-card">
-                  0
-                </Card>
-              </Col>
-
-              <Col span={8} style={{ marginBottom: '16px'}}>
-              <Card title="Total Users" bordered={false} className="custom-card">
-                6
-              </Card>
-            </Col>
-
-            <Col span={8}>
-              <Card title="Active Users" bordered={false} className="custom-card">
-                6
-              </Card>
-            </Col>
-            </>
-          ) : (
-            <>
-              <Col span={8}>
-                <Card title="Pending Work Orders" bordered={false} style={{ marginBottom: '16px'}} className="custom-card">
-                  2
-                </Card>
-              </Col>
-
-              <Col span={8}>
-                <Card title="In Progress Work Orders" bordered={false} className="custom-card">
-                  1
-                </Card>
-              </Col>
-
-              <Col span={8}>
-                <Card title="Completed Work Orders" bordered={false} className="custom-card">
-                  2
-                </Card>
-              </Col>
-
-              <Col span={8}>
-                <Card title="Reviewed Work Orders" bordered={false} className="custom-card">
-                  0
-                </Card>
-              </Col>
-            </>
-          )
-        }
-      </Row>
+      <DashboardComponent user={user} />
     </Layout>
   )
 }
