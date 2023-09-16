59f180d24620343e5f9c66b9015073d353fb56a2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-16 15:13:16 +0300

    backend code refactor

--- backend/routes/workOrderRoutes.js
+++ backend/routes/workOrderRoutes.js
@@ -7,7 +7,7 @@ const { protect,  restrict} = require('../middleware/authMiddleware');
 router.post("/create/work", protect, restrict(["admin", "hod", "user", "supervisor", "superadmin", "reviewer", "engineer"]), createWorkOrder);
 router.get("/getall/work", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer"]), getAllWorkOrders);
 router.get("/single/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "hod", "reviewer", "engineer"]), getSingleWorkOrder);
-router.put("/update/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "hod", "reviewer", "engineer"]), updateWorkOrder);
+router.put("/update/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "reviewer", "engineer"]), updateWorkOrder);
 router.delete("/delete/work/:id", protect, restrict(["admin"]), deleteWorkOrder);
 
 module.exports = router;
