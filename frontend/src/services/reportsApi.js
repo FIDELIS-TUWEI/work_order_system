c7bde4c945e9af1f15011aa4c7448b2bce4c8c2c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-16 15:57:59 +0300

    reports chnages

--- frontend/src/services/reportsApi.js
+++ frontend/src/services/reportsApi.js
@@ -1,6 +1,17 @@
 import axios from 'axios';
 const WORK_URL = "/hin";
 
+// Endpoint to fetch all work orders report by status
+export const getWorkOrdersReport = async (filterStatus) => {
+    try {
+        const res = await axios.get(`${WORK_URL}/work?status=${filterStatus}`);
+        const data = res.data;
+        return data;
+    } catch (error) {
+        console.log("Error while fetching all work orders by status query", error);   
+    }
+}
+
 // Enpoint to fetch daily work orders report
 export const getDailyReport = async () => {
     try {
