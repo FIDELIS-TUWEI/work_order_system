dfa014ba597990262a17d25afb8050aa13abaec2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-16 20:06:04 +0300

    Backend Count Work Orders with status routes config

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { filterWorkStatus } = require('../controllers/report');
+const { filterWorkStatus, countPendingWorkOrders, countInProgressWorkOrders, countCompletedWorkOrders, countReviewedWorkOrders } = require('../controllers/report');
 const { protect, restrict } = require('../middleware/authMiddleware');
 
 router.get("/work", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer"]), filterWorkStatus);
+router.get("/count/pending-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), countPendingWorkOrders);
+router.get("/count/in-progress-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), countInProgressWorkOrders);
+router.get("/count/completed-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), countCompletedWorkOrders);
+router.get("/count/reviewed-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), countReviewedWorkOrders);
 
 
 module.exports = router;
