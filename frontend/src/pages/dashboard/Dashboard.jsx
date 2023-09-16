f0114a418f8849cb83b281e5527bde9d94074b99
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-16 19:48:03 +0300

    Dashboard summary display on cards

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import Layout from "../../components/Layout"
-import { Card, Col, Row } from "antd";
+import { Card, Col, Row, Typography } from "antd";
 import { selectUserInfo } from "../../utils/redux/slices/authSlice";
 
 const Dashboard = () => {
@@ -8,68 +8,84 @@ const Dashboard = () => {
 
   return (
     <Layout>
-      <Row gutter={16} >
+      <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>
+        System Analytics
+      </Typography>
+      <Row gutter={16} style={{ marginTop: '30px' }}>
         {
           user && user.role === "admin" || user && user.role === "superadmin" ? (
             <>
             <Col span={8}>
-              <Card title="Total Work Orders" bordered={false}>
+              <Card title="Total Work Orders" bordered={false} style={{ marginBottom: '16px'}} className="custom-card">
                 4
               </Card>
             </Col>
 
             <Col span={8}>
-              <Card title="Total Users" bordered={false}>
+                <Card title="Pending Work Orders" bordered={false} className="custom-card">
+                  2
+                </Card>
+              </Col>
+
+              <Col span={8}>
+                <Card title="In Progress Work Orders" bordered={false} className="custom-card">
+                  1
+                </Card>
+              </Col>
+
+              <Col span={8}>
+                <Card title="Completed Work Orders" bordered={false} className="custom-card">
+                  2
+                </Card>
+              </Col>
+
+              <Col span={8}>
+                <Card title="Reviewed Work Orders" bordered={false} className="custom-card">
+                  0
+                </Card>
+              </Col>
+
+              <Col span={8} style={{ marginBottom: '16px'}}>
+              <Card title="Total Users" bordered={false} className="custom-card">
                 6
               </Card>
             </Col>
 
             <Col span={8}>
-                <Card title="Pending Work Orders" bordered={false}>
-                  2
-                </Card>
-              </Col>
-
-              <Col span={8}>
-                <Card title="In Progress Work Orders" bordered={false}>
-                  1
-                </Card>
-              </Col>
-
-              <Col span={8}>
-                <Card title="Completed Work Orders" bordered={false}>
-                  2
-                </Card>
-              </Col>
+              <Card title="Active Users" bordered={false} className="custom-card">
+                6
+              </Card>
+            </Col>
             </>
           ) : (
             <>
               <Col span={8}>
-                <Card title="Pending Work Orders" bordered={false}>
+                <Card title="Pending Work Orders" bordered={false} style={{ marginBottom: '16px'}} className="custom-card">
                   2
                 </Card>
               </Col>
 
               <Col span={8}>
-                <Card title="In Progress Work Orders" bordered={false}>
+                <Card title="In Progress Work Orders" bordered={false} className="custom-card">
                   1
                 </Card>
               </Col>
 
               <Col span={8}>
-                <Card title="Completed Work Orders" bordered={false}>
+                <Card title="Completed Work Orders" bordered={false} className="custom-card">
                   2
+                </Card>
+              </Col>
+
+              <Col span={8}>
+                <Card title="Reviewed Work Orders" bordered={false} className="custom-card">
+                  0
                 </Card>
               </Col>
             </>
           )
         }
-        
-
-        
       </Row>
-      
-      
     </Layout>
   )
 }
