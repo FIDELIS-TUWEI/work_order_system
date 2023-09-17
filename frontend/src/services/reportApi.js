2e8b583dc855e58687c20244761f284790ca0f64
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-17 08:52:33 +0300

    API Service to count Reviewed work orders by status

--- frontend/src/services/reportApi.js
+++ frontend/src/services/reportApi.js
@@ -33,3 +33,14 @@ export const countCompletedWork = async () => {
         console.log("Error While fetching all work orders by completed status", error);
     }
 }
+
+// API Service to count Reviewed work orders by status
+export const countReviewedWork = async () => {
+    try {
+        const res = await axios.get(`${WORK_URL}/count/reviewed-status`);
+        const data = res.data;
+        return data;
+    } catch (error) {
+        console.log("Error While fetching all work orders by reviewed status", error);
+    }
+}
