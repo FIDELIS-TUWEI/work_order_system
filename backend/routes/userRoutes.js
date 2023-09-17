1d0ce2ffb1ea3f0d43c22bcb1c2f3c7f9f60fa8a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-17 09:37:17 +0300

    Controller function to count all users active

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, singleUser, editUser, deleteUser, countAllUsers} = require('../controllers/userController');
+const { getAllUsers, singleUser, editUser, deleteUser, countAllUsers, countActiveUsers} = require('../controllers/userController');
 const { protect, restrict } = require('../middleware/authMiddleware');
 
 router.get("/all-users", protect, restrict(['admin', "superadmin"]), getAllUsers);
 router.get("/user/:id", protect, singleUser);
 router.get("/count/total-users", protect, restrict(["admin", "superadmin"]), countAllUsers);
+router.get("/count/active-users", protect, restrict(["admin", "superadmin"]), countActiveUsers);
 router.put("/edit/:id", protect, restrict(['admin', "superadmin"]),  editUser);
 router.delete("/admin/user/delete/:id", protect, restrict(['admin', "superadmin"]), deleteUser);
 
