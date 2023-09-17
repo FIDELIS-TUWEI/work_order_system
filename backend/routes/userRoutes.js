119cb071f97f8196c6c73afe748efe5b23f8de30
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-17 09:24:51 +0300

    Controller function to count all users routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, singleUser, editUser, deleteUser} = require('../controllers/userController');
+const { getAllUsers, singleUser, editUser, deleteUser, countAllUsers} = require('../controllers/userController');
 const { protect, restrict } = require('../middleware/authMiddleware');
 
 router.get("/all-users", protect, restrict(['admin', "superadmin"]), getAllUsers);
 router.get("/user/:id", protect, singleUser);
+router.get("/count/total-users", protect, restrict(["admin", "superadmin"]), countAllUsers);
 router.put("/edit/:id", protect, restrict(['admin', "superadmin"]),  editUser);
 router.delete("/admin/user/delete/:id", protect, restrict(['admin', "superadmin"]), deleteUser);
 
