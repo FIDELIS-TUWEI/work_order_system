1ec8596673b565b97922b5e556cdd326283d86f2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-17 13:41:52 +0300

    category display bug fix

--- frontend/src/components/ViewAllCategories.jsx
+++ frontend/src/components/ViewAllCategories.jsx
@@ -1,6 +1,9 @@
-import { Button, Card } from "antd"
+import { Button, Card } from "antd";
+import {AiFillEye} from "react-icons/ai"
+import {BiSolidEditAlt} from "react-icons/bi"
 
-const ViewAllCategories = ({ navigate, loading }) => {
+const ViewAllCategories = ({ navigate, loading, categories, user }) => {
+    console.log(categories)
   return (
     <div>
         <div className="add-btn">
@@ -12,9 +15,37 @@ const ViewAllCategories = ({ navigate, loading }) => {
             </Button>
         </div>
         
-        <Card loading={loading} title="All Categories" style={{ margin: "auto", width: "300px" }}></Card>
+        <Card loading={loading} title="All Categories" style={{ margin: "auto", width: "400px" }}>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Categories</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {categories.map((category) => (
+                        <tr key={category._id}>
+                            <td>{category.categoryTitle}</td>
+                            <td className="actions__btn">
+                                <Button style={{ color: 'green', border: 'none', margin: '0 5px'}} onClick={() => navigate(`/work/details/${work._id}`)}><AiFillEye/></Button>
+                                {
+                                    user.role === "admin" || user.role === "superadmin" || user.role === "hod" || user.role === "supervisor" || user.role === "reviewer" ? 
+                                    <Button danger style={{ border: 'none'}} 
+                                        onClick={() => navigate(`/edit/work/${category._id}`)}
+                                    >
+                                        <BiSolidEditAlt/>
+                                    </Button> 
+                                    : null
+                                }
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </Card>
     </div>
   )
 }
 
-export default ViewAllCategories
+export default ViewAllCategories;
--- frontend/src/pages/admin/category/AllCategories.jsx
+++ frontend/src/pages/admin/category/AllCategories.jsx
@@ -1,17 +1,43 @@
 import { useNavigate } from "react-router-dom"
 import Layout from "../../../components/Layout"
 import ViewAllCategories from "../../../components/ViewAllCategories"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { allWorkCategories } from "../../../services/categoryApi"
+import { useSelector } from "react-redux"
+import { selectToken, selectUserInfo } from "../../../utils/redux/slices/authSlice"
 
 const AllCategories = () => {
+    const user = useSelector(selectUserInfo);
+    const token = useSelector(selectToken);
+    const [categories, setCategories] = useState([]);
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+
+    // Function to get all Work Categories
+    const getCategories = async () => {
+        setLoading(true)
+        const res = await allWorkCategories({
+            withCredentials: true,
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        setCategories(res.data);
+        setLoading(false);
+    }
+
+    // UseEffect hook
+    useEffect(() => {
+        getCategories();
+    }, [])
     
   return (
     <Layout>
         <ViewAllCategories 
             navigate={navigate}
             loading={loading}
+            categories={categories}
+            user={user}
         />
     </Layout>
   )
