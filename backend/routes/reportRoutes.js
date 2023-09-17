c724e48964f0920a75e554fd5ebc848726f42b64
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-17 09:08:54 +0300

    Count All Work Orders backend routes code

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { filterWorkStatus, countPendingWorkOrders, countInProgressWorkOrders, countCompletedWorkOrders, countReviewedWorkOrders } = require('../controllers/report');
+const { filterWorkStatus, countPendingWorkOrders, countInProgressWorkOrders, countCompletedWorkOrders, countReviewedWorkOrders, countAllWorkOrders } = require('../controllers/report');
 const { protect, restrict } = require('../middleware/authMiddleware');
 
 router.get("/work", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer"]), filterWorkStatus);
@@ -8,6 +8,7 @@ router.get("/count/pending-status", protect, restrict(["admin", "hod", "reviewer
 router.get("/count/in-progress-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), countInProgressWorkOrders);
 router.get("/count/completed-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), countCompletedWorkOrders);
 router.get("/count/reviewed-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), countReviewedWorkOrders);
+router.get("/count/total-work", protect, restrict(["admin", "superadmin", "user", "hod", "engineer", "reviewer", "supervisor"]), countAllWorkOrders)
 
 
 module.exports = router;
