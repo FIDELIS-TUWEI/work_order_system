fd87e2833236ef8b95080f80a24d74e75ede5ad4
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-18 08:28:59 +0300

    Edit category component

---
+++ frontend/src/pages/admin/category/EditCategory.jsx
@@ -0,0 +1,12 @@
+import Layout from "../../../components/Layout"
+import UpdateCategory from "../../../components/UpdateCategory"
+
+const EditCategory = () => {
+  return (
+    <Layout>
+        <UpdateCategory />
+    </Layout>
+  )
+}
+
+export default EditCategory
