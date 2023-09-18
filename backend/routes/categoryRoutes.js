b73249496c9dd1c00ca475ea932298f7b0465afc
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-18 05:04:08 +0300

    router to categories backend

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createCategory, getAllCategories } = require("../controllers/categoryController");
+const { createCategory, getAllCategories, updateCategory, deleteCategory } = require("../controllers/categoryController");
 const { protect, restrict } = require("../middleware/authMiddleware");
 
 router.post("/new/category", protect, restrict(["admin", "superadmin"]), createCategory);
 router.get("/all/categories", protect, getAllCategories);
+router.put("/edit/category/:id", protect, restrict(["admin", "superadmin"]), updateCategory);
+router.delete("/delete/category/:id", protect, restrict(["admin", "superadmin"]), deleteCategory);
 
 module.exports = router;
