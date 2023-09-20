09360fe8c9921f032461d4605b2a61df131d4e2b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-20 15:37:07 +0300

    backend model changes

--- backend/model/category.js
+++ backend/model/category.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const categorySchema = new mongoose.Schema({
     categoryTitle: {
         type: String,
+        required: true
     },
 });
 
--- backend/model/location.js
+++ backend/model/location.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const locationSchema = new mongoose.Schema({
     locationTitle: {
         type: String,
+        required: true
     }
 });
 
