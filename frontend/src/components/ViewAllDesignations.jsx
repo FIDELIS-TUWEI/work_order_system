7e44adc6349ef11e326d4aed6496e2ff5b28c7f7
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-23 21:34:38 +0300

    all designations component

--- frontend/src/components/ViewAllDesignations.jsx
+++ frontend/src/components/ViewAllDesignations.jsx
@@ -1,7 +1,110 @@
+import { Button, Card, Modal, message } from "antd";
+import { useNavigate } from "react-router-dom";
+import {MdDelete} from "react-icons/md";
+import {GrFormNext, GrFormPrevious} from "react-icons/gr";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectToken } from "../utils/redux/slices/authSlice";
+import { deleteDesignation } from "../services/designation";
 
-const ViewAllDesignations = () => {
+
+const ViewAllDesignations = ({ designations, loading, handlePageChange, page, pages, getDesignations }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedDesignationToDelete, setSelectedDesignationToDelete] = useState(null);
+  const token = useSelector(selectToken);
+  const navigate = useNavigate();
+
+  // Function to show modal to delete designation
+  const showModal = async (designation) => {
+    setSelectedDesignationToDelete(designation);
+    setIsModalVisible(true);
+  };
+
+  // Function to confirm modal delete designation
+  const handleDelete = async () => {
+    try {
+      await deleteDesignation(selectedDesignationToDelete._id, {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      message.success("Designation deleted successfully");
+      setIsModalVisible(false);
+      getDesignations();
+    } catch (error) {
+      console.error(error);
+      message.error("An error occurred while deleting the designation", error);
+    }
+  };
+
+  // Function to handle modal cancel
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  }
+
   return (
-    <div>ViewAllDesignations</div>
+    <>
+      <div className="add-btn">
+      <Button
+          style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }}
+          onClick={() => navigate("/new/designation")}
+      >
+          Add Designation
+      </Button>
+      </div>
+
+      <Card loading={loading} title="All Designations" style={{ margin: "auto", width: "500px" }}>
+        <table>
+          <thead>
+            <tr>
+              <th>Designation</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {designations.map((designation) => (
+              <tr key={designation._id}>
+                <td>{designation.designationName}</td>
+                <td className="actions__btn">
+                  <Button danger style={{ border: "none" }}
+                    onClick={() => showModal(designation)}
+                  >
+                    <MdDelete />
+                  </Button>  
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Card>
+
+      <Modal
+        title="Delete Designation"
+        open={isModalVisible}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Cancel
+          </Button>,
+          <Button key="submit" type="primary" onClick={handleDelete}>
+            Delete
+          </Button>,
+        ]}
+      >
+        <p>Are you sure you want to delete this designation?</p>
+      </Modal>
+
+      <div className="pagination">
+        <Button disabled={page === 1} onClick={() => handlePageChange(page - 1)} style={{ border: 'none', margin: '0 5px' }}>
+          <GrFormPrevious />
+        </Button>
+        <span> Page {page} of {pages}</span>
+        <Button disabled={page === pages} onClick={() => handlePageChange(page + 1)} style={{ border: 'none', margin: '0 5px' }}>
+          <GrFormNext />
+        </Button>
+      </div>
+    </>
   )
 }
 
--- frontend/src/pages/admin/designation/AllDesignations.jsx
+++ frontend/src/pages/admin/designation/AllDesignations.jsx
@@ -1,10 +1,51 @@
+import { useSelector } from 'react-redux';
 import Layout from '../../../components/Layout';
 import ViewAllDesignations from '../../../components/ViewAllDesignations';
+import { selectToken } from '../../../utils/redux/slices/authSlice';
+import { useEffect, useState } from 'react';
+import { allDesignations } from '../../../services/designation';
 
 const AllDesignations = () => {
+  const token = useSelector(selectToken);
+  const [designations, setDesignations] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  // Function to get all designations
+  const getDesignations = async () => {
+    setLoading(true);
+    const { data, pages } = await allDesignations(page, {
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    setDesignations(data);
+    setPages(pages);
+    setLoading(false);
+  };
+
+  // UseEffect hook
+  useEffect(() => {
+    getDesignations();
+  }, [page]);
+
+  // Function to handle page change
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  }
+
   return (
     <Layout>
-      <ViewAllDesignations />
+      <ViewAllDesignations 
+        designations={designations}
+        loading={loading}
+        handlePageChange={handlePageChange}
+        page={page}
+        pages={pages}
+        getDesignations={getDesignations}
+      />
     </Layout>
   )
 }
