5f3480975f1246eb9f4983e62192505f8da37174
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-23 17:08:36 +0300

    function to create new department

--- frontend/src/components/CreateDepartment.jsx
+++ frontend/src/components/CreateDepartment.jsx
@@ -1,7 +1,43 @@
+import { Button, Card, Col, Form, Input, Row, Typography } from "antd"
+import LoadingBox from "./LoadingBox"
 
-const CreateDepartment = () => {
+const CreateDepartment = ({loading, onFinishHandler, navigate}) => {
   return (
-    <div>CreateDepartment</div>
+    <>
+      <Typography
+        style={{
+          display: "flex", justifyContent: "center",
+          alignItems: "center", fontSize: "1.5rem", fontWeight: "bold"
+        }}
+      >
+        Create New Department Form
+      </Typography>
+      <Card title="Add Department" style={{ margin: '15px' }}>
+        <Form layout="vertical" onFinish={onFinishHandler}>
+          <Row gutter={16}>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                name="departmentTitle"
+                label="Create Department"
+                required rules={[{ required: true, message: "Please Enter new Department" }]}
+              >
+                <Input type="text" placeholder="New Department" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <div>
+            <Button style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }} htmlType="submit" onClick={() => {navigate(-1)}}>Go Back</Button>
+          </div>
+          <div className="user_submit">
+            <Button style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }} htmlType="submit">Submit</Button>
+          </div>
+          <Col xs={24} md={24} lg={8}></Col>
+          <div className="loader">
+            { loading && <LoadingBox /> }
+          </div>
+        </Form>
+      </Card>
+    </>
   )
 }
 
--- frontend/src/pages/admin/department/NewDepartment.jsx
+++ frontend/src/pages/admin/department/NewDepartment.jsx
@@ -1,8 +1,31 @@
+import { useState } from 'react';
+import CreateDepartment from '../../../components/CreateDepartment';
 import Layout from '../../../components/Layout'
+import { useNavigate } from 'react-router-dom';
+import { message } from 'antd';
+import { createNewDepartment } from '../../../services/departmentApi';
 
 const NewDepartment = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  // Function to handle form submit
+  const onFinishHandler = async (values) => {
+    setLoading(true);
+    await createNewDepartment(values);
+    navigate("/all-departments");
+    message.success("New Department Created Succesfully");
+    setLoading(false);
+  };
+
   return (
-    <Layout>NewDepartment</Layout>
+    <Layout>
+      <CreateDepartment 
+        onFinishHandler={onFinishHandler}
+        loading={loading}
+        navigate={navigate}
+      />
+    </Layout>
   )
 }
 
