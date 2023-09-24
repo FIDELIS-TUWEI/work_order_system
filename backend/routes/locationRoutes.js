7ea5fc4c5f6d1f0e88408dc714a9b7673c917d49
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-24 12:50:19 +0300

    Query to get all locations router

--- backend/routes/locationRoutes.js
+++ backend/routes/locationRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createLocation, getAllLocations, deleteLocation } = require('../controllers/locationController');
+const { createLocation, getAllLocations, deleteLocation, queryAllLocations } = require('../controllers/locationController');
 const { protect, restrict } = require('../middleware/authMiddleware');
 
 
 router.post("/create/location", protect, restrict(["admin", "superadmin"]), createLocation);
-router.get("/all-locations", protect, restrict(["admin", "superadmin", "hod", "reviewer", "engineer", "user"]), getAllLocations);
+router.get("/all-locations", protect, restrict(["admin", "superadmin"]), getAllLocations);
+router.get("/search/location", protect, restrict(["admin", "superadmin", "hod", "reviewer", "engineer", "user"]), queryAllLocations);
 router.delete("/delete/location/:id", protect, restrict(["admin", "superadmin"]), deleteLocation);
 
 module.exports = router;
