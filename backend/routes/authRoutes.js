49fdea5835750f52f52bf62daa0dd2a69882f88d
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-24 17:49:48 +0300

    Update user password route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { signupUser, login, logout, getUserInfo } = require("../controllers/authController");
+const { signupUser, login, logout, getUserInfo, updatePassword } = require("../controllers/authController");
 const { protect, restrict } = require("../middleware/authMiddleware");
 
 router.post("/register", protect, restrict(["admin", "superadmin"]), signupUser);
 router.post("/login",  login);
 router.post("/logout", logout);
 router.get("/userInfo", protect, getUserInfo);
+router.put("/updatePassword/:username", protect, restrict(["admin", "superadmin"]), updatePassword);
 
 module.exports = router;
