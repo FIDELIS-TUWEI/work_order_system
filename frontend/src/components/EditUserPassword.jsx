31e489986ca271eabbca9a246b87c016bc794e72
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-09-26 10:47:40 +0300

    change password frontend

--- frontend/src/components/EditUserPassword.jsx
+++ frontend/src/components/EditUserPassword.jsx
@@ -3,7 +3,12 @@ import { Button, Card, Col, Form, Input, Row, Typography } from "antd"
 const EditUserPassword = ({ onFinishHandler }) => {
   return (
     <>
-        <Typography>
+        <Typography
+            style={{
+                display: "flex", justifyContent: "center",
+                alignItems: "center", fontSize: "1.5rem", fontWeight: "bold"
+            }}
+        >
             Change User Password Form
         </Typography>
 
@@ -16,7 +21,7 @@ const EditUserPassword = ({ onFinishHandler }) => {
                             label="Old Password"
                             required rules={[{ required: true, message: "Please Enter your Old Password" }]}
                         >
-                            <Input type="password" value="" onChange={(e) => setOldPassword(e.target.value)} />
+                            <Input type="password" value="" onChange={(e) => setOldPassword(e.target.value)} placeholder="Enter Old Password" />
                         </Form.Item>
                     </Col>
                     <Col xs={24} md={24} lg={8}>
@@ -28,7 +33,7 @@ const EditUserPassword = ({ onFinishHandler }) => {
                                 { min: 8, message: "Password must be atleast 8 characters long" },
                             ]}
                         >
-                            <Input type="password" value="" onChange={(e) => setNewPassword(e.target.value)} />
+                            <Input type="password" value="" onChange={(e) => setNewPassword(e.target.value)} placeholder="Enter New Password" />
                         </Form.Item>
                     </Col>
                 </Row>
