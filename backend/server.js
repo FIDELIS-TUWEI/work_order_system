4b1dd32a5178c4b2b340fa27818ae5e89fc506b7
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-01 05:44:57 +0300

    rate limiter config

--- backend/server.js
+++ backend/server.js
@@ -45,8 +45,17 @@ app.use(cors({
 app.use(mongoSanitize());
 // Rate Limit
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    windowMs: 60 * 1000, // 1 minute
+    max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    message: {
+        message: "Too many login attempts from this IP, please try again after  a 1 minute interval"
+    },
+    handler: (req, res, next, options) => {
+        res.status(429).json({
+            success: false,
+            message: options.message
+        });
+    },
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
