254cdd6c011abd4149e98f275998144641dde9b8
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-03 10:21:20 +0300

    function to logout when cookie expire backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -91,6 +91,7 @@ const login = asyncHandler (async (req, res, next) => {
             res.status(200).json({
                 success: true,
                 message: "User logged in successfully",
+                user,
                 accessToken, 
                 refreshToken
             })
