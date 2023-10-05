0da1a4cca9031e0e7a6c1717992f11191cd5473a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-05 11:25:12 +0300

    verify jwt logic

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { signupUser, login, logout, getUserInfo, forgotPassword, resetPassword } = require("../controllers/authController");
+const { signupUser, login, logout, getUserInfo, forgotPassword, resetPassword, refresh } = require("../controllers/authController");
 const { protect, restrict } = require("../middleware/authMiddleware");
 
 router.post("/register", protect, restrict(["admin", "superadmin"]), signupUser);
 router.post("/login",  login);
 router.post("/logout", logout);
 router.get("/userInfo", protect, getUserInfo);
+router.get("/auth/refresh", refresh);
 router.post("/forgot/password", protect, forgotPassword);
 router.patch("/reset/password/:token", protect, resetPassword);
 
