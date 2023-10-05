a221f30c2ca20cfc97791e270e83b75635b6ca53
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-05 08:18:43 +0300

    frontend changes

--- frontend/src/components/PublicRoute.jsx
+++ frontend/src/components/PublicRoute.jsx
@@ -1,6 +1,4 @@
 import { Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectUserInfo } from "../utils/redux/slices/authSlice";
 
 
 const PublicRoute = ({ children }) => {
