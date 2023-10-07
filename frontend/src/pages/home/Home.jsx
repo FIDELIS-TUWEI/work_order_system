1bd4c844581523760e6da68886ccb4f34a44090a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-07 12:19:05 +0300

    home page changes

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,28 @@
-import { Card } from "antd";
-import { Link } from "react-router-dom"
+import { Button, Card, Typography } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import Logo from "../../assets/logo.png";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
-    <Card style={{ margin: "auto", width: "200px", display: "flex", justifyContent: "center" }}>
-      <h1>Home</h1>
-      <Link to="/login">Login</Link>
-    </Card>
+    <>
+    <div style={{ display: "flex" }}>
+      <div style={{ flex: 1 }}>
+        <img 
+          src={Logo}
+          alt="logo"
+          style={{ width: '90%', height: 'auto' }}
+        />
+      </div>
+      <div style={{ flex: 1 }}>
+        <Card>
+          <Typography>Home</Typography>
+          <Button onClick={() => navigate("/login")}>Login</Button>
+        </Card>
+      </div>
+    </div>
+    </>
   )
 }
 
