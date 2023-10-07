059883af4d36d8a406e289c17dd785b8e750f8f2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-07 12:37:30 +0300

    home page changes

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,25 +2,49 @@ import { Button, Card, Typography } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../assets/logo.png";
 
+const Footer = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <p>
+        &copy; {new Date().getFullYear()} Work Order Management System. All Rights Reserved.
+      </p>
+    </div>
+  )
+}
+
 const Home = () => {
   const navigate = useNavigate();
 
   return (
     <>
-    <div style={{ display: "flex" }}>
-      <div style={{ flex: 1 }}>
-        <img 
-          src={Logo}
-          alt="logo"
-          style={{ width: '90%', height: 'auto' }}
-        />
+    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+
+      {/* Main Content */}
+      <div style={{ flex: 1, display: "flex" }}>
+        <div style={{ flex: 1 }}>
+          <img 
+            src={Logo}
+            alt="logo"
+            style={{ width: '80%', height: 'auto' }}
+          />
+        </div>
+        
+        { /* Right Side */}
+        <div style={{ flex: 1 }}>
+          <Card style=
+            {{ 
+              padding: '20px', textAlign: 'center', 
+              fontSize: '1.5rem', fontWeight: 'bold',
+              margin: '100px' 
+            }}
+          >
+            <Typography>Home</Typography>
+            <Button onClick={() => navigate("/login")}>Login</Button>
+          </Card>
       </div>
-      <div style={{ flex: 1 }}>
-        <Card>
-          <Typography>Home</Typography>
-          <Button onClick={() => navigate("/login")}>Login</Button>
-        </Card>
       </div>
+      
+      <Footer />
     </div>
     </>
   )
