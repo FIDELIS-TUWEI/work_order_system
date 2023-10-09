f6b0294f1077a008f10b5d2cf5b7e89e6fb75137
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-09 11:57:33 +0300

    modal to delete user config

--- frontend/src/components/AllUsers.jsx
+++ frontend/src/components/AllUsers.jsx
@@ -1,10 +1,49 @@
-import { Button, Card } from "antd";
+import { Button, Card, Modal, message } from "antd";
 import {BiSolidEditAlt} from "react-icons/bi";
 import {AiFillEye} from "react-icons/ai";
 import {GrFormNext, GrFormPrevious} from "react-icons/gr";
+import {MdDelete} from "react-icons/md";
+import { selectToken } from "../utils/redux/slices/authSlice";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { deleteUser } from "../services/usersApi";
 
 
-const AllUsers = ({ allUsers, loading, page, pages, handlePageChange, navigate}) => {
+const AllUsers = ({ allUsers, loading, page, pages, handlePageChange, navigate, getUsers}) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedUserToDelete, setSelectedUserToDelete] = useState(null);
+  const token = useSelector(selectToken);
+
+
+  // Fucntion to show modal to delete user
+  const showModal = async (user) => {
+    setSelectedUserToDelete(user);
+    setIsModalVisible(true);
+  };
+
+  // Function to confirm modal delete
+  const handleDelete = async () => {
+      try {
+         await deleteUser(selectedUserToDelete._id, {
+            withCredentials: true,
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+         });
+         message.success("User deleted successfully");
+         setIsModalVisible(false);
+         getUsers();
+      } catch (error) {
+          console.error(error);
+          message.error("An error occurred while deleting the user", error);
+      }
+  };
+
+  // Function to handle modal cancel
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  }
+
   return (
     <div>
         <div className="add-btn">
@@ -47,11 +86,30 @@ const AllUsers = ({ allUsers, loading, page, pages, handlePageChange, navigate})
                 <Button danger style={{ border: 'none'}} onClick={() => {navigate(`/edit/user/${user._id}`)}}>
                   <BiSolidEditAlt/>
                 </Button>
+                <Button danger style={{ border: 'none'}} onClick={() => showModal(user)}>
+                  <MdDelete/>
+                </Button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      <Modal
+        title="Delete User"
+        open={isModalVisible}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Cancel
+          </Button>,
+          <Button key="submit" type="primary" danger onClick={handleDelete}>
+            Delete
+          </Button>,
+        ]}
+      >
+        <p>Are you sure you want to delete this user?</p>
+      </Modal>
       </Card>
       <div className="pagination">
         <Button disabled={page === 1} onClick={() => handlePageChange(page - 1)} style={{ border: 'none', margin: '0 5px' }}>
