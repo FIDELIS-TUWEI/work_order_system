63dce8ad71990b9bedf855b74cb11a9d51261c4a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-10 12:15:43 +0300

    dashboard cards conditional render refactor

--- frontend/src/components/DashboardComponent.jsx
+++ frontend/src/components/DashboardComponent.jsx
@@ -5,6 +5,98 @@ const DashboardComponent = ({ user, pendingWorkCount,
     inProgressCount, completedCount, reviewedCount, 
     totalWorkCount, totalUsersCount, activeUsersCount 
 }) => {
+
+    // Conditional render for admin or superadmin
+    const isAdmin = user &&(user.role === "admin" || user.role === "superadmin" || user.role === "supervisor");
+    const isManager = user && (user.role === "hod" || user.role === "reviewer" || user.role === "engineer");
+
+    // Render admin or superadmin card
+    const renderAdminsCard = () => (
+        <>
+            <Col span={8}>
+                <Card title="Total Work Orders" bordered={false} className="custom-card">
+                    {totalWorkCount}
+                </Card>
+            </Col>
+
+            <Col span={8}>
+                <Card title="Pending Work Orders" bordered={false} className="custom-card">
+                    {pendingWorkCount}
+                </Card>
+            </Col>
+
+            <Col span={8}>
+                <Card title="In Progress Work Orders" bordered={false} className="custom-card">
+                    {inProgressCount}
+                </Card>
+            </Col>
+
+            <Col span={8}>
+                <Card title="Completed Work Orders" bordered={false} className="custom-card">
+                    {completedCount}
+                </Card>
+            </Col>
+
+            <Col span={8}>
+                <Card title="Reviewed Work Orders" bordered={false} className="custom-card">
+                    {reviewedCount}
+                </Card>
+            </Col>
+
+            <Col span={8}>
+                <Card title="Total Users" bordered={false} className="custom-card">
+                    {totalUsersCount}
+                </Card>
+            </Col>
+
+            <Col span={8}>
+                <Card title="Active Users" bordered={false} className="custom-card">
+                    {activeUsersCount}
+                </Card>
+            </Col>
+        </>
+    );
+
+    // Render Managers card
+    const renderManagersCard = () => (
+        <>
+            <Col span={8}>
+                <Card title="Pending Work Orders" bordered={false} className="custom-card">
+                    {pendingWorkCount}
+                </Card>
+            </Col>
+
+            <Col span={8}>
+                <Card title="In Progress Work Orders" bordered={false} className="custom-card">
+                    {inProgressCount}
+                </Card>
+            </Col>
+
+            <Col span={8}>
+                <Card title="Completed Work Orders" bordered={false} className="custom-card">
+                    {completedCount}
+                </Card>
+            </Col>
+
+            <Col span={8}>
+                <Card title="Reviewed Work Orders" bordered={false} className="custom-card">
+                    {reviewedCount}
+                </Card>
+            </Col>
+        </>
+    );
+
+    // Render Users card
+    const renderUsersCard = () => (
+        <>
+        <div>
+            <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: '400', margin: '2rem' }}>
+                The Ultimate Work Order Management System for the Holiday Inn.
+            </Typography>
+        </div>
+        </>
+    )
+
     
   return (
     <>
@@ -12,79 +104,7 @@ const DashboardComponent = ({ user, pendingWorkCount,
             System Analytics
         </Typography>
         <Row gutter={16}>
-            {
-                user && user.role === "admin" || user && user.role === "superadmin" ? (
-                <>
-                    <Col span={8}>
-                        <Card title="Total Work Orders" bordered={false} className="custom-card">
-                            {totalWorkCount}
-                        </Card>
-                    </Col>
-
-                    <Col span={8}>
-                        <Card title="Pending Work Orders" bordered={false} className="custom-card">
-                            {pendingWorkCount}
-                        </Card>
-                    </Col>
-
-                    <Col span={8}>
-                        <Card title="In Progress Work Orders" bordered={false} className="custom-card">
-                            {inProgressCount}
-                        </Card>
-                    </Col>
-
-                    <Col span={8}>
-                        <Card title="Completed Work Orders" bordered={false} className="custom-card">
-                            {completedCount}
-                        </Card>
-                    </Col>
-
-                    <Col span={8}>
-                        <Card title="Reviewed Work Orders" bordered={false} className="custom-card">
-                            {reviewedCount}
-                        </Card>
-                    </Col>
-
-                    <Col span={8}>
-                        <Card title="Total Users" bordered={false} className="custom-card">
-                            {totalUsersCount}
-                        </Card>
-                    </Col>
-
-                    <Col span={8}>
-                        <Card title="Active Users" bordered={false} className="custom-card">
-                            {activeUsersCount}
-                        </Card>
-                    </Col>
-                </>
-            ) : (
-                <>
-                    <Col span={8}>
-                        <Card title="Pending Work Orders" bordered={false} className="custom-card">
-                            {pendingWorkCount}
-                        </Card>
-                    </Col>
-
-                    <Col span={8}>
-                        <Card title="In Progress Work Orders" bordered={false} className="custom-card">
-                            {inProgressCount}
-                        </Card>
-                    </Col>
-
-                    <Col span={8}>
-                        <Card title="Completed Work Orders" bordered={false} className="custom-card">
-                            {completedCount}
-                        </Card>
-                    </Col>
-
-                    <Col span={8}>
-                        <Card title="Reviewed Work Orders" bordered={false} className="custom-card">
-                            {reviewedCount}
-                        </Card>
-                    </Col>
-                </>
-            )
-            }
+            {isAdmin ? renderAdminsCard() : isManager ? renderManagersCard() : renderUsersCard()}
         </Row>
     </>
   )
