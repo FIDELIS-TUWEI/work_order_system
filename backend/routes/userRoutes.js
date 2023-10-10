a40fdc8fb3d0f4210878df433c6b9966fcc6f6d1
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-10 12:16:41 +0300

    conditional render refactor

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,8 +5,8 @@ const { protect, restrict } = require('../middleware/authMiddleware');
 
 router.get("/all-users", protect, restrict(['admin', "superadmin"]), getAllUsers);
 router.get("/user/:id", protect, singleUser);
-router.get("/count/total-users", protect, restrict(["admin", "superadmin"]), countAllUsers);
-router.get("/count/active-users", protect, restrict(["admin", "superadmin"]), countActiveUsers);
+router.get("/count/total-users", protect, countAllUsers);
+router.get("/count/active-users", protect, countActiveUsers);
 router.put("/edit/:id", protect, restrict(['admin', "superadmin"]),  editUser);
 router.delete("/admin/user/delete/:id", protect, restrict(['admin', "superadmin"]), deleteUser);
 
