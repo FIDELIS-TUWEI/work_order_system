a9e38f939a8d6942f55bcd85574a2b8d706f0a3b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-15 05:24:02 +0300

    All employees design

--- frontend/src/components/AllEmployees.jsx
+++ frontend/src/components/AllEmployees.jsx
@@ -1,7 +1,25 @@
+import { Button } from "antd"
+import { useNavigate } from "react-router-dom";
 
 const AllEmployees = () => {
+  const navigate = useNavigate();
   return (
-    <div>AllEmployees</div>
+    <div>
+      <div className="add-btn">
+        <Button
+        style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }}
+          onClick={() => navigate("/new/employee")}
+        >
+          Add Employee
+        </Button>
+      </div>
+
+      <div className="add-btn">
+        <Button style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }} onClick={() => navigate(-1)}>
+          Go Back
+        </Button>
+      </div>
+    </div>
   )
 }
 
