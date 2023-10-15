1cc20ac3589f0a9a0888efffad9956710ab44bd1
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-15 17:04:55 +0300

    backend controller changes

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { newEmployee, getAllEmployees, singleEmployee, deleteEmployee, editEmployee } = require("../controllers/employeeController");
+const { newEmployee, getAllEmployees, singleEmployee, deleteEmployee, editEmployee, countWorkAssigned, queryAllEmployees } = require("../controllers/employeeController");
 const { protect, restrict } = require("../middleware/authMiddleware");
 
 router.post("/new/employee", protect, restrict(["admin", "superadmin"]), newEmployee);
 router.get("/all/employees", protect, restrict(["admin", "superadmin"]), getAllEmployees);
+router.get("/query/all/employees", protect, restrict(["admin", "superadmin", "hod", "reviewer", "engineer", "supervisor"]), queryAllEmployees);
 router.get("/single/employee/:id", protect, singleEmployee);
+router.get("/employee/work/count", protect, restrict(["admin", "superadmin"]), countWorkAssigned);
 router.put("/edit/employee/:id", protect, restrict(["admin", "superadmin"]), editEmployee);
 router.delete("/delete/employee/:id", protect, restrict(["admin", "superadmin"]), deleteEmployee);
 
