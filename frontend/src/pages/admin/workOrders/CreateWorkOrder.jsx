6d38bfce6a208cc5d94fdefd63ed29d6138aa5c1
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-16 09:51:51 +0300

    create work with all employees

--- frontend/src/pages/admin/workOrders/CreateWorkOrder.jsx
+++ frontend/src/pages/admin/workOrders/CreateWorkOrder.jsx
@@ -18,6 +18,8 @@ const CreateWorkOrder = () => {
   const [selectedLocation, setSelectedLocation] = useState(null);
   const [category, setCategory] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [employees, setEmployees] = useState([]);
+  const [selectedEmployee, setSelectedEmployee] = useState(null);
   const navigate = useNavigate();
 
   const authorisedAccess = user?.role === "admin" || user?.role === "superadmin" || user?.role === "supervisor" || user?.role === "hod" || user?.role === "reviewer" || user?.role === "engineer";
@@ -70,6 +72,8 @@ const CreateWorkOrder = () => {
     });
     setCategory(response.data);
   };
+
+  // Funtion to handle change in employee
 
   // UseEffect hook
   useEffect(() => {
