5850c734023354f491a3654e161231d690d926bf
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-17 22:21:34 +0300

    edit work refactor frontend logic

--- frontend/src/pages/admin/workOrders/EditWorkOrder.jsx
+++ frontend/src/pages/admin/workOrders/EditWorkOrder.jsx
@@ -20,9 +20,6 @@ const EditWorkOrder = () => {
   // Function to handle form submit
   const onFinishHandler = async (values) => {
     try {
-      if (values.assignedTo) {
-        values.status = 'In_Progress';
-      }
       await updateWorkOrder(id, values);
       navigate('/work/list');
       message.success('Work Order Updated Successfully');
@@ -65,15 +62,6 @@ const EditWorkOrder = () => {
       getEmployees();
     }
   }, [id]);
-
-  // Hook to check if work is already assigned to an employee
-  useEffect(() => {
-    if (workDetails.assignedTo) {
-      setSelectedEmployee(workDetails.assignedTo);
-    } else {
-      setSelectedEmployee(null);
-    }
-  }, [workDetails.assignedTo]);
   
   return (
     <Layout>
