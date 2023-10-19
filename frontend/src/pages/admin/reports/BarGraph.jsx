ed8fe652b0158dea3a18f193b7f58f5d1a4dd7a9
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-19 19:51:28 +0300

    visualize requested work data

--- frontend/src/pages/admin/reports/BarGraph.jsx
+++ frontend/src/pages/admin/reports/BarGraph.jsx
@@ -3,11 +3,11 @@ import { useSelector } from "react-redux";
 import { selectToken } from "../../../utils/redux/slices/authSlice";
 import { getAllWorkOrders } from "../../../services/workApi";
 import { Card } from "antd";
-import {  ResponsiveContainer, Tooltip, Legend, PieChart, Pie } from 'recharts';
+import {  ResponsiveContainer, Tooltip, Legend, PieChart, Pie, LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
 
 const BarGraph = () => {
     const token = useSelector(selectToken);
-    const [workOrders, setWorkOrders] = useState([]);
+    const [pieData, setPieData] = useState([]);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
@@ -23,12 +23,12 @@ const BarGraph = () => {
                 Authorization: `Bearer ${token}`
             }
         });
-        setWorkOrders(data);
+        setPieData(data);
         setLoading(false);
     }
 
     // Count the number of work assigned to each employee
-    const workCounts = workOrders.reduce((counts, workOrder) => {
+    const workCounts = pieData.reduce((counts, workOrder) => {
         const assignedTo = workOrder.assignedTo ? workOrder.assignedTo.firstName : "Unassigned";
         counts[assignedTo] = (counts[assignedTo] || 0) + 1;
         return counts;
@@ -40,15 +40,42 @@ const BarGraph = () => {
     // Convert the workCounts object to an array of objects
     const workCountsArray = Object.entries(workCounts).map(([employee, count], index) => ({ employee, count, fill: uniqueColors[index] }));
 
+    // Count the number of requested by each user
+    const userCounts = pieData.reduce((counts, workOrder) => {
+        const requestedBy = workOrder.requestedBy.username;
+        counts[requestedBy] = (counts[requestedBy] || 0) + 1;
+        return counts;
+    }, {});
+
+    // Generate an Array of unique colors for each user
+    const uniqueUserColors = generateUniqueColors(Object.keys(userCounts).length);
+
+    // Convert the userCounts object to an array of objects
+    const userCountsArray = Object.entries(userCounts).map(([user, count], index) => ({ user, count, fill: uniqueUserColors[index] }));
+
+
   return (
     <div>
         <Card title="Work Orders Assigned To Employees" style={{ margin: "15px 25px" }} loading={loading}>
-            <ResponsiveContainer width="100%" aspect={3}>
+            <ResponsiveContainer width="70%" aspect={3}>
                 <PieChart>
                     <Pie data={workCountsArray} dataKey="count" nameKey="employee" cx="50%" cy="50%" outerRadius={80} fill="#8884d8"  />
                     <Tooltip />
                     <Legend />
                 </PieChart>
+            </ResponsiveContainer>
+        </Card>
+
+        <Card title="WorkOrders Requested By Users" style={{ margin: "15px 25px" }} loading={loading}>
+            <ResponsiveContainer width="70%" aspect={3}>
+                <LineChart>
+                    <Line data={userCountsArray} dataKey="count" type='monotone' stroke="#8884d8" dot={{r:6}} activeDot={{r:8}} fill="#8884d8"  />
+                    <XAxis dataKey="user" />
+                    <YAxis />
+                    <Tooltip />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <Legend />
+                </LineChart>
             </ResponsiveContainer>
         </Card>
     </div>
