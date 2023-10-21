d568c82d5d0b41a662a65d40de7435a08d0c4d1e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-21 11:02:12 +0300

    error message refactor

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -34,7 +34,7 @@ const LogIn = () => {
             message.success("Login Succesful");
             navigate('/private');
         } catch (error) {
-            message.error(error.data.error);
+            message.error("Too Many Attempts, Please Try Again Later", error);
         }
     }
 
