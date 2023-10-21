0eabdb92ecb64da5037122e1099a55c550c88b46
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-21 17:32:51 +0300

    form to update employee

--- frontend/src/components/UpdateEmployee.jsx
+++ frontend/src/components/UpdateEmployee.jsx
@@ -1,7 +1,55 @@
+import { Button, Card, Col, Form, Input, Row, Typography } from "antd";
 
-const UpdateEmployee = () => {
+const UpdateEmployee = ({ onFinishHandler, navigate, employeeDetails }) => {
   return (
-    <div>UpdateEmployee</div>
+    <div>
+      <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>Edit Employee Details</Typography>
+      <Card title="Edit Employee Details" style={{ margin: '15px' }}>
+        <Form onFinish={onFinishHandler} layout="vertical" style={{ margin: "18px" }}>
+          <Row gutter={20}>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                name="firstName"
+                label="First Name"
+              >
+                <Input type="text" placeholder={employeeDetails?.firstName}/>
+              </Form.Item>
+            </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                name="lastName"
+                label="Last Name"
+              >
+                  <Input type="text" placeholder={employeeDetails?.lastName} />
+                </Form.Item>
+              </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                name="username"
+                label="Username"
+              >
+                  <Input type="text" placeholder={employeeDetails?.username} />
+                </Form.Item>
+              </Col>
+            <Col xs={24} md={24} lg={8}>
+              <Form.Item
+                name="phone"
+                label="Phone Number"
+              >
+                <Input type="text" placeholder="Enter Employee Phone Number" />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <div>
+            <Button style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }} htmlType="submit" onClick={() => {navigate(-1)}}>Go Back</Button>
+          </div>
+          <div className="user_submit">
+            <Button style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }} htmlType="submit">Update</Button>
+          </div>
+        </Form>
+      </Card>
+    </div>
   )
 }
 
