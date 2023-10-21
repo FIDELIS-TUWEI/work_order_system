724f9348ada6f082f7b29689470a1cb3b8e412e0
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-21 11:01:43 +0300

    route to change password

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { signupUser, login, logout, getUserInfo, forgotPassword, resetPassword } = require("../controllers/authController");
+const { signupUser, login, logout, getUserInfo, forgotPassword, resetPassword, changePassword } = require("../controllers/authController");
 const { protect, restrict } = require("../middleware/authMiddleware");
 
 router.post("/register", protect, restrict(["admin", "superadmin"]), signupUser);
 router.post("/login",  login);
 router.post("/logout", logout);
 router.get("/userInfo", protect, getUserInfo);
+router.put("/update/password/:id", protect, restrict(["admin", "superadmin"]), changePassword);
 router.post("/forgot/password", protect, forgotPassword);
 router.patch("/reset/password/:token", protect, resetPassword);
 
