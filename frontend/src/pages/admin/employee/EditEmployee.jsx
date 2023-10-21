7f11d3f9253b60c801d88e745aacd2faf5f8eb11
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-21 17:33:02 +0300

    function to edit employee

--- frontend/src/pages/admin/employee/EditEmployee.jsx
+++ frontend/src/pages/admin/employee/EditEmployee.jsx
@@ -1,10 +1,60 @@
+import { useSelector } from "react-redux";
 import Layout from "../../../components/Layout";
 import UpdateEmployee from "../../../components/UpdateEmployee";
+import { selectToken } from "../../../utils/redux/slices/authSlice";
+import { useNavigate, useParams } from "react-router-dom";
+import { editEmployee, getEmployeeData } from "../../../services/employeeApi";
+import { useEffect, useState } from "react";
+import { message } from "antd";
 
 const EditEmployee = () => {
+  const token = useSelector(selectToken);
+  const [employeeDetails, setEmployeeDetails] = useState([]);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  // Function to handle form submit
+  const onFinishHandler = async (values) => {
+    const res = await editEmployee(id, values, {
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (res) {
+      message.success("Employee Updated Successfully");
+      navigate("/employees/all");
+    } else {
+      message.error("Employee Update Failed", error);
+    }
+  };
+
+  // Function to get single employee details
+  const getEmployeeDetails = async (id) => {
+    const res = await getEmployeeData(id, {
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    
+    setEmployeeDetails(res.data);
+  };
+
+  // UseEffect hook
+  useEffect(() => {
+    if (id) {
+      getEmployeeDetails(id);
+    }
+  }, [id]);
+
   return (
     <Layout>
-      <UpdateEmployee />
+      <UpdateEmployee 
+        onFinishHandler={onFinishHandler}
+        navigate={navigate}
+        employeeDetails={employeeDetails}
+      />
     </Layout>
   )
 }
