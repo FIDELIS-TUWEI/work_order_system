125d851f92c10c2719539d81c5c4a3dd2b4e1342
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-21 17:32:40 +0300

    employee route

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { newEmployee, getAllEmployees, singleEmployee, deleteEmployee, editEmployee, countWorkAssigned, queryAllEmployees, countEmployees } = require("../controllers/employeeController");
+const { newEmployee, getAllEmployees, singleEmployee, deleteEmployee, editEmployee, countWorkAssigned, queryAllEmployees, countEmployees, getEmployeeInfo } = require("../controllers/employeeController");
 const { protect, restrict } = require("../middleware/authMiddleware");
 
 router.post("/new/employee", protect, restrict(["admin", "superadmin"]), newEmployee);
 router.get("/all/employees", protect, restrict(["admin", "superadmin"]), getAllEmployees);
 router.get("/query/all/employees", protect, restrict(["admin", "superadmin", "hod", "reviewer", "engineer", "supervisor"]), queryAllEmployees);
 router.get("/single/employee/:id", protect, singleEmployee);
+router.get("/employee/data", protect, getEmployeeInfo);
 router.get("/employee/work/count", protect, restrict(["admin", "superadmin"]), countWorkAssigned);
 router.get("/count/employees", protect, countEmployees);
 router.put("/edit/employee/:id", protect, restrict(["admin", "superadmin"]), editEmployee);
