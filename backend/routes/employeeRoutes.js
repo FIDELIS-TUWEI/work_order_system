6cd35cfca638a45b1add78e68d10113fe6f31fbf
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-22 12:44:14 +0300

    backend code refactor

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -7,7 +7,7 @@ router.post("/new/employee", protect, restrict(["admin", "superadmin"]), newEmpl
 router.get("/all/employees", protect, restrict(["admin", "superadmin"]), getAllEmployees);
 router.get("/query/all/employees", protect, restrict(["admin", "superadmin", "hod", "reviewer", "engineer", "supervisor"]), queryAllEmployees);
 router.get("/single/employee/:id", protect, singleEmployee);
-router.get("/employee/data", protect, getEmployeeInfo);
+router.get("/employee/data/:id", protect, getEmployeeInfo);
 router.get("/employee/work/count", protect, restrict(["admin", "superadmin"]), countWorkAssigned);
 router.get("/count/employees", protect, countEmployees);
 router.put("/edit/employee/:id", protect, restrict(["admin", "superadmin"]), editEmployee);
