64e97af33259f7fcdb687ba058bbaf6761db5fff
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-23 14:07:55 +0300

    backend code refactor

--- backend/routes/workOrderRoutes.js
+++ backend/routes/workOrderRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createWorkOrder, updateWorkOrder, getAllWorkOrders, getSingleWorkOrder, deleteWorkOrder } = require('../controllers/workOrderController');
+const { createWorkOrder, updateWorkOrder, getAllWorkOrders, getSingleWorkOrder, deleteWorkOrder, queryAllWork } = require('../controllers/workOrderController');
 const { protect,  restrict, setReviewedBy} = require('../middleware/authMiddleware');
 
 
 router.post("/create/work", protect, restrict(["admin", "hod", "user", "supervisor", "superadmin", "reviewer", "engineer"]), createWorkOrder);
 router.get("/getall/work", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer"]), getAllWorkOrders);
+router.get("/query/all/work", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer"]), queryAllWork);
 router.get("/single/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "hod", "reviewer", "engineer"]), getSingleWorkOrder);
 router.put("/update/work/:id", protect, setReviewedBy, restrict(["admin", "supervisor", "superadmin", "reviewer", "engineer"]), updateWorkOrder);
 router.delete("/delete/work/:id", protect, restrict(["admin", "superadmin"]), deleteWorkOrder);
