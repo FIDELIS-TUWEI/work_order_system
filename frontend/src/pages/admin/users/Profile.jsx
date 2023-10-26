6393b6d0be7a78d378fdab7ed3171921fa3a396b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-26 09:15:21 +0300

    profile card display details changes and refactor

--- frontend/src/pages/admin/users/Profile.jsx
+++ frontend/src/pages/admin/users/Profile.jsx
@@ -1,10 +1,10 @@
-import { Button, Card, Typography } from 'antd'
+import { Button, Typography } from 'antd'
 import Layout from '../../../components/Layout'
 import { useSelector } from 'react-redux';
 import { selectUserInfo } from '../../../utils/redux/slices/authSlice';
 
-import { RxAvatar } from 'react-icons/rx';
 import { useNavigate } from 'react-router-dom';
+import Logo from "../../../assets/logo.png"
 
 
 const Profile = () => {
@@ -14,39 +14,57 @@ const Profile = () => {
 
   return (
     <Layout>
-      <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>Manage Profile</Typography>
-      <Card title="Profile" style={{ margin: ' auto 15px', width: '300px' }}>
-        <RxAvatar style={{ fontSize: '3rem' }} />
-        <p>
-          First Name: {user?.firstName}
-        </p>
-        <p>
-          Last Name: {user?.lastName}
-        </p>
-        <p>
-          Username: {user?.username}
-        </p>
-        <p>
-          Department: {user?.department}
-        </p>
-        <p>
-          Designation: {user?.designation}
-        </p>
-        <p>
-          Email: {user?.email}
-        </p>
-        <p>
-          Role: {user?.role}
-        </p>
-        <p>
-          Status: {user?.active === true ? "Active" : "Not Active"}
-        </p>
+      <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>Profile Details</Typography>
 
-        {user?.role === "admin" || user?.role === "superadmin" ? (
-          <Button style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }} onClick={() => {navigate(`/edit/user/${user._id}`)}}>Edit</Button>
-        )
-        : null}
-      </Card>
+      <div className="details--card">
+          <div className="company--logo">
+              <img src={Logo} alt="Logo" />
+          </div>
+
+          <div className="details--header">
+            <div className="details--header1">
+              <h2>First Name: {user?.firstName}</h2>
+              <p>Department: {user?.department}</p>
+            </div>
+            <div className="details--header2">
+              <h2>Last Name: {user?.lastName}</h2>
+              <p>Designation: {user?.designation}</p>
+            </div>
+          </div>
+
+          <hr />
+        <div className="details--grid">
+          <div className="details">
+            <span>Username:</span>
+            <span>{user?.username}</span>
+          </div>
+          <div className="details">
+            <span>Email:</span>
+            <span>{user?.email}</span>
+          </div>
+          <div className="details">
+            <span>Role:</span>
+            <span>{user?.role}</span>
+          </div>
+          <div className="details">
+            <span>Active</span>
+            <span>{user?.active === true ? "Active" : "Not Active"}</span>
+          </div>
+        </div>
+        {
+          user?.role === "admin" || user?.role === "superadmin" ? (
+            <Button 
+              style={{ 
+                color: 'white', backgroundColor: 'darkgreen', 
+                border: 'none', marginTop: "10px" 
+              }} 
+              onClick={() => {navigate(`/edit/user/${user._id}`)}}
+            >
+              Edit
+            </Button>
+          ) : null
+        }
+      </div>
     </Layout>
   )
 }
