e087d17b7b265720e4d356e16a0a12490ac8425a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-10-28 16:16:16 +0300

    query all designation route

--- backend/routes/designationRoutes.js
+++ backend/routes/designationRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createDesignation, getAllDesignations, deleteDesignation } = require('../controllers/designationController');
+const { createDesignation, getAllDesignations, deleteDesignation, queryAllDesignations } = require('../controllers/designationController');
 const { protect, restrict } = require('../middleware/authMiddleware');
 
 router.post("/new/designation", protect, restrict(["admin", "superadmin"]), createDesignation);
 router.get("/all-designations", protect, restrict(["admin", "superadmin"]), getAllDesignations);
+router.get("/query/all-designations", protect, restrict(["admin", "superadmin"]), queryAllDesignations);
 router.delete("/delete/designation/:id", protect, restrict(["admin", "superadmin"]), deleteDesignation);
 
 module.exports = router;
