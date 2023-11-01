ea1625a300338f05aecb95c09912d1a918b19f58
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-01 10:58:01 +0300

    function to fetch work

--- frontend/src/pages/admin/workOrders/AllWorkOrders.jsx
+++ frontend/src/pages/admin/workOrders/AllWorkOrders.jsx
@@ -1,12 +1,13 @@
-import { Button } from "antd"
+import { Button, message } from "antd"
 import Layout from "../../../components/Layout"
-import { getAllWorkOrders } from "../../../services/workApi";
+import { getAllWorkOrders, getWorkCalendar } from "../../../services/workApi";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import {GrFormNext, GrFormPrevious} from "react-icons/gr";
 
 import { selectToken, selectUserInfo } from "../../../utils/redux/slices/authSlice";
 import Work from "../../../components/Work";
+
 
 const AllWorkOrders = () => {
   const user = useSelector(selectUserInfo);
@@ -15,6 +16,8 @@ const AllWorkOrders = () => {
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [pages, setPages] = useState(1);
+  const [dateFilter, setDateFilter] = useState('day');
+
 
   // Function to get all work orders from Api
   const getAllWork = async () => {
@@ -28,20 +31,48 @@ const AllWorkOrders = () => {
       setAllWork(data);
       setPages(pages);
       setLoading(false);
+  };
+
+  // Fetch work orders with filter by date
+  const getFilteredWork = async (dateFilter) => {
+    try {
+      const res = await getWorkCalendar((dateFilter), {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      setDateFilter(res.data);
+    } catch (error) {
+      message.error(error.message)
+    }
   }
 
   useEffect(() => {
     getAllWork();
-  }, [page]);
+    getFilteredWork(dateFilter);
+  }, [page, dateFilter]);
 
   // function to handle page change
   const handlePageChange = (newPage) => {
     setPage(newPage);
   }
 
+  // Function to handle filter change
+  const handleFilterChange = (value) => {
+    setDateFilter(value);
+  }
+
   return (
     <Layout>
-      <Work allWork={allWork} user={user} loading={loading} getAllWork={getAllWork} />
+      <Work 
+        allWork={allWork} 
+        user={user} 
+        loading={loading} 
+        getAllWork={getAllWork} 
+        dateFilter={dateFilter}
+        handleFilterChange={handleFilterChange}
+      />
       
       <div className="pagination">
         <Button disabled={page === 1} onClick={() => handlePageChange(page - 1)} style={{ border: 'none', margin: '0 5px' }}>
