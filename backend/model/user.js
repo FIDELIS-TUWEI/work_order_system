7d4386524bf8114b9ee814c400ef3db34ed91838
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-03 11:31:49 +0300

    backend typo refactor

--- backend/model/user.js
+++ backend/model/user.js
@@ -46,7 +46,7 @@ const userSchema = new mongoose.Schema({
     },
     department: {
         type: ObjectId,
-        ref: "Deoartment"
+        ref: "Department"
     },
     designation: {
         type: ObjectId,
