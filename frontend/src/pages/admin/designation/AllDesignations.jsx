e1d2e691ff69c843199dcadbb4be4ec0902853e3
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-06 04:42:47 +0300

    useCallback hook used in functions fetching data

--- frontend/src/pages/admin/designation/AllDesignations.jsx
+++ frontend/src/pages/admin/designation/AllDesignations.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import Layout from '../../../components/Layout';
 import ViewAllDesignations from '../../../components/ViewAllDesignations';
 import { selectToken } from '../../../utils/redux/slices/authSlice';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { allDesignations } from '../../../services/designation';
 
 const AllDesignations = () => {
@@ -13,7 +13,7 @@ const AllDesignations = () => {
   const [loading, setLoading] = useState(false);
 
   // Function to get all designations
-  const getDesignations = async () => {
+  const getDesignations = useCallback (async () => {
     setLoading(true);
     const { data, pages } = await allDesignations(page, {
       withCredentials: true,
@@ -24,12 +24,12 @@ const AllDesignations = () => {
     setDesignations(data);
     setPages(pages);
     setLoading(false);
-  };
+  }, [token, page]);
 
   // UseEffect hook
   useEffect(() => {
     getDesignations();
-  }, [page]);
+  }, [page, getDesignations]);
 
   // Function to handle page change
   const handlePageChange = (newPage) => {
