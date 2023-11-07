c5174c670ec5e6e1a87a6412b993f8e5860c79f3
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-07 05:37:55 +0300

    error handling best practices UI refactor

--- frontend/src/pages/admin/users/Profile.jsx
+++ frontend/src/pages/admin/users/Profile.jsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from 'antd'
+import { Button, Typography, message } from 'antd'
 import Layout from '../../../components/Layout'
 import { useSelector } from 'react-redux';
 import { selectToken, selectUserInfo } from '../../../utils/redux/slices/authSlice';
@@ -18,13 +18,17 @@ const Profile = () => {
 
   // Function to get user data
   const getUserData = useCallback (async (id) => {
-    const res = await getUserInfo(id, {
-      withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    setUserData({...res.data});
+    try {
+      const res = await getUserInfo(id, {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUserData(res.data);
+    } catch (error) {
+      message.error("Failed to fetch user details", error.message);
+    }
   }, [token]);
 
   // useEffect hook
