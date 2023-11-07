151cff6888d0f05988da96fa055a95184832ec33
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-07 05:38:19 +0300

    error handling best practices UI refactor

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -6,6 +6,7 @@ import { countCompletedWork, countInProgressWork, countPendingWork, countReviewe
 import { useEffect, useState } from "react";
 import { countActiveUsers, countAllUsers } from "../../services/usersApi";
 import { countAllEmployees } from "../../services/employeeApi";
+import { message } from "antd";
 
 const Dashboard = () => {
   const user = useSelector(selectUserInfo);
@@ -21,90 +22,122 @@ const Dashboard = () => {
 
   // Function to get work orders with pending status from API Service
   const getPendingWorkCount = async () => {
-    const res = await countPendingWork({
-      withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    setPendingWorkCount(res.countPending);
+    try {
+      const res = await countPendingWork({
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setPendingWorkCount(res.countPending);
+    } catch (error) {
+      message.error("Error while fetching pending work count", error.message);
+    }
   };
 
   // Function to get work orders with In_Progress status from API Service
   const getInProgressWorkCount = async () => {
-    const res = await countInProgressWork({
-      withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    setInProgressCount(res.countInProgress);
+    try {
+      const res = await countInProgressWork({
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setInProgressCount(res.countInProgress);
+    } catch (error) {
+      message.error("Error while fetching in progress work count", error.message);
+    }
   };
 
   // Function to get work orders with completed status from API Service
   const getCompletedWorkCount = async () => {
-    const res = await countCompletedWork({
-      withCredentials: true,
-      headers: {
-        Authoization: `Bearer ${token}`,
-      },
-    });
-    setCompletedCount(res.countCompleted);
+    try {
+      const res = await countCompletedWork({
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setCompletedCount(res.countCompleted);
+    } catch (error) {
+      message.error("Error while fetching completed work count", error.message);
+    }
   };
 
   // Function to get work orders with reviewed status from API Service
   const getReviewedWorkCount = async () => {
-    const res = await countReviewedWork({
-      withCredentials: true,
-      headers: {
-        Authoization: `Bearer ${token}`,
-      },
-    });
-    setReviewedCount(res.countReviewed);
+    try {
+      const res = await countReviewedWork({
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setReviewedCount(res.countReviewed);
+    } catch (error) {
+      message.error("Error while fetching reviewed work count", error.message);
+    }
   };
 
   // Function to get total work count
   const getTotalWorkCount = async () => {
-    const res = await countTotalWork({
-      withCredentials: true,
-      headers: {
-        Authoization: `Bearer ${token}`,
-      },
-    });
-    setTotalWorkCount(res.totalWorkCount);
+    try {
+      const res = await countTotalWork({
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setTotalWorkCount(res.countTotal);
+    } catch (error) {
+      message.error("Error while fetching total work count", error.message);
+    }
   };
 
   // Function to get Total users count
   const getTotalUsersCount = async () => {
-    const res = await countAllUsers({
-      withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    setTotalUsersCount(res.totalUsers);
+    try {
+      const res = await countAllUsers({
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setTotalUsersCount(res.totalUsersCount);
+    } catch (error) {
+      message.error("Error while fetching total users count", error.message);
+    }
   };
 
   // Function to get total active users
   const getTotalActiveUsers = async () => {
-    const res = await countActiveUsers({
-      withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    setActiveUsersCount(res.activeUsersCount);
+    try {
+      const res = await countActiveUsers({
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setActiveUsersCount(res.activeUsersCount);
+    } catch (error) {
+      message.error("Error while fetching total active users count", error.message);
+    }
   };
 
   // Function to count all employees
   const getAllEmployees = async () => {
-    const res = await countAllEmployees({
-      withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    setEmployees(res.employeeCount);
+    try {
+      const res = await countAllEmployees({
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setEmployees(res.employeesCount);
+    } catch (error) {
+      message.error("Error while fetching total employees count", error.message);
+    }
   }
 
   // UseEffect hook
