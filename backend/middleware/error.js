f2fe2c95aff7ed616f027f1c6338eff671ab68e8
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-08 12:19:06 +0300

    error handker middlware refactor

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,32 +4,55 @@ const errorHandler = (err, req, res, next) => {
     let error = {...err};
     error.message = err.message;
 
+    // Handle Cast Error
     if (err.name === 'CastError') {
-        const message = "Resource not found";
+        const message = `Resource not found with ID of ${err.value}`;
         error = new ErrorResponse(message, 404);
     }
 
+    // Handle Axios Error (network error)
+    if (err.name === "AxiosError") {
+        const message = "Network Error, please Check your internet connection";
+        error = new ErrorResponse(message, 500);
+    };
+
+    // Handle Duplicate field value
     if (err.code === 11000) {
-        const message = "Duplicate field value entered";
+        const field = Object.values(err.keyValue)[0];
+        const message = `Duplicate ${field} value entered`;
         error = new ErrorResponse(message, 400);
     }
 
+    // Handle validation error
     if (err.name === "ValidationError") {
-        const message = Object.values(err.errors).map(value => value.message);
-        error = new ErrorResponse(message, 400);
+        const errors = Object.keys(err.errors).map((key) => {
+            return {
+                field: key,
+                message: err.errors[key].message
+            };
+        });
+        error = new ErrorResponse(errors, 400);
     }
 
-    res.status(error.statusCode || 500).json({
+    // Handle rate limit error (Too many requests)
+    if (err.name === "RateLimitError") {
+        const message = "Too many requests, Please wait a moment and try again";
+        error = new ErrorResponse(message, 429);
+        res.set("X-RateLimit-Retry-After", err.retryAfter);
+    };
+
+    // Handle other errors
+    if (!error.statusCode) {
+        error.statusCode = 500;
+        error.message = "Server Error, Please try again later";
+    }
+
+    res.status(error.statusCode).json({
         success: false,
-        error: error.message || "Server Error"
+        error: error.message
     });
 
-    if (err.code === 429) {
-        res.status(429).json({
-            success: false,
-            error: "Too many requests"
-        });
-    };
+
 
     next();
 }
