c4f14a9c2cc48e29b6dc307f0fc3bb8268a3a9f3
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-13 08:21:31 +0300

    frontend UI changes

--- frontend/src/components/FilterWorkDate.jsx
+++ frontend/src/components/FilterWorkDate.jsx
@@ -1,13 +1,71 @@
-import { Button } from "antd";
+import PropTypes from "prop-types";
+import { Button, Card, DatePicker } from "antd";
+import LoadingBox from "./LoadingBox";
+import moment from "moment";
 
-const FilterWorkDate = () => {
+const FilterWorkDate = ({ workFilterDate, filterWorkOrders, handleDateChange, loading }) => {
   return (
     <>
-      <div className="add-btn">
-        <Button>Calendar Filter</Button>
+    <Card title="Filter Work Orders By Date Created" style={{ margin: "15px" }}>
+      <div style={{ display: "flex", justifyContent: "flex-end", margin: "20px" }}>
+        <DatePicker onChange={handleDateChange} />
+        <Button
+          onClick={filterWorkOrders}
+          style={{
+            backgroundColor: "darkgreen",
+            color: "white",
+            border: "none",
+            marginLeft: "10px",
+          }}
+        >
+          Filter
+        </Button>
       </div>
+        
+        {loading ? (
+          <div style={{ display: "flex", justifyContent: "center", margin: "20px" }}>
+            <LoadingBox />
+          </div>
+        ) : (
+          <>
+            <table id="table">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Service Type</th>
+                  <th>Category</th>
+                  <th>Priority</th>
+                  <th>Assigned To</th>
+                  <th>Requested By</th>
+                  <th>Date Completed</th>
+                </tr>
+              </thead>
+              <tbody>
+                {workFilterDate.map((workOrder) => (
+                  <tr key={workOrder._id}>
+                    <td>{workOrder.title}</td>
+                    <td>{workOrder.serviceType}</td>
+                    <td>{workOrder.category ? workOrder.category.categoryTitle : ''}</td>
+                    <td>{workOrder.priority}</td>
+                    <td>{workOrder.assignedTo}</td>
+                    <td>{workOrder.requestedBy}</td>
+                    <td>{workOrder.dateCompleted ? moment(workOrder.dateCompleted).format("DD/MM/YYYY, hh:mm a") : 'Not yet complete'}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        )}
+    </Card>
     </>
   )
-}
+};
+
+FilterWorkDate.propTypes = {
+  workFilterDate: PropTypes.array,
+  filterWorkOrders: PropTypes.func,
+  handleDateChange: PropTypes.func,
+  loading: PropTypes.bool,
+};
 
 export default FilterWorkDate;
