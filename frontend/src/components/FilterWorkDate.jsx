c3391b88a6246f57f027a20ec2f571b6fe2a3646
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-15 22:04:44 +0300

    UI changes

--- frontend/src/components/FilterWorkDate.jsx
+++ frontend/src/components/FilterWorkDate.jsx
@@ -1,13 +1,12 @@
-import PropTypes from "prop-types";
-import { Button, Card, DatePicker } from "antd";
+import { Card, DatePicker } from "antd";
 import LoadingBox from "./LoadingBox";
 
 
-const FilterWorkDate = ({ loading, workOrders, handleDateChange, getWorkOrders }) => {
+const FilterWorkDate = () => {
   return (
     <Card title="Filter Work Orders By Date Created" style={{ margin: "15px" }} loading={loading}>
       <div style={{ display: "flex", justifyContent: "flex-end", margin: "20px" }}>
-        <DatePicker onChange={(date) => {handleDateChange(date); getWorkOrders();}} />
+        <DatePicker />
         
       </div>
         
@@ -28,28 +27,12 @@ const FilterWorkDate = ({ loading, workOrders, handleDateChange, getWorkOrders }
                 </tr>
               </thead>
               <tbody>
-                {workOrders.map((workOrder) => (
-                  <tr key={workOrder._id}>
-                    <td>{workOrder.title}</td>
-                    <td>{workOrder.serviceType}</td>
-                    <td>{workOrder.category ? workOrder.category.categoryTitle : ''}</td>
-                    <td>{workOrder.priority}</td>
-                    <td>{workOrder.assignedTo ? workOrder.assignedTo.firstName : 'Not Assigned'}</td>
-                    <td>{workOrder.requestedBy ? workOrder.requestedBy.username : 'Not Requested'}</td>
-                  </tr>
-                ))}
+                
               </tbody>
             </table>
         )}
     </Card>
   )
 };
-
-FilterWorkDate.propTypes = {
-  loading: PropTypes.bool,
-  workOrders: PropTypes.array,
-  handleDateChange: PropTypes.func,
-  getWorkOrders: PropTypes.func
-}
 
 export default FilterWorkDate;
