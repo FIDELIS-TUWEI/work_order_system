6329921f44a857fa0bc1558fdf79511b369b6361
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-15 13:07:17 +0300

    query work with date added field frontend

--- frontend/src/components/FilterWorkDate.jsx
+++ frontend/src/components/FilterWorkDate.jsx
@@ -2,11 +2,12 @@ import PropTypes from "prop-types";
 import { Button, Card, DatePicker } from "antd";
 import LoadingBox from "./LoadingBox";
 
-const FilterWorkDate = ({ loading, workOrders, handleDateChange, selectedDate }) => {
+
+const FilterWorkDate = ({ loading, workOrders, handleDateChange, getWorkOrders }) => {
   return (
-    <Card title="Filter Work Orders By Date Created" style={{ margin: "15px" }}>
+    <Card title="Filter Work Orders By Date Created" style={{ margin: "15px" }} loading={loading}>
       <div style={{ display: "flex", justifyContent: "flex-end", margin: "20px" }}>
-        <DatePicker onChange={handleDateChange} />
+        <DatePicker onChange={(date) => {handleDateChange(date); getWorkOrders();}} />
         
       </div>
         
@@ -24,7 +25,6 @@ const FilterWorkDate = ({ loading, workOrders, handleDateChange, selectedDate })
                   <th>Priority</th>
                   <th>Assigned To</th>
                   <th>Requested By</th>
-                  <th>Date Completed</th>
                 </tr>
               </thead>
               <tbody>
@@ -32,11 +32,10 @@ const FilterWorkDate = ({ loading, workOrders, handleDateChange, selectedDate })
                   <tr key={workOrder._id}>
                     <td>{workOrder.title}</td>
                     <td>{workOrder.serviceType}</td>
-                    <td>{workOrder.category}</td>
+                    <td>{workOrder.category ? workOrder.category.categoryTitle : ''}</td>
                     <td>{workOrder.priority}</td>
-                    <td>{workOrder.assignedTo}</td>
-                    <td>{workOrder.requestedBy}</td>
-                    <td>{workOrder.dateCompleted}</td>
+                    <td>{workOrder.assignedTo ? workOrder.assignedTo.firstName : 'Not Assigned'}</td>
+                    <td>{workOrder.requestedBy ? workOrder.requestedBy.username : 'Not Requested'}</td>
                   </tr>
                 ))}
               </tbody>
@@ -50,6 +49,7 @@ FilterWorkDate.propTypes = {
   loading: PropTypes.bool,
   workOrders: PropTypes.array,
   handleDateChange: PropTypes.func,
+  getWorkOrders: PropTypes.func
 }
 
 export default FilterWorkDate;
