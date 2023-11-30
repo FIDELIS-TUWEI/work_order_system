6b96fb4a78dbd93a84b4e21e843bbc78e2d90ac4
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-30 10:25:09 +0300

    cache middleware used in get work routes

--- backend/routes/workOrderRoutes.js
+++ backend/routes/workOrderRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { createWorkOrder, updateWorkOrder, getAllWorkOrders, getSingleWorkOrder, deleteWorkOrder, queryAllWork } = require('../controllers/workOrderController');
-const { protect,  restrict, setReviewedBy} = require('../middleware/authMiddleware');
+const { protect,  restrict, setReviewedBy, cacheMiddleware} = require('../middleware/authMiddleware');
 
 
 router.post("/create/work", protect, restrict(["admin", "hod", "user", "supervisor", "superadmin", "reviewer", "engineer"]), createWorkOrder);
-router.get("/getall/work", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer"]), getAllWorkOrders);
-router.get("/query/all/work", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer"]), queryAllWork);
-router.get("/single/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "hod", "reviewer", "engineer"]), getSingleWorkOrder);
+router.get("/getall/work", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer"]), cacheMiddleware, getAllWorkOrders);
+router.get("/query/all/work", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer"]), cacheMiddleware, queryAllWork);
+router.get("/single/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "hod", "reviewer", "engineer"]), cacheMiddleware, getSingleWorkOrder);
 router.put("/update/work/:id", protect, setReviewedBy, restrict(["admin", "supervisor", "superadmin", "reviewer", "engineer", "hod"]), updateWorkOrder);
 router.delete("/delete/work/:id", protect, restrict(["admin", "superadmin"]), deleteWorkOrder);
 
