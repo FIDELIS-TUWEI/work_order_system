bc93f6b88e9b942f8acecaa6d7d4397dca6815e2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-30 10:31:52 +0300

    cache middleware used routes

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { filterWorkStatus, countPendingWorkOrders, countInProgressWorkOrders, countCompletedWorkOrders, countReviewedWorkOrders, countAllWorkOrders } = require('../controllers/report');
-const { protect, restrict } = require('../middleware/authMiddleware');
+const { protect, restrict, cacheMiddleware } = require('../middleware/authMiddleware');
 
-router.get("/work", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer"]), filterWorkStatus);
-router.get("/count/pending-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), countPendingWorkOrders);
-router.get("/count/in-progress-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), countInProgressWorkOrders);
-router.get("/count/completed-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), countCompletedWorkOrders);
-router.get("/count/reviewed-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), countReviewedWorkOrders);
-router.get("/count/total-work", protect, restrict(["admin", "superadmin", "user", "hod", "engineer", "reviewer", "supervisor"]), countAllWorkOrders)
+router.get("/work", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer"]), cacheMiddleware, filterWorkStatus);
+router.get("/count/pending-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countPendingWorkOrders);
+router.get("/count/in-progress-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countInProgressWorkOrders);
+router.get("/count/completed-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countCompletedWorkOrders);
+router.get("/count/reviewed-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countReviewedWorkOrders);
+router.get("/count/total-work", protect, restrict(["admin", "superadmin", "user", "hod", "engineer", "reviewer", "supervisor"]), cacheMiddleware, countAllWorkOrders)
 
 
 module.exports = router;
