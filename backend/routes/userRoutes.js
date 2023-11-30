a0980cdb077828545b5fe1058fc64784cc03596d
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-30 10:31:57 +0300

    cache middleware used routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getAllUsers, singleUser, editUser, deleteUser, countAllUsers, countActiveUsers} = require('../controllers/userController');
-const { protect, restrict } = require('../middleware/authMiddleware');
+const { protect, restrict, cacheMiddleware } = require('../middleware/authMiddleware');
 
-router.get("/all-users", protect, restrict(['admin', "superadmin"]), getAllUsers);
-router.get("/single/user/:id", protect, singleUser);
-router.get("/count/total-users", protect, countAllUsers);
-router.get("/count/active-users", protect, countActiveUsers);
+router.get("/all-users", protect, restrict(['admin', "superadmin"]), cacheMiddleware, getAllUsers);
+router.get("/single/user/:id", protect, cacheMiddleware, singleUser);
+router.get("/count/total-users", protect, cacheMiddleware, countAllUsers);
+router.get("/count/active-users", protect, cacheMiddleware, countActiveUsers);
 router.put("/edit/:id", protect, restrict(['admin', "superadmin"]),  editUser);
 router.delete("/admin/user/delete/:id", protect, restrict(['admin', "superadmin"]), deleteUser);
 
