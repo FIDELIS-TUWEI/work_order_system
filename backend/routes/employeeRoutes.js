799d5a3c6c834ba3e4d413926d5021cbaf2f6c7e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-30 10:31:41 +0300

    cache middleware used routes

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { newEmployee, getAllEmployees, singleEmployee, deleteEmployee, editEmployee, countWorkAssigned, queryAllEmployees, countEmployees, getEmployeeInfo } = require("../controllers/employeeController");
-const { protect, restrict } = require("../middleware/authMiddleware");
+const { protect, restrict, cacheMiddleware } = require("../middleware/authMiddleware");
 
 router.post("/new/employee", protect, restrict(["admin", "superadmin"]), newEmployee);
-router.get("/all/employees", protect, restrict(["admin", "superadmin", "engineer"]), getAllEmployees);
-router.get("/query/all/employees", protect, restrict(["admin", "superadmin", "hod", "reviewer", "engineer", "supervisor"]), queryAllEmployees);
-router.get("/single/employee/:id", protect, singleEmployee);
-router.get("/employee/data/:id", protect, getEmployeeInfo);
-router.get("/employee/work/count", protect, restrict(["admin", "superadmin", "engineer"]), countWorkAssigned);
-router.get("/count/employees", protect, countEmployees);
+router.get("/all/employees", protect, restrict(["admin", "superadmin", "engineer"]), cacheMiddleware, getAllEmployees);
+router.get("/query/all/employees", protect, restrict(["admin", "superadmin", "hod", "reviewer", "engineer", "supervisor"]), cacheMiddleware, queryAllEmployees);
+router.get("/single/employee/:id", protect, cacheMiddleware, singleEmployee);
+router.get("/employee/data/:id", protect, cacheMiddleware, getEmployeeInfo);
+router.get("/employee/work/count", protect, cacheMiddleware, restrict(["admin", "superadmin", "engineer"]), countWorkAssigned);
+router.get("/count/employees", protect, cacheMiddleware, countEmployees);
 router.put("/edit/employee/:id", protect, restrict(["admin", "superadmin"]), editEmployee);
 router.delete("/delete/employee/:id", protect, restrict(["admin", "superadmin"]), deleteEmployee);
 
