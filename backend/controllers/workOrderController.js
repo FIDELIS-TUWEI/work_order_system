613a9e3ee47f8df688a9c36081458faccd9b9dfa
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-30 10:54:14 +0300

    pagination refactor

--- backend/controllers/workOrderController.js
+++ backend/controllers/workOrderController.js
@@ -220,7 +220,7 @@ async function sendUpdateEmailNotification (updatedWorkOrder, user) {
 // Get all Work Orders
 const getAllWorkOrders = asyncHandler (async (req, res, next) => {
     // Enable Pagination
-    const pageSize = 5;
+    const pageSize = 8;
     const page = Number(req.query.pageNumber) || 1;
     const count = await WorkOrder.find({}).estimatedDocumentCount();
     try {
