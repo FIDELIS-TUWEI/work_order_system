8104f62412053068c6a42a14df1586113b61d3a3
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-11-30 10:34:33 +0300

    database optimization

--- config/connectDB.js
+++ config/connectDB.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const mongoURI = process.env.MONGO_URI;
 const options = {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 }
 
 mongoose
