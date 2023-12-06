ad81a06c40f9a2b87512eab0eea2d9cc6a1ca1ac
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-12-06 20:25:44 +0300

    loading box added

--- frontend/src/pages/admin/users/ChangePassword.jsx
+++ frontend/src/pages/admin/users/ChangePassword.jsx
@@ -10,11 +10,13 @@ import { useNavigate, useParams } from "react-router-dom";
 const ChangePassword = () => {
   const token = useSelector(selectToken);
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const {id} = useParams();
 
   const onFinishHandler = async (values) => {
     try {
+      setLoading(true);
       await updateUserPassword(id, values, {
         withCredentials: true,
         headers: {
@@ -23,9 +25,10 @@ const ChangePassword = () => {
       });
       navigate('/work/list');
       message.success('Password Updated Successfully');
+      setLoading(false);
     } catch (error) {
-      console.log(error);
       message.error('Password Update Failed');
+      setLoading(false);
     }
   }
 
@@ -35,6 +38,7 @@ const ChangePassword = () => {
         onFinishHandler={onFinishHandler}
         password={password}
         setPassword={setPassword}
+        loading={loading}
       />
     </Layout>
   )
