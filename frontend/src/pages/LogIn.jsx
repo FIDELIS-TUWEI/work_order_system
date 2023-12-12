1de3e0752bdf3d43bd1dfc625e266135dff82522
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-12-12 21:58:27 +0300

    unnecessary fragment removed

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -39,34 +39,32 @@ const LogIn = () => {
     }
 
   return (
-    <>
-        <div className="form-container">
-            <Form onFinish={onFinishHandler} layout="vertical">
-                <Card title="Log In" style={{ width: "300px", margin: "auto", textAlign: "center" }}>
-                    <Form.Item name="username" label="Username" required rules={[{ required: true, message: 'Please input your username!' }]}>
-                        <Input 
-                            id="username" 
-                            type="text"
-                            placeholder="Username" 
-                        />
-                    </Form.Item>
+    <div className="form-container">
+        <Form onFinish={onFinishHandler} layout="vertical">
+            <Card title="Log In" style={{ width: "300px", margin: "auto", textAlign: "center" }}>
+                <Form.Item name="username" label="Username" required rules={[{ required: true, message: 'Please input your username!' }]}>
+                    <Input 
+                        id="username" 
+                        type="text"
+                        placeholder="Username" 
+                    />
+                </Form.Item>
 
-                    <Form.Item name="password" label="Password" required rules={[{ required: true, message: 'Please input your password!' }]}>
-                        <Input 
-                            id="password" 
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </Form.Item>
-                    <div className="loader">
-                        { isLoading && <LoadingBox /> }
-                    </div>
+                <Form.Item name="password" label="Password" required rules={[{ required: true, message: 'Please input your password!' }]}>
+                    <Input 
+                        id="password" 
+                        type="password"
+                        placeholder="Password"
+                    />
+                </Form.Item>
+                <div className="loader">
+                    { isLoading && <LoadingBox /> }
+                </div>
 
-                    <Button style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }} htmlType="submit">Log In</Button>
-                </Card>
-            </Form>
-        </div>
-    </>
+                <Button style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }} htmlType="submit">Log In</Button>
+            </Card>
+        </Form>
+    </div>
   )
 }
 
