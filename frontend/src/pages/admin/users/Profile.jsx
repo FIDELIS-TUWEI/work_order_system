27f49e36a53ccfcd3c2b36e1bbbabd1547414fba
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-12-16 20:28:56 +0300

    user profile

--- frontend/src/pages/admin/users/Profile.jsx
+++ frontend/src/pages/admin/users/Profile.jsx
@@ -105,7 +105,7 @@ const Profile = () => {
         </Tooltip>
         <Tooltip title="View Work History">
           <Button 
-            onClick={() => {navigate(`/workHistory/${userDetails?._id}`)}} 
+            onClick={() => {navigate(`/user/workHistory`)}} 
             style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }}
           >
             Work History
