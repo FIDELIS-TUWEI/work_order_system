8accf5b21dfe58950f7d4157f1a31050534c888a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-12-22 08:09:03 +0300

    go back button added

--- frontend/src/pages/admin/users/UserWorkHistory.jsx
+++ frontend/src/pages/admin/users/UserWorkHistory.jsx
@@ -1,15 +1,16 @@
-import { Table, Typography } from "antd";
+import { Button, Table, Typography } from "antd";
 import Layout from "../../../components/Layout";
 import { useSelector } from "react-redux";
 import { selectToken } from "../../../utils/redux/slices/authSlice";
 import { useCallback, useEffect, useState } from "react";
 import { getUserInfo } from "../../../services/usersApi";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const UserWorkHistory = () => {
     const token = useSelector(selectToken);
     const [userData, setUserData] = useState([]);
     const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
     const { id } = useParams();
 
     // Function to get user data
@@ -88,6 +89,14 @@ const UserWorkHistory = () => {
             pagination={false}
         />
       <Typography.Text>Total Work Orders: {userData?.workOrders?.length} </Typography.Text>
+      <div className="add-btn">
+        <Button 
+            onClick={() => navigate(-1)} 
+            style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }}
+        >
+            Back
+        </Button>
+      </div>
     </Layout>
   )
 };
