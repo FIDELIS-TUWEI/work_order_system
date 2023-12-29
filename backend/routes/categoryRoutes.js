051f50f14463dbbdb3cb5ab077a0de06cd3413a7
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-12-29 05:27:04 +0300

    query categories without pagination backend route updated

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createCategory, getAllCategories, updateCategory, deleteCategory } = require("../controllers/categoryController");
+const { createCategory, getAllCategories, updateCategory, deleteCategory, queryAllCategories } = require("../controllers/categoryController");
 const { protect, restrict, cacheMiddleware } = require("../middleware/authMiddleware");
 
 router.post("/new/category", protect, restrict(["admin", "superadmin", "supervisor"]), createCategory);
 router.get("/all/categories", protect, cacheMiddleware, getAllCategories);
-router.get("/query/all/categories", protect, restrict(["admin", "superadmin", "hod", "reviewer", "engineer", "user", "supervisor"]), cacheMiddleware, getAllCategories);
+router.get("/query/all/categories", protect, restrict(["admin", "superadmin", "hod", "reviewer", "engineer", "user", "supervisor"]), cacheMiddleware, queryAllCategories);
 router.put("/edit/category/:id", protect, restrict(["admin", "superadmin", "supervisor"]), updateCategory);
 router.delete("/delete/category/:id", protect, restrict(["admin", "superadmin", "supervisor"]), deleteCategory);
 
