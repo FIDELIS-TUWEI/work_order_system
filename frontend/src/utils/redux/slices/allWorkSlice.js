4bd3b03e2a3408b45fbbebfb5815f9451c66c233
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-01 14:51:26 +0300

    all work slice created

---
+++ frontend/src/utils/redux/slices/allWorkSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const workSlice = createSlice({
+    name: "work",
+    initialState: {
+        isLoading: false,
+        work: [],
+        error: false
+    },
+    reducers: {
+        workAdded (state, action) {
+            state.push(action.payload);
+        }
+    },
+});
+
+export const { workAdded } = workSlice.actions;
+
+export default workSlice.reducer;
