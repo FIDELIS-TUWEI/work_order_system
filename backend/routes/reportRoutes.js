40633a961b99721ea49bcc736949059ec605d34a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-02 16:08:53 +0300

    backend code refactor

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { filterWorkStatus, countPendingWorkOrders, countInProgressWorkOrders, countCompletedWorkOrders, countReviewedWorkOrders, countAllWorkOrders, fetchWorkOrdersByDate } = require('../controllers/report');
+const { filterWorkStatus, countPendingWorkOrders, countInProgressWorkOrders, countCompletedWorkOrders, countReviewedWorkOrders, countAllWorkOrders } = require('../controllers/report');
 const { protect, restrict, cacheMiddleware } = require('../middleware/authMiddleware');
 
 router.get("/work", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer"]), cacheMiddleware, filterWorkStatus);
-router.get("work/date-created/:date", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer"]), cacheMiddleware, fetchWorkOrdersByDate);
 router.get("/count/pending-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countPendingWorkOrders);
 router.get("/count/in-progress-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countInProgressWorkOrders);
 router.get("/count/completed-status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countCompletedWorkOrders);
