e603b5056ee54658e5d6f5fe3ec62e0dbb2cdd58
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-03 05:34:09 +0300

    create work using createasyncthunk redux

--- frontend/src/utils/redux/slices/allWorkSlice.js
+++ frontend/src/utils/redux/slices/allWorkSlice.js
@@ -1,17 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+const WORK_URL = "/hin";
+
+const initialState = {
+    work: [],
+    addWorkStatus: "",
+    addWorkError: "",
+    getWorkStatus: "",
+    getWorkError: "",
+    updateWorkStatus: "",
+    updateWorkError: "",
+    deleteWorkStatus: "",
+    deleteWorkError: ""
+};
+
+export const addWork = createAsyncThunk("work/addWork", async (work, { rejectWithValue }) => {
+    try {
+        const res = await axios.post(`${WORK_URL}/create/work`, work)
+        return res.data
+    } catch (error) {
+        return rejectWithValue(error.response.data)
+    }
+        
+})
 
 const workSlice = createSlice({
     name: "work",
-    initialState: {
-        isLoading: false,
-        work: [],
-        error: false
-    },
-    reducers: {
-        workAdded: (state, action) => {
-            state.push(action.payload);
-        }
-    },
+    initialState,
+    reducers: {},
+    extraReducers: {}
 });
 
 export const { workAdded } = workSlice.actions;
