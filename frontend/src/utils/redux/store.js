5114c6e42c8945fdd58db013eccf49acb347883d
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-04 22:15:56 +0300

    allWorkslice renamed to workSlice

--- frontend/src/utils/redux/store.js
+++ frontend/src/utils/redux/store.js
@@ -2,18 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../redux/slices/authSlice";
 import { apiSlice } from "../redux/slices/apiSlice";
 import queryWorkSlice from "./slices/queryWorkSlice";
-import allWorkSlice from "./slices/allWorkSlice";
 import {locationsApi} from "../redux/slices/locationSlice";
+import { workApi } from "./slices/workSlice";
 
 const store = configureStore({
     reducer: {
         auth: authSlice,
         queryWork: queryWorkSlice,
-        work: allWorkSlice,
         [apiSlice.reducerPath]: apiSlice.reducer,
+        [workApi.reducerPath]: workApi.reducer,
         [locationsApi.reducerPath]: locationsApi.reducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware, locationsApi.middleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware, workApi.middleware, locationsApi.middleware),
     devTools: true
 });
 
