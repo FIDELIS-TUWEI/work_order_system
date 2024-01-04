a7fce1291af51ab9af2b750d6af49e598a33fc7b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-04 21:29:21 +0300

    display error message from backend

--- frontend/src/pages/admin/locations/NewLocation.jsx
+++ frontend/src/pages/admin/locations/NewLocation.jsx
@@ -2,20 +2,35 @@ import { useState } from "react";
 import CreateLocation from "../../../components/CreateLocation"
 import Layout from "../../../components/Layout"
 import { useNavigate } from "react-router-dom";
-import { createNewLocation } from "../../../services/locationApi";
 import { message } from "antd";
+import { useCreateLocationMutation } from "../../../utils/redux/slices/locationSlice";
 
 const NewLocation = () => {
+    const [addLocation] = useCreateLocationMutation();
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
     // Function to handle form submit
     const onFinishHandler = async (values) => {
-        setLoading(true);
-        await createNewLocation(values);
-        navigate("/all-locations");
-        message.success("New Location Created Succesfully");
-        setLoading(false);
+        try {
+          setLoading(true);
+          const { error } = await addLocation(values);
+
+          if (error) {
+            if (error.status === 400 && error.data && error.data.message) {
+              message.error(error.data.message);
+              navigate("/all-locations");
+            } else {
+              message.error("Failed to create new location");
+            }
+          } else {
+            navigate("/all-locations");
+            message.success("New Location Created Succesfully");
+            setLoading(false);
+          }
+        } catch (error) {
+          message.error(error.message);
+        }
     }
 
   return (
