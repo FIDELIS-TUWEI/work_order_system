0feb0ba138414aeea2aab339cde199512e57af12
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-04 22:16:34 +0300

    new work component code refactor

--- frontend/src/pages/admin/locations/NewLocation.jsx
+++ frontend/src/pages/admin/locations/NewLocation.jsx
@@ -12,25 +12,25 @@ const NewLocation = () => {
 
     // Function to handle form submit
     const onFinishHandler = async (values) => {
-        try {
-          setLoading(true);
-          const { error } = await addLocation(values);
+      try {
+        setLoading(true);
+        const { error } = await addLocation(values);
 
-          if (error) {
-            if (error.status === 400 && error.data && error.data.message) {
-              message.error(error.data.message);
-              navigate("/all-locations");
-            } else {
-              message.error("Failed to create new location");
-            }
-          } else {
+        if (error) {
+          if (error.status === 400 && error.data && error.data.message) {
+            message.error(error.data.message);
             navigate("/all-locations");
-            message.success("New Location Created Succesfully");
-            setLoading(false);
+          } else {
+            message.error("Failed to create new location");
           }
-        } catch (error) {
-          message.error(error.message);
+        } else {
+          navigate("/all-locations");
+          message.success("New Location Created Succesfully");
+          setLoading(false);
         }
+      } catch (error) {
+        message.error(error.message);
+      }
     }
 
   return (
