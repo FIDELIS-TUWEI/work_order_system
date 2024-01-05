6d551db4dd13f2b8428980be1c2d6b333735bcb1
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-05 08:24:08 +0300

    redux slice import path refactored

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './utils/redux/store';
+import store from './features/store';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/route';
 
