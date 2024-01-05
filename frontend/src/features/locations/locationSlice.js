4a9a6fed3f98fde61911e5433ba0bad73b5f6f54
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-05 08:23:19 +0300

    redux rtk actions imports refactor

---
+++ frontend/src/features/locations/locationSlice.js
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const WORK_URL = "/hin";
+
+export const locationsApi = createApi({
+    reducerPath: "locationsApi",
+    baseQuery: fetchBaseQuery({ baseUrl: WORK_URL }),
+    endpoints: (builder) => ({
+        createLocation: builder.mutation({
+            query: (values) => ({
+                url: `/create/location`,
+                method: "POST",
+                body: values,
+            }),
+        }),
+        allLocations: builder.query({
+            query: (page) => ({
+                url: `/all-locations?pageNumber=${page}`,
+                method: "GET",
+            }),
+        }),
+        queryAllLocations: builder.query({
+            query: () => ({
+                url: `/search/location`,
+                method: "GET",
+            }),
+        }),
+        deleteLocation: builder.mutation({
+            query: (id) => ({
+                url: `/location/${id}`,
+                method: "DELETE",
+            }),
+        })
+    })
+});
+
+export const { 
+    useCreateLocationMutation, 
+    useQueryAllLocationsQuery, 
+    useAllLocationsQuery, 
+    useDeleteLocationMutation 
+} = locationsApi;
