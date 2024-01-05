ef2e42ba715b65dc5812169ecf5dfeffeae97826
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-05 08:23:07 +0300

    redux rtk actions imports refactor

---
+++ frontend/src/features/auth/authApiSlice.js
@@ -0,0 +1,25 @@
+import { apiSlice } from "../api/apiSlice";
+const USERS_URL = "/hin";
+
+export const authApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: (data) => ({
+                url: `${ USERS_URL }/login`,
+                method: "POST",
+                body: data,
+            }),
+        }),
+        logout: builder.mutation({
+            query: () => ({
+                url: `${ USERS_URL }/logout`,
+                method: "POST",
+            }),
+        }),
+    })
+});
+
+export const { 
+    useLoginMutation, 
+    useLogoutMutation, 
+} = authApiSlice;
