9e3513b73a255d195cced2042baaf80fb930e8b8
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-06 11:27:14 +0300

    baseQuery with prepareHeaders added

--- frontend/src/features/work/workSlice.js
+++ frontend/src/features/work/workSlice.js
@@ -2,9 +2,22 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const WORK_URL = "/hin";
 
+const baseQuery = fetchBaseQuery({
+    baseUrl: WORK_URL,
+    credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+        const token = getState().auth.token;
+        if (token) {
+            headers.set("authorization", `Bearer ${token}`);
+        }
+        return headers;
+    },
+})
+
 export const workApi = createApi({
     reducerPath: "workApi",
-    baseQuery: fetchBaseQuery({ baseUrl: WORK_URL }),
+    baseQuery,
+    tagTypes: ["WorkOrder"],
     endpoints: (builder) => ({
         createWork: builder.mutation({
             query: (values) => ({
