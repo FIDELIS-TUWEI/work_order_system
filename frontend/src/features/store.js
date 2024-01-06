a77e035c2fef3f4a80d4c7f54d62d25f2b829278
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-06 11:07:32 +0300

    redux slice imports deleted

--- frontend/src/features/store.js
+++ frontend/src/features/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./auth/authSlice";
 import { apiSlice } from "./api/apiSlice";
-import queryWorkSlice from "./work/queryWorkSlice";
 import {locationsApi} from "./locations/locationSlice";
 import { workApi } from "./work/workSlice";
 
 const store = configureStore({
     reducer: {
         auth: authSlice,
-        queryWork: queryWorkSlice,
         [apiSlice.reducerPath]: apiSlice.reducer,
         [workApi.reducerPath]: workApi.reducer,
         [locationsApi.reducerPath]: locationsApi.reducer,
