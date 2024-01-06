e3f55a380ec9c5b0912790f889d0a579acb08a1b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-06 13:26:53 +0300

    update mutation

--- frontend/src/features/work/workSlice.js
+++ frontend/src/features/work/workSlice.js
@@ -25,37 +25,44 @@ export const workApi = createApi({
                 method: "POST",
                 body: values,
             }),
+            invalidatesTags: ["WorkOrder"],
         }),
         WorkOrders: builder.query({
             query: (page) => ({
                 url: `/getall/work?pageNumber=${page}`,
                 method: "GET",
             }),
+            providesTags: ["WorkOrder"],
         }),
         singleWork: builder.query({
             query: (id) => ({
                 url: `/single/work/${id}`,
                 method: "GET",
             }),
+            providesTags: ["WorkOrder"],
         }),
         queryAllWork: builder.query({
             query: () => ({
                 url: `/query/all/work`,
                 method: "GET",
             }),
+            providesTags: ["WorkOrder"],
         }),
         updateWork: builder.mutation({
-            query: ({ id, values }) => ({
+            query: ({ id, ...values }) => ({
                 url: `/update/work/${id}`,
                 method: "PUT",
                 body: values,
             }),
+            invalidatesTags: ["WorkOrder"],
         }),
         deleteWork: builder.mutation({
             query: (id) => ({
                 url: `/delete/work/${id}`,
                 method: "DELETE",
+                body: id,
             }),
+            invalidatesTags: ["WorkOrder"],
         })
     })
 });
