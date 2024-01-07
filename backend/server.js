330fc4f2ef36ac9aea08b1d024f6a72442faf4f2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-07 15:40:04 +0300

    server configuration update

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,14 @@ app.use(cors({
     origin: "http://localhost:3000"
 }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 app.use(express.json()); // To parse JSON data in the request body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the request body
 // Prevent HTTP Parameter pollution
