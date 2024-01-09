e24a4e4ec536b78cda535c3ed9927b84cf9b374c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-09 10:04:29 +0300

    fetching and displaying paginated data with rtk query

--- frontend/src/pages/admin/workOrders/AllWorkOrders.jsx
+++ frontend/src/pages/admin/workOrders/AllWorkOrders.jsx
@@ -11,63 +11,31 @@ import { useWorkOrdersQuery } from "@/features/work/workSlice";
 
 
 const AllWorkOrders = () => {
-  const { data: workOrders, isLoading, error } = useWorkOrdersQuery(1);
   const user = useSelector(selectUserInfo);
-  const token = useSelector(selectToken);
-  const [allWork, setAllWork] = useState([]);
   const [page, setPage] = useState(1);
-  const [pages, setPages] = useState(1);
-  const [loading, setLoading] = useState(false);
+  const { data: workOrders, isLoading, error } = useWorkOrdersQuery(page);
 
-  console.log("All WorkOrders : ", workOrders);
+  const workOrdersArray = workOrders?.data || [];
+  
+  console.log("All WorkOrders : ", workOrdersArray);
   console.log("Loading :", isLoading);
   console.log("Error : ", error);
-
-  // Function to get all work orders from API
-  const getAllWork = async () => {
-    try {
-      setLoading(true);
-      const { data, pages } = await getAllWorkOrders(page, {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setAllWork(data);
-      setPages(pages);
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      message.error("Failed to fetch work orders", error.message);
-    }
-  };
-
-  // UseEffect hook
-  useEffect(() => {
-    getAllWork();
-  }, [page]);
-
-  // function to handle page change
-  const handlePageChange = (newPage) => {
-    setPage(newPage);
-  }
 
   return (
     <Layout>
       <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>All Work Orders</Typography>
       <Work 
-        allWork={allWork} 
+        workOrdersArray={workOrdersArray}
         user={user} 
-        loading={loading}
-        getAllWork={getAllWork} 
+        isLoading={isLoading}
       />
       
       <div className="pagination">
-        <Button disabled={page === 1} onClick={() => handlePageChange(page - 1)} style={{ border: 'none', margin: '0 5px', backgroundColor: 'lightgrey' }}>
+        <Button disabled={page === 1} onClick={() => setPage(page - 1)} style={{ border: 'none', margin: '0 5px', backgroundColor: 'lightgrey' }}>
           <GrFormPrevious />
         </Button>
-        <span> Page {page} of {pages}</span>
-        <Button disabled={page === pages} onClick={() => handlePageChange(page + 1)} style={{ border: 'none', margin: '0 5px', backgroundColor: 'lightgrey' }}>
+        <span> Page {page} of {workOrdersArray?.pages || 1}</span>
+        <Button disabled={page === workOrdersArray?.pages || 1} onClick={() => setPage(page + 1)} style={{ border: 'none', margin: '0 5px', backgroundColor: 'lightgrey' }}>
           <GrFormNext />
         </Button>
       </div>
