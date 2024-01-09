426100febf206b8debf050bd4287fad8fd67f6cb
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-09 10:37:38 +0300

    success fetch paginated data and pages

--- frontend/src/pages/admin/workOrders/AllWorkOrders.jsx
+++ frontend/src/pages/admin/workOrders/AllWorkOrders.jsx
@@ -1,23 +1,36 @@
 import { Button, Typography, message } from "antd"
 import Layout from "@/components/Layout";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import {GrFormNext, GrFormPrevious} from "react-icons/gr";
 
 import Work from "@/pages/admin/workOrders/Work";
 import { useWorkOrdersQuery } from "@/features/work/workSlice";
+import { selectUserInfo } from "@/features/auth/authSlice";
 
 
 const AllWorkOrders = () => {
   const user = useSelector(selectUserInfo);
   const [page, setPage] = useState(1);
-  const { data: workOrders, isLoading: loading, error } = useWorkOrdersQuery(page);
+  const { data, isLoading: loading, error, refetch } = useWorkOrdersQuery(page);
 
-  const workOrdersArray = workOrders?.data || [];
+  const { data: workOrdersArray, pages } = data || {};
+
+  console.log("All Work Orders:", workOrdersArray);
+
  // Handle errors
- if (error) {
-    message.error(error.message);
-  };
+ useEffect(() => {
+    if (error) {
+      message.error(error.message);
+    };
+ }, [error]);
+
+ // Handle pagination
+ const handlePageChange = (newPage) => {
+   setPage(newPage);
+   refetch();
+ }
+ 
 
   return (
     <Layout>
@@ -26,14 +39,15 @@ const AllWorkOrders = () => {
         workOrdersArray={workOrdersArray}
         user={user} 
         loading={loading}
+        refetch={refetch}
       />
       
       <div className="pagination">
-        <Button disabled={page === 1} onClick={() => setPage(page - 1)} style={{ border: 'none', margin: '0 5px', backgroundColor: 'lightgrey' }}>
+        <Button disabled={page === 1} onClick={() => handlePageChange(page - 1)} style={{ border: 'none', margin: '0 5px', backgroundColor: 'lightgrey' }}>
           <GrFormPrevious />
         </Button>
-        <span> Page {page} of {workOrdersArray?.pages || 1}</span>
-        <Button disabled={page === workOrdersArray?.pages || 1} onClick={() => setPage(page + 1)} style={{ border: 'none', margin: '0 5px', backgroundColor: 'lightgrey' }}>
+        <span> Page {page} of {pages}</span>
+        <Button disabled={page === pages} onClick={() => handlePageChange(page + 1)} style={{ border: 'none', margin: '0 5px', backgroundColor: 'lightgrey' }}>
           <GrFormNext />
         </Button>
       </div>
