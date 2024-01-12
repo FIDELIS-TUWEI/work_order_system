c2f57dcd918d8e882fec6f418c1898291728a1a2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-12 05:44:26 +0300

    categories rtk createApi actions config

--- frontend/src/features/categories/categorySlice.js
+++ frontend/src/features/categories/categorySlice.js
@@ -12,4 +12,55 @@ const baseQuery = fetchBaseQuery({
         }
         return headers;
     },
-})
+});
+
+export const categoriesApi = createApi({
+    reducerPath: "categoriesApi",
+    baseQuery,
+    endpoints: (builder) => ({
+        createCategory: builder.mutation({
+            query: (values) => ({
+                url: `/new/category`,
+                method: "POST",
+                body: values,
+            }),
+            invalidatesTags: ["Categories"],
+        }),
+        allCategories: builder.query({
+            query: (page) => ({
+                url: `/all/categories?pageNumber=${page}`,
+                method: "GET",
+            }),
+            providesTags: ["Categories"],
+        }),
+        queryAllCategories: builder.query({
+            query: () => ({
+                url: `/query/all/categories`,
+                method: "GET",
+            }),
+            providesTags: ["Categories"],
+        }),
+        updateCategory: builder.mutation({
+            query: ({id, values}) => ({
+                url: `/edit/category/${id}`,
+                method: "PUT",
+                body: values,
+            }),
+            invalidatesTags: ["Categories"],
+        }),
+        deleteCategory: builder.mutation({
+            query: (id) => ({
+                url: `/delete/category/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["Categories"],
+        }),
+    })
+});
+
+export const {
+    useCreateCategoryMutation,
+    useAllCategoriesQuery,
+    useQueryAllCategoriesQuery,
+    useDeleteCategoryMutation,
+} = categoriesApi;
