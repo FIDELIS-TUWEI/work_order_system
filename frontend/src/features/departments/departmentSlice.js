8104904d271d03ab4fd2f8fba26e3491daa266da
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-12 05:20:14 +0300

    departments rtk createApi actions config

--- frontend/src/features/departments/departmentSlice.js
+++ frontend/src/features/departments/departmentSlice.js
@@ -14,3 +14,47 @@ const baseQuery = fetchBaseQuery({
     },
 });
 
+export const departmentsApi = createApi({
+    reducerPath: "departmentsApi",
+    baseQuery,
+    tagTypes: ["Departments"],
+    endpoints: (builder) => ({
+        createDepartment: builder.mutation({
+            query: (values) => ({
+                url: `/new/department`,
+                method: "POST",
+                body: values,
+            }),
+            invalidatesTags: ["Departments"],
+        }),
+        allDepartments: builder.query({
+            query: (page) => ({
+                url: `/all-departments?pageNumber=${page}`,
+                method: "GET",
+            }),
+            providesTags: ["Departments"],
+        }),
+        queryAllDepartments: builder.query({
+            query: () => ({
+                url: `/query/all/departments`,
+                method: "GET",
+            }),
+            providesTags: ["Departments"],
+        }),
+        deleteDepartment: builder.mutation({
+            query: (id) => ({
+                url: `/delete/department/${id}`,
+                method: "DELETE",
+                body: id,
+            }),
+            invalidatesTags: ["Departments"]
+        }),
+    })
+});
+
+export const {
+    useCreateDepartmentMutation,
+    useAllDepartmentsQuery,
+    useQueryAllDepartmentsQuery,
+    useDeleteDepartmentMutation,
+} = departmentsApi;
