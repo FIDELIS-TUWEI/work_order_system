cbc097784c8d86648f81691486b6a71c58fed68e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-13 11:25:15 +0300

    edit user api endpoint refactor

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,7 +7,7 @@ router.get("/all-users", protect, restrict(['admin', "superadmin"]), cacheMiddle
 router.get("/single/user/:id", protect, cacheMiddleware, singleUser);
 router.get("/count/total-users", protect, cacheMiddleware, countAllUsers);
 router.get("/count/active-users", protect, cacheMiddleware, countActiveUsers);
-router.put("/edit/:id", protect, restrict(['admin', "superadmin"]),  editUser);
+router.put("/edit/user/:id", protect, restrict(['admin', "superadmin"]),  editUser);
 router.delete("/admin/user/delete/:id", protect, restrict(['admin', "superadmin"]), deleteUser);
 
 
