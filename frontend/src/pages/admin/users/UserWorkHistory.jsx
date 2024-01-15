d561d1f00b16c5e6c03b8cb1500f7db10e24a134
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-15 06:42:28 +0300

    logic to fetch user work history using redux rtk query

--- frontend/src/pages/admin/users/UserWorkHistory.jsx
+++ frontend/src/pages/admin/users/UserWorkHistory.jsx
@@ -1,98 +1,23 @@
-import { Button, Card, Table, Typography } from "antd";
 import Layout from "@/components/Layout";
-import { useSelector } from "react-redux";
-import { selectToken } from "@/features/auth/authSlice";
-import { useCallback, useEffect, useState } from "react";
-import { getUserInfo } from "../../../services/usersApi";
 import { useNavigate, useParams } from "react-router-dom";
+import { useGetSingleUserQuery } from "@/features/users/userSlice";
+import UserWorkInfo from "./UserWorkInfo";
 
 const UserWorkHistory = () => {
-    const token = useSelector(selectToken);
-    const [userData, setUserData] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
     const { id } = useParams();
+    const { data: userHistory, isLoading: loading, error } = useGetSingleUserQuery(id);
+    const navigate = useNavigate();
 
-    const userDataArray = userData?.workOrders || [];
-
-    // Function to get user data
-    const getUserData = useCallback (async () => {
-        try {
-            setLoading(true);
-            const res = await getUserInfo(id, {
-                withCredentials: true,
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            setUserData(res.data);
-            setLoading(false);
-        } catch (error) {
-            setLoading(false);
-            message.error("Failed to fetch user details", error.message);
-        }
-    }, [token]);
-
-
-    // useEffect hook
-    useEffect(() => {
-        if (id) {
-            getUserData(id);
-        }
-    }, [id, getUserData]);
-
-    // antd Columns
-    const columns = [
-        {
-            title: "Description",
-            responsive: ["md", "lg"],
-            align: "center",
-            dataIndex: "description",
-            key: "description",
-            
-        },
-        {
-            title: "Status",
-            responsive: ["md", "lg"],
-            align: "center",
-            dataIndex: "status",
-            key: "status",
-        },
-        {
-            title: "Tracker",
-            responsive: ["md", "lg"],
-            align: "center",
-            dataIndex: "tracker",
-            key: "tracker",
-        }
-    ]
+    const userHistoryArray = userHistory?.data || [];
 
   return (
     <Layout>
-        <Typography 
-            style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}
-        >
-            Work History
-        </Typography>
-
-        <Card>
-        <Table 
-            columns={columns}
-            dataSource={userDataArray}
-            rowKey="_id"
+        <UserWorkInfo 
+            userHistoryArray={userHistoryArray}
+            navigate={navigate}
             loading={loading}
-            pagination={false}
+            error={error}
         />
-        </Card>
-      <Typography.Text>Total Work Orders: {userData?.workOrders?.length} </Typography.Text>
-      <div className="add-btn">
-        <Button 
-            onClick={() => navigate(-1)} 
-            style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none' }}
-        >
-            Back
-        </Button>
-      </div>
     </Layout>
   )
 };
