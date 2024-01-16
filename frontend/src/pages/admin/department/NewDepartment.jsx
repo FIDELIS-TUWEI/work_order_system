dd801f7189898d7d6580799ee7f32dc46dd6f964
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-16 15:39:18 +0300

    logic to create department using redux rtk mutation

--- frontend/src/pages/admin/department/NewDepartment.jsx
+++ frontend/src/pages/admin/department/NewDepartment.jsx
@@ -1,21 +1,32 @@
-import { useState } from 'react';
 import CreateDepartment from "@/pages/admin/department/CreateDepartment";
 import Layout from "@/components/Layout";
 import { useNavigate } from 'react-router-dom';
 import { message } from 'antd';
-import { createNewDepartment } from '../../../services/departmentApi';
+import { useCreateDepartmentMutation } from '@/features/departments/departmentSlice';
 
 const NewDepartment = () => {
-  const [loading, setLoading] = useState(false);
+  const [newDepartment, { isLoading: loading }] = useCreateDepartmentMutation();
   const navigate = useNavigate();
 
   // Function to handle form submit
   const onFinishHandler = async (values) => {
-    setLoading(true);
-    await createNewDepartment(values);
-    navigate("/all/departments");
-    message.success("New Department Created Succesfully");
-    setLoading(false);
+    try {
+      const { error } = await newDepartment(values).unwrap();
+
+      if (error) {
+        if (error === 400 && error.data && error.data.message) {
+          message.error(error.data.message);
+          navigate("/all/departments");
+        } else {
+          message.error("Failed to create new department")
+        }
+      } else {
+        message.success("New Department Created Succesfully");
+        navigate("/all/departments");
+      }
+    } catch (error) {
+      message.error(error.message);
+    }
   };
 
   return (
@@ -27,6 +38,6 @@ const NewDepartment = () => {
       />
     </Layout>
   )
-}
+};
 
 export default NewDepartment;
