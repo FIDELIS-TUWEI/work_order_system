b37ee39461ea043af567a0a24e0f5c8d2eea50f6
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-16 09:22:57 +0300

    fetch work reports redux rtk query logic

---
+++ frontend/src/features/reports/reportSlice.js
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const WORK_URL = "/hin";
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: WORK_URL,
+    credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+        const token = getState().auth.token;
+        if (token) {
+            headers.set("authorization", `Bearer ${token}`);
+        }
+        return headers;
+    }
+});
+
+export const reportsApi = createApi({
+    reducerPath: "reportsApi",
+    baseQuery,
+    tagTypes: ["Reports"],
+    endpoints: (builder) => ({
+        getFilterStatus: builder.query ({
+            query: () => ({
+                url: `/work`,
+                method: "GET",
+            }),
+            providesTags: ["Reports"],
+        }),
+    })
+});
+
+export const {
+    useGetFilterStatusQuery,
+} = reportsApi;
