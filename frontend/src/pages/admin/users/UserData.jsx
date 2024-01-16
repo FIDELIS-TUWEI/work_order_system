38091e99b8f54c0fc4f21276d58bf72f69f3ba87
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-16 07:49:21 +0300

    view user data separated from fetching logic

---
+++ frontend/src/pages/admin/users/UserData.jsx
@@ -0,0 +1,67 @@
+
+const UserData = () => {
+  return (
+    <>
+      <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold', marginBottom: '10px' }}>
+        User Details
+      </Typography>
+      <div className="details--card">
+        <div className="company--logo">
+            <img src={Logo} alt="Logo" />
+        </div>
+
+        <div className="details--header">
+          <div className="details--header1">
+            <h2>First Name: {userDetails?.firstName}</h2>
+            <p>Department: {userDetails?.department?.departmentName}</p>
+          </div>
+          <div className="details--header2">
+            <h2>Last Name: {userDetails?.lastName}</h2>
+            <p>Designation: {userDetails?.designation?.designationName}</p>
+          </div>
+        </div>
+
+        <hr />
+        <div className="details--grid">
+          <div className="details">
+            <span>Username:</span>
+            <span>{userDetails?.username}</span>
+          </div>
+          <div className="details">
+            <span>Email:</span>
+            <span>{userDetails?.email}</span>
+          </div>
+          <div className="details">
+            <span>Role:</span>
+            <span>{userDetails?.role}</span>
+          </div>
+          <div className="details">
+            <span>Active:</span>
+            <span>{userDetails?.active === true ? "Active" : "Not Active"}</span>
+          </div>
+        </div>
+        <div className="add-btn">
+          <Tooltip title="Back">
+            <Button 
+              style={{ color: 'white', backgroundColor: 'darkgreen', border: 'none', marginLeft: '10px' }} 
+              onClick={() => {navigate(-1)}}
+            >
+              Back
+            </Button>
+          </Tooltip>
+          <Tooltip title={user?.role !== "superadmin" ? "Only System admin can change password" : "Edit Password"}>
+            <Button 
+              onClick={() => {navigate(`/updatePassword/${userDetails?._id}`)}} 
+              disabled={user?.role !== "superadmin"}
+              style={{ color: 'black', backgroundColor: 'white', border: '1px solid black', marginLeft: '10px' }}
+            >
+              Change Password
+            </Button>
+          </Tooltip>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default UserData;
