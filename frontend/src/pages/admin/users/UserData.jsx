54e2000d27c7a4b4b863e81728058e469232e046
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-16 08:12:58 +0300

    logic to display single user details

--- frontend/src/pages/admin/users/UserData.jsx
+++ frontend/src/pages/admin/users/UserData.jsx
@@ -1,5 +1,29 @@
+import PropTypes from "prop-types";
+import { Button, Tooltip, Typography } from 'antd';
+import Logo from "@/assets/images/logo.png";
+import LoadingBox from "@/components/LoadingBox";
 
-const UserData = () => {
+
+const UserData = ({ userInfoArray, navigate, user, loading, error }) => {
+  // Conditional statement to display loading and error messages
+  if (loading) {
+    return <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <LoadingBox />
+    </div>;
+  };
+
+  if (error) {
+    return message.error(error);
+  };
+
+  const departmentTitle = userInfoArray.department 
+    ? `${userInfoArray.department.departmentName}`
+    : "No department";
+
+  const designationTitle = userInfoArray.designation
+    ? `${userInfoArray.designation.designationName}`
+    : "No designation";
+
   return (
     <>
       <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold', marginBottom: '10px' }}>
@@ -12,12 +36,12 @@ const UserData = () => {
 
         <div className="details--header">
           <div className="details--header1">
-            <h2>First Name: {userDetails?.firstName}</h2>
-            <p>Department: {userDetails?.department?.departmentName}</p>
+            <h2>First Name: {userInfoArray?.firstName}</h2>
+            <p>Department: {departmentTitle}</p>
           </div>
           <div className="details--header2">
-            <h2>Last Name: {userDetails?.lastName}</h2>
-            <p>Designation: {userDetails?.designation?.designationName}</p>
+            <h2>Last Name: {userInfoArray?.lastName}</h2>
+            <p>Designation: {designationTitle}</p>
           </div>
         </div>
 
@@ -25,19 +49,19 @@ const UserData = () => {
         <div className="details--grid">
           <div className="details">
             <span>Username:</span>
-            <span>{userDetails?.username}</span>
+            <span>{userInfoArray?.username}</span>
           </div>
           <div className="details">
             <span>Email:</span>
-            <span>{userDetails?.email}</span>
+            <span>{userInfoArray?.email}</span>
           </div>
           <div className="details">
             <span>Role:</span>
-            <span>{userDetails?.role}</span>
+            <span>{userInfoArray?.role}</span>
           </div>
           <div className="details">
             <span>Active:</span>
-            <span>{userDetails?.active === true ? "Active" : "Not Active"}</span>
+            <span>{userInfoArray?.active === true ? "Active" : "Not Active"}</span>
           </div>
         </div>
         <div className="add-btn">
@@ -51,7 +75,7 @@ const UserData = () => {
           </Tooltip>
           <Tooltip title={user?.role !== "superadmin" ? "Only System admin can change password" : "Edit Password"}>
             <Button 
-              onClick={() => {navigate(`/updatePassword/${userDetails?._id}`)}} 
+              onClick={() => {navigate(`/updatePassword/${userInfoArray?._id}`)}} 
               disabled={user?.role !== "superadmin"}
               style={{ color: 'black', backgroundColor: 'white', border: '1px solid black', marginLeft: '10px' }}
             >
@@ -62,6 +86,14 @@ const UserData = () => {
       </div>
     </>
   )
-}
+};
+
+UserData.propTypes = {
+  userInfoArray: PropTypes.array,
+  navigate: PropTypes.func,
+  user: PropTypes.object,
+  loading: PropTypes.bool,
+  error: PropTypes.object,
+};
 
 export default UserData;
