fd54bcf58b27113db9fae1fb45cbb39955ae91a2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-18 08:55:06 +0300

    add employee using rtk mutation logic

--- frontend/src/pages/admin/employee/NewEmployee.jsx
+++ frontend/src/pages/admin/employee/NewEmployee.jsx
@@ -1,25 +1,32 @@
 import Layout from "@/components/Layout";
 import CreateEmployee from "@/pages/admin/employee/CreateEmployee";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { createNewEmployee } from "../../../services/employeeApi";
 import { message } from "antd";
+import { useCreateEmployeeMutation } from "@/features/employees/employeeSlice";
 
 const NewEmployee = () => {
-  const [loading, setLoading] = useState(false);
+  const [addEmployee, { isLoading: loading }] = useCreateEmployeeMutation();
   const navigate = useNavigate();
 
 
   // Function to create new employee
   const onFinishHandler = async (values) => {
     try {
-      setLoading(true);
-      await createNewEmployee(values);
-      navigate("/all/employees");
-      message.success("New Employee Created Succesfully");
+      const { error } = await addEmployee(values).unwrap();
+
+      if (error) {
+        if (error === 400 && error.data && error.data.message) {
+          message.error(error.data.message);
+          navigate("/all/employees");
+        } else {
+          message.error("Failed to create new employee");
+        }
+      } else {
+        message.success("New Employee Created Succesfully");
+        navigate("/all/employees");
+      }
     } catch (error) {
-      setLoading(false);
-      message.error("New Employee Creation Failed:", error);
+      message.error(error.message);
     }
   }
 
