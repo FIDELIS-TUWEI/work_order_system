5dee18e8a879f2acc5896d4d88db378da0151ad6
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-18 11:41:57 +0300

    backend logic controller to filter work by date created route

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { filterWorkStatus, countWorkStatus, countTotalWork, countReviewed } = require('../controllers/report');
+const { filterWorkStatus, countWorkStatus, countTotalWork, countReviewed, workCreatedByDate } = require('../controllers/report');
 const { protect, restrict, cacheMiddleware } = require('../middleware/authMiddleware');
 
 router.get("/work", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer"]), cacheMiddleware, filterWorkStatus);
+router.get("/work/created/date/:selectedDate", protect, cacheMiddleware, workCreatedByDate);
 router.get("/report/work/count/status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countWorkStatus);
 router.get("/report/work/total/reviewed", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countReviewed);
 router.get("/report/total/work/count", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countTotalWork);
