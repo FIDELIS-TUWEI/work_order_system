6f0b742d37bfc550a44a5cac26a33786cea8e313
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-20 12:19:29 +0300

    backend route restrict middleware added

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -4,7 +4,7 @@ const { filterWorkStatus, countWorkStatus, countTotalWork, countReviewed, workCr
 const { protect, restrict, cacheMiddleware } = require('../middleware/authMiddleware');
 
 router.get("/work", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer"]), cacheMiddleware, filterWorkStatus);
-router.get("/work/created/date/:selectedDate", protect, cacheMiddleware, workCreatedByDate);
+router.get("/work/created/date/:selectedDate", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer"]), cacheMiddleware, workCreatedByDate);
 router.get("/report/work/count/status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countWorkStatus);
 router.get("/report/work/total/reviewed", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countReviewed);
 router.get("/report/total/work/count", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countTotalWork);
