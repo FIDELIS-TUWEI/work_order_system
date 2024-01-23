1711b5fc4cf17418d887a97a036cdfaad25aa6a2
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-23 19:20:52 +0300

    logic to diable react-devtools in production

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,9 @@ import { Provider } from 'react-redux';
 import store from './features/store';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/route';
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
+if (process.env.NODE_ENV === production) disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
