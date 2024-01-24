10d416ec32ba4a500cf7ded575e4e536b4660e86
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-24 05:25:07 +0300

    mongo uri connection

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,12 +1,7 @@
 const mongoose = require("mongoose");
 
-const mongoUser = process.env.MONGO_USER
-const mongoPass = process.env.MONGO_PASS
-const mongoHost = process.env.MONGO_HOST
-const mongoDbName = process.env.MONGO_DB_NAME
-
 // Database connection
-const mongoURI = process.env.MONGO_URI || `mongodb+srv://${mongoUser}:${mongoPass}@${mongoHost}.opfay3d.mongodb.net/${mongoDbName}?retryWrites=true&w=majority`
+const mongoURI = process.env.MONGO_URI
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true
