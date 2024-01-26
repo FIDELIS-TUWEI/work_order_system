cf01fa17114d15055db77e5342b669cf375998c5
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-26 11:32:17 +0300

    mongo uri connection error resolved

--- config/connectDB.js
+++ config/connectDB.js
@@ -3,19 +3,23 @@ const mongoose = require("mongoose");
 
 dotenv.config();
 
-// Database connection
-const mongoURI = `${process.env.MONGO_URI}`
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useMongoClient: true
+const connectDB = async () => {
+    try {
+        // Database connection
+        const mongoURI = process.env.MONGO_URI
+        const options = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        };
+
+        const connected = await mongoose.connect(mongoURI, options)
+        console.log(`Database is connected ${connected.connection.host}`);
+    } catch (error) {
+        console.log("Error connecting to MongoDB", error);
+        
+    }
 };
 
-mongoose
-    .connect(mongoURI, options)
-    .then(() => {
-        console.log('Database connected successfully!');
-    })
-    .catch((error) => {
-        console.log("Error connecting to MongoDB", error);
-    });
+module.exports = connectDB
+
+
