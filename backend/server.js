12f0f1b3290e4a83929bb01f4cd1b4af1aaecdae
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-26 17:47:12 +0300

    domain name added to cors origin

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cors({
     credentials: true,
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://work-orders.online"],
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use(cookieParser());
