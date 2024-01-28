3bcd9902e7cbdc77f95b66b13512f00fb97b9819
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-28 23:31:19 +0300

    CORS header refactor

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://work-orders.online", "https://workorder-client.onrender.com"],
+    origin: ["http://localhost:3000", "https://www.work-orders.online", "https://workorder-client.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use(cookieParser());
