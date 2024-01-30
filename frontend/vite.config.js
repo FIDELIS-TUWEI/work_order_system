25896865093150d5dcc802587354f11ef63ee6dd
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-30 10:50:29 +0300

    vite config proxy refactor

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['lodash']
+        }
+      }
+    }
+  },
   server: {
     port: 3000,
     proxy: {
