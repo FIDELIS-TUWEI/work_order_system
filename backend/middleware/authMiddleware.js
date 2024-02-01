2c61c692e93159a799cf8cdd1090c80247491d32
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-02 02:48:50 +0300

    jwt secret reconfigure

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -12,7 +12,7 @@ const protect = asyncHandler(async (req, res, next) => {
         }
 
         // Verify token
-        const verified = jwt.verify(token, 'Holiday-inn@-2023_$$');
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
 
         // Get user id from the token
         const user = await User.findById(verified.id);
