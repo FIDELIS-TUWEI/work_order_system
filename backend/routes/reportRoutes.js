459ab34199016355765929562498fd4741756e7a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-01 11:58:41 +0300

    auth middleware refactor

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { filterWorkStatus, countWorkStatus, countTotalWork, countReviewed, workCreatedByDate } = require('../controllers/report');
-const { protect, restrict, cacheMiddleware } = require('../middleware/authMiddleware');
+const { restrict, cacheMiddleware } = require('../middleware/authMiddleware');
 
-router.get("/work", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer"]), cacheMiddleware, filterWorkStatus);
-router.get("/work/created/date/:selectedDate", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer"]), cacheMiddleware, workCreatedByDate);
-router.get("/report/work/count/status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countWorkStatus);
-router.get("/report/work/total/reviewed", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countReviewed);
-router.get("/report/total/work/count", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countTotalWork);
+router.get("/work", restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer"]), cacheMiddleware, filterWorkStatus);
+router.get("/work/created/date/:selectedDate", restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer"]), cacheMiddleware, workCreatedByDate);
+router.get("/report/work/count/status", restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countWorkStatus);
+router.get("/report/work/total/reviewed", restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countReviewed);
+router.get("/report/total/work/count", restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user"]), cacheMiddleware, countTotalWork);
 
 
 module.exports = router;
