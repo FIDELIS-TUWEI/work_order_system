83cda683681320000f25c5448512c0eeaf634b15
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-01 11:58:19 +0300

    auth middleware refactor

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { signupUser, login, logout, getUserInfo, forgotPassword, resetPassword, changePassword } = require("../controllers/authController");
-const { protect, restrict } = require("../middleware/authMiddleware");
+const { signupUser, login, logout, getUserInfo, changePassword } = require("../controllers/authController");
+const { restrict } = require("../middleware/authMiddleware");
 
-router.post("/register", protect, restrict(["admin", "superadmin"]), signupUser);
+router.post("/register", restrict(["admin", "superadmin"]), signupUser);
 router.post("/login",  login);
 router.post("/logout", logout);
-router.get("/userInfo", protect, getUserInfo);
-router.put("/update/password/:id", protect, restrict(["admin", "superadmin"]), changePassword);
+router.get("/userInfo", getUserInfo);
+router.put("/update/password/:id", restrict(["admin", "superadmin"]), changePassword);
 
 module.exports = router;
