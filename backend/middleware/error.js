3d4b86a4b0b5d43eac8f4893605f689e7d6ea58c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-01 10:51:40 +0300

    backend error handler middleware refactor

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,31 +1,47 @@
-
-const notFound = (req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-
-    next(error);
-}
+const ErrorResponse = require("../utils/errorRespone");
 
 const errorHandler = (err, req, res, next) => {
-    
-    let  statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    let message = err.message;
+    let error = {...err};
+    error.message = err.message;
 
-    if (err.message === 'CastError' && err.kind === 'ObjectId') {
-        statusCode = 404;
-        message = 'Resource not found';
+    // Handle Cast Error
+    if (err.name === 'CastError') {
+        const message = `Resource not found with ID of ${err.value}`;
+        error = new ErrorResponse(message, 404);
     }
 
-    res.status(statusCode).json({
+    // Handle Axios Error (network error)
+    if (err.name === "AxiosError") {
+        const message = "Network Error, please Check your internet connection";
+        error = new ErrorResponse(message, 500);
+    };
+
+    // Handle Duplicate field value
+    if (err.code === 11000) {
+        const field = Object.values(err.keyValue)[0];
+        const message = `Duplicate ${field} value entered`;
+        error = new ErrorResponse(message, 400);
+    };
+
+    // Handle validation error
+    if (err.name === "ValidationError") {
+        const errors = Object.keys(err.errors).map((key) => {
+            return {
+                field: key,
+                message: err.errors[key].message
+            };
+        });
+        error = new ErrorResponse(errors, 400);
+    };
+
+    res.status(error.statusCode).json({
         success: false,
-        message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+        error: error.message
     });
+
+
 
     next();
 };
 
-module.exports = {
-    notFound, 
-    errorHandler
-};
+module.exports = errorHandler;
