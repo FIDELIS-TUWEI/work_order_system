9681d3c8fcb194ee226c0f3beff6070854293c08
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-02 12:02:27 +0300

    restart server

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ app.use("/hin", designationRoutes);
 app.use("/hin", employeeRoutes);
 
 // Server Setup
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
