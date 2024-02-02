7fea7225485db8981871a97e5d5b4fdd8aa7fc7c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-02 08:02:53 +0300

    react backend server config

--- frontend/src/features/api/apiSlice.js
+++ frontend/src/features/api/apiSlice.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const serverUrl = "/hin"
+const serverUrl = import.meta.env.VITE_SERVER_URL;
 
 const baseQuery = fetchBaseQuery({ 
     baseUrl: serverUrl,
--- frontend/src/pages/admin/reports/DailyWork.jsx
+++ frontend/src/pages/admin/reports/DailyWork.jsx
@@ -7,7 +7,7 @@ import Logo from "@/assets/images/logo.png";
 import moment from "moment";
 import { message } from "antd";
 
-const serverUrl = "/hin"
+const serverUrl = import.meta.env.VITE_SERVER_URL;
 
 
 const DailyWork = () => {
