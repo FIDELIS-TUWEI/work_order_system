9f4e21ffbe236cb51700af67116166338ef43a5c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-03 10:46:08 +0300

    key generator added

---
+++ backend/utils/keyGenerator.js
@@ -0,0 +1,10 @@
+// utils/keyGenerator.ts
+
+module.exports =  function keyGenerator(request, _response){
+    if (!request.ip) {
+        console.error('Warning: request.ip is missing!')
+        return request.socket.remoteAddress
+    }
+
+    return request.ip.replace(/:\d+[^:]*$/, '')
+}
