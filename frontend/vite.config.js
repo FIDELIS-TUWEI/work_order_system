2c267823a8fcdc90110ec7d4493b7fc099168fe6
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-04 14:34:01 +0300

    frontend proxy changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/hin': { 
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
   },
   resolve: {
     alias: {
