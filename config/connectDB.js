3e3894e3bc90e4ac436f544ac7b993bb14bdfbd0
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-04 16:08:05 +0300

    backend connect database refactor

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,18 +1,24 @@
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-// Database connection
-const mongoURI = process.env.MONGO_URI;
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-};
+dotenv.config();
 
-mongoose
-    .connect(mongoURI, options)
-    .then(() => {
-        console.log('Database connected successfully!');
-    })
-    .catch((error) => {
-        console.log("Error connecting to MongoDB", error);
+const connectDB = async () => {
+    try {
+        // Database connection
+        const mongoURI = process.env.MONGO_URI;
+        const options = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        };
+
+         await mongoose.connect(mongoURI, options)
+        console.log(`MongoDB Database is connected successfully.`);
+            
+    } catch (error) {
+        console.log(`Error connecting to MongoDB, ${error.message}`);
         process.exit(1)
-    });
+    }
+}
+
+module.exports = connectDB;
