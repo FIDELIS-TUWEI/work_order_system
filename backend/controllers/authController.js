211ccceb4b6a8839bc1299d4dd4318fbe5fe6ce1
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-05 04:56:27 +0300

    backend auth refactor

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -104,7 +104,7 @@ const login = asyncHandler (async (req, res) => {
                 expires: cookieExpiry
             });
 
-            const { ...restParams } = user._doc;
+            const { password, ...restParams } = user._doc;
             user.token = token;
             user.password = undefined;
 
