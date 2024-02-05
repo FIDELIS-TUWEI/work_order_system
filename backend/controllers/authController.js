1d335b72c3a854979416ba87b1256b82b361d532
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-05 10:50:41 +0300

    same site set

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -100,7 +100,7 @@ const login = asyncHandler (async (req, res) => {
                 expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
                 httpOnly: true,
                 secure: true,
-                sameSite: 'lax',
+                sameSite: 'None',
             };
             
             const { password, ...restParams } = user._doc;
