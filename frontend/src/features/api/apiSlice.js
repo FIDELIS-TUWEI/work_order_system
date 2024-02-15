86d0ec31bf3b895341c4dcdb66b8d1f4ca399d78
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-15 10:17:33 +0300

    settings headers frontend

--- frontend/src/features/api/apiSlice.js
+++ frontend/src/features/api/apiSlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseQuery = fetchBaseQuery({ 
     baseUrl: "",
     credentials: "include",
-    prepareHeaders: (headers) => {
-        const token = JSON.parse(localStorage.getItem("token"));
+    prepareHeaders: (headers, { getState }) => {
+        const token = getState().auth.token;
         if (token) {
             headers.set("authorization", `Bearer ${token}`);
         }
