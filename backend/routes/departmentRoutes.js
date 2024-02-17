4079a3eadc1433ab32cbcb0727e960bc9f45e838
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-17 14:57:13 +0300

    newly created role api endpoints access added

--- backend/routes/departmentRoutes.js
+++ backend/routes/departmentRoutes.js
@@ -4,9 +4,9 @@ const { protect, restrict, cacheMiddleware } = require("../middleware/authMiddle
 const { createDepartment, getAllDepartments, deleteDepartment, queryAllDepartments } = require("../controllers/departmentController");
 
 
-router.post("/new/department", protect, restrict(["admin", "superadmin", "supervisor"]), createDepartment);
-router.get("/all-departments", protect, restrict(["admin", "superadmin", "supervisor"]), cacheMiddleware, getAllDepartments);
-router.get("/query/all/departments", protect, restrict(["admin", "superadmin", "supervisor"]), cacheMiddleware, queryAllDepartments);
-router.delete("/delete/department/:id", protect, restrict(["admin", "superadmin", "supervisor"]), deleteDepartment);
+router.post("/new/department", protect, restrict(["admin", "superadmin"]), createDepartment);
+router.get("/all-departments", protect, restrict(["admin", "superadmin"]), cacheMiddleware, getAllDepartments);
+router.get("/query/all/departments", protect, restrict(["admin", "superadmin"]), cacheMiddleware, queryAllDepartments);
+router.delete("/delete/department/:id", protect, restrict(["admin", "superadmin"]), deleteDepartment);
 
 module.exports = router;
