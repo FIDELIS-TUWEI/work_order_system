cd35b515d939c0d6e6dc00ab11a00da4a9861770
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-24 11:47:00 +0300

    display work tracker count dashboard

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import Layout from "@/components/Layout";
 import { selectUserInfo } from "@/features/auth/authSlice";
 import DashboardComponent from "@/pages/dashboard/DashboardComponent";
-import { useGetWorkCountByStatusQuery, useTotalReviewedWorkQuery, useTotalWorkCountQuery } from "@/features/reports/reportSlice";
+import { useCountWorkTrackerQuery, useGetWorkCountByStatusQuery, useTotalReviewedWorkQuery, useTotalWorkCountQuery } from "@/features/reports/reportSlice";
 import { useCountActiveUsersQuery, useCountAllUsersQuery } from "@/features/users/userSlice";
 import { useCountAllEmployeesQuery } from "@/features/employees/employeeSlice";
 
@@ -11,12 +11,16 @@ const Dashboard = () => {
   const { data: reviewed } = useTotalReviewedWorkQuery();
   const { data: counts } = useGetWorkCountByStatusQuery();
   const { data: workTotal } = useTotalWorkCountQuery();
+  const { data: trackerCounts } = useCountWorkTrackerQuery();
   const { data: totalUsers } = useCountAllUsersQuery();
   const { data: activeUsers } = useCountActiveUsersQuery();
   const { data: totalEmployees } = useCountAllEmployeesQuery();
 
+  const notAttendedCount = trackerCounts?.data.not_attended || [];
   const pendingCount = counts?.data.pending || [];
-  const inProgressCount = counts?.data.inProgress || [];
+  const inAttendanceCount = trackerCounts?.data.in_attendance || [];
+  const inCompleteCount = trackerCounts?.data.in_complete || [];
+  const attendedCount = trackerCounts?.data.attended || [];
   const completedCount = counts?.data.completed || [];
   const reviewCount = reviewed?.data || [];
   const workCountData = workTotal?.data || [];
@@ -28,8 +32,11 @@ const Dashboard = () => {
   return (
     <Layout>
       <DashboardComponent 
+        notAttendedCount={notAttendedCount}
         pendingCount={pendingCount}
-        inProgressCount={inProgressCount}
+        inAttendanceCount={inAttendanceCount}
+        inCompleteCount={inCompleteCount}
+        attendedCount={attendedCount}
         completedCount={completedCount}
         reviewedCount={reviewCount}
         workCountData={workCountData}
