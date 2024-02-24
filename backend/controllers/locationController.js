a803cd629385aa872cd83aa29ce696722b517ce5
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-02-24 20:33:51 +0300

    backend global error handle logic used refactor

--- backend/controllers/locationController.js
+++ backend/controllers/locationController.js
@@ -1,120 +1,96 @@
 const Location = require("../model/location"); 
 const asyncHandler = require("express-async-handler");
+const asyncErrorHandler = require("../utils/asyncErrorHandler");
+const CustomError = require("../utils/CustomError");
 
 // @desc Create locations
 // @route POST /locations
 // @access Private
 
-const createLocation = asyncHandler(async (req, res) => {
-    try {
-        // Check duplicate location
-        const duplicate = await Location.findOne({ locationTitle: req.body.locationTitle });
-        if (duplicate) {
-            return res.status(400).json({
-                success: false,
-                message: "Location already exists",
-            });
-        };
-        
-        // Create new location
-        const newWorkLocation = new Location(req.body);
-        if (!newWorkLocation) {
-            return res.status(400).json({
-                success: false,
-                message: "Location not created",
-            });
-        };
-
-        // Save the created location
-        await newWorkLocation.save();
-        res.status(201).json({
-            success: true,
-            data: newWorkLocation
-        })
-    } catch (error) {
-        res.status(500).json({
+const createLocation = asyncHandler (asyncErrorHandler (async (req, res, next) => {
+    // Check duplicate location
+    const duplicate = await Location.findOne({ locationTitle: req.body.locationTitle });
+    if (duplicate) {
+        return res.status(400).json({
             success: false,
-            message: error.message
+            message: "Location already exists",
         });
-    }
-});
+    };
+    
+    // Create new location
+    const newWorkLocation = new Location(req.body);
+    if (!newWorkLocation) {
+        const error = new CustomError("Failed to create new Location!", 400);
+        return next(error);
+    };
+
+    // Save the created location
+    await newWorkLocation.save();
+    res.status(201).json({
+        success: true,
+        data: newWorkLocation
+    })
+}));
 
 // @desc Get all locations
 // @route GET /all-locations
 // @access Private
-const getAllLocations = asyncHandler(async (req, res) => {
+const getAllLocations = asyncHandler (asyncErrorHandler (async (req, res, next) => {
     // Enable Pagination
     const pageSize = 10;
     const page = Number(req.query.pageNumber) || 1;
     const count = await Location.find({}).estimatedDocumentCount();
-    try {
-        const locations = await Location.find({})
-            .skip(pageSize * (page - 1))
-            .limit(pageSize)
 
-        if (!locations) {
-            return res.status(400).json({ message: "No locations found" });
-        };
+    // Find locations in DB
+    const locations = await Location.find({})
+        .skip(pageSize * (page - 1))
+        .limit(pageSize)
 
-        res.status(200).json({
-            success: true,
-            data: locations,
-            page,
-            pages: Math.ceil(count / pageSize),
-            count
-        });
+    if (!locations) {
+        const error = new CustomError("Locations not found!", 404);
+        return next(error)
+    };
 
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
-    }
-});
+    res.status(200).json({
+        success: true,
+        data: locations,
+        page,
+        pages: Math.ceil(count / pageSize),
+        count
+    });
+}));
 
 // Query to get all locations
-const queryAllLocations = asyncHandler(async (req, res) => {
-    try {
-        const locations = await Location.find({});
+const queryAllLocations = asyncHandler (asyncErrorHandler (async (req, res, next) => {
+    const locations = await Location.find({});
 
-        if (!locations) {
-            return res.status(400).json({ message: "No locations found" });
-        }
-        res.status(200).json({
-            success: true,
-            data: locations
-        })
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
+    if (!locations) {
+        const error = new CustomError("Locations not found!", 404);
+        return next(error)
     }
-})
+    res.status(200).json({
+        success: true,
+        data: locations
+    });
+}))
 
 // @desc Delete location
 // @route DELETE /delete/location/:id
 // @access Private
-const deleteLocation = asyncHandler(async (req, res) => {
-    try {
-        const locationId = req.params.id;
-        const deleteLocation = await Location.findByIdAndDelete(locationId);
+const deleteLocation = asyncHandler (asyncErrorHandler (async (req, res, next) => {
+    const locationId = req.params.id;
+    const deleteLocation = await Location.findByIdAndDelete(locationId);
 
-        if (!deleteLocation) {
-            return res.status(404).json({ message: "Location not found" });
-        };
+    if (!deleteLocation) {
+        const error = new CustomError("Location not found!", 404);
+        return next(error)
+    };
 
-        return res.status(200).json({
-            success: true,
-            message: "Location deleted successfully"
-        })
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
-    }
-})
+    return res.status(200).json({
+        success: true,
+        message: "Location deleted successfully"
+    });
+}))
 
 module.exports = {
     createLocation,
