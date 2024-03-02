857b0b6c6aa7782449271697f9fb6606724e2c34
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-02 15:21:06 +0300

    additional service type added

--- frontend/src/pages/admin/workOrders/NewWork.jsx
+++ frontend/src/pages/admin/workOrders/NewWork.jsx
@@ -65,7 +65,8 @@ const NewWork = ({
                   { value: 'Fix', label: 'Fix' }, { value: 'Repair', label: 'Repair' }, 
                   { value: 'Replace', label: 'Replace' }, { value: 'Install', label: 'Install' },
                   { value: 'Upgrade', label: 'Upgrade' }, { value: 'Remove', label: 'Remove' },
-                  { value: 'Touchup', label: 'Touchup' }, { value: 'Move', label: 'Move' }
+                  { value: 'Touchup', label: 'Touchup' }, { value: 'Move', label: 'Move' },
+                  { value: 'Paint', label: 'Paint' }, { value: 'Prune', label: 'Prune' }
                 ]}
               />
             </Form.Item>
