0519375ab9fb06e00a81f6a66485270f19bc281c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-05 13:41:31 +0300

    isAdmin middleware added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { getAllUsers, singleUser, editUser, deleteUser, countAllUsers, countActiveUsers} = require('../controllers/userController');
-const { protect, restrict, cacheMiddleware } = require('../middleware/authMiddleware');
+const { protect, restrict, cacheMiddleware, isAdmin } = require('../middleware/authMiddleware');
 
 router.get("/all-users", protect, restrict(['admin', "superadmin"]), cacheMiddleware, getAllUsers);
 router.get("/single/user/:id", protect, cacheMiddleware, singleUser);
 router.get("/count/total-users", protect, cacheMiddleware, countAllUsers);
 router.get("/count/active-users", protect, cacheMiddleware, countActiveUsers);
 router.put("/edit/user/:id", protect, restrict(['admin', "superadmin"]),  editUser);
-router.delete("/admin/user/delete/:id", protect, restrict(['admin', "superadmin"]), deleteUser);
+router.delete("/admin/user/delete/:id", protect, isAdmin, restrict(["superadmin"]), deleteUser);
 
 
 module.exports = router;
