b48a3b502277cc1915832eb71b890580b37f98f8
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-05 09:15:45 +0300

    login rate limiter added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,9 +2,17 @@ const express = require("express");
 const router = express.Router();
 const { signupUser, login, logout, getUserInfo, changePassword } = require("../controllers/authController");
 const { protect, restrict } = require("../middleware/authMiddleware");
+const rateLimit = require("express-rate-limit");
+
+// Prevent brute-force attackes
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 3, // 3 login attempts
+    message: "Too many Login attempts, please try again later!",
+});
 
 router.post("/register", protect, restrict(["admin", "superadmin"]), signupUser);
-router.post("/login",  login);
+router.post("/login", loginLimiter, login);
 router.post("/logout", logout);
 router.get("/userInfo", protect, getUserInfo);
 router.put("/update/password/:id", protect, restrict(["admin", "superadmin"]), changePassword);
