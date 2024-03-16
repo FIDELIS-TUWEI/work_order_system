ce832ba072fcc410517e2f2dace687a9552aa5d9
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-16 17:24:19 +0300

    get single category route

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { createCategory, getAllCategories, updateCategory, deleteCategory, queryAllCategories } = require("../controllers/categoryController");
+const { createCategory, getAllCategories, updateCategory, deleteCategory, queryAllCategories, singleCategory } = require("../controllers/categoryController");
 const { protect, restrict, cacheMiddleware } = require("../middleware/authMiddleware");
 
 router.post("/new/category", protect, restrict(["admin", "superadmin", "maintenance"]), createCategory);
 router.get("/all/categories", protect, cacheMiddleware, getAllCategories);
 router.get("/query/all/categories", protect, restrict(["admin", "superadmin", "hod", "reviewer", "engineer", "user", "supervisor", "maintenance"]), cacheMiddleware, queryAllCategories);
+router.get("/single/category/:id", protect, restrict(["admin", "superadmin"]), cacheMiddleware, singleCategory);
 router.put("/edit/category/:id", protect, restrict(["admin", "superadmin"]), updateCategory);
 router.delete("/delete/category/:id", protect, restrict(["admin", "superadmin"]), deleteCategory);
 
