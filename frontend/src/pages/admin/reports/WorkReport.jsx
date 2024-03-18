14e22cdc4263b09bc48b3e81c9549ddc614ddc46
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-18 19:35:03 +0300

    work report UI refactor

--- frontend/src/pages/admin/reports/WorkReport.jsx
+++ frontend/src/pages/admin/reports/WorkReport.jsx
@@ -1,84 +1,13 @@
 import PropTypes from "prop-types";
-import { Button, Card, Input, Space, Table, Tooltip } from "antd";
-import {GrSearch} from "react-icons/gr";
-import Highlighter from 'react-highlight-words';
+import { Button, Card, Table, Tooltip } from "antd";
 
 import "jspdf-autotable";
-import { useRef, useState } from "react";
 import moment from "moment";
 import { useNavigate } from "react-router-dom";
 
 
 const WorkReport = ({ workOrdersData, loading, handleStatusChange, exportPDF }) => {
-  const [searchText, setSearchText] = useState("");
-  const [searchedColumn, setSearchedColumn] = useState("");
-  const searchInput = useRef(null);
   const navigate = useNavigate();
-
-  const handleSearch = (selectedKeys, confirm, dataIndex) => {
-    confirm();
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
-  };
-
-  const handleReset = (clearFilters) => {
-    clearFilters();
-    setSearchText("");
-  };
-
-  const getColumnSearchProps = (dataIndex, title) => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
-      <div style={{ padding: 8 }}>
-        <Input 
-          ref={searchInput}
-          placeholder={`Search ${title}`}
-          value={selectedKeys[0]}
-          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-          style={{ marginBottom: 8, display: "block" }}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-            icon={<GrSearch />}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Search
-          </Button>
-          <Button
-            onClick={() => handleReset(clearFilters)}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Reset
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered) => (
-      <GrSearch style={{ color: filtered ? "#1890ff" : undefined }} />
-      ),
-    onFilter: (value, record) =>
-      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
-    onFilterDropdownOpenChange: (visible) => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.select(), 100);
-      }
-    },
-    render: (text) =>
-      searchedColumn === dataIndex ? (
-        <Highlighter
-          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
-          searchWords={[searchText]}
-          autoEscape
-          textToHighlight={text ? text.toString() : ""}
-        />
-      ) : (
-        text
-      ),
-  });
 
   // Format date
   const formatDate = (date) => {
@@ -95,13 +24,13 @@ const WorkReport = ({ workOrdersData, loading, handleStatusChange, exportPDF }) 
 
   const columns = [
     {
-      title: "Description",
+      title: "Order Number",
       width: 300,
       fixed: "left",
       align: "center",
       responsive: ["md", "lg"],
-      dataIndex: "description",
-      key: "description",
+      dataIndex: "workOrderNumber",
+      key: "workOrderNumber",
     },
     {
       title: "Service Type",
@@ -110,7 +39,6 @@ const WorkReport = ({ workOrdersData, loading, handleStatusChange, exportPDF }) 
       responsive: ["md", "lg"],
       dataIndex: "serviceType",
       key: "serviceType",
-      ...getColumnSearchProps("serviceType", "Service Type"),
     },
     {
       title: "Priority",
@@ -119,7 +47,6 @@ const WorkReport = ({ workOrdersData, loading, handleStatusChange, exportPDF }) 
       responsive: ["md", "lg"],
       dataIndex: "priority",
       key: "priority",
-      ...getColumnSearchProps("priority", "Priority"),
     },
     {
       title: "Category",
@@ -171,15 +98,6 @@ const WorkReport = ({ workOrdersData, loading, handleStatusChange, exportPDF }) 
       render: (assignedTo) => (
         assignedTo ? `${assignedTo.firstName} ${assignedTo.lastName}` : "Unassigned"
       )
-    },
-    {
-      title: "Supervisor",
-      width: 150,
-      align: "center",
-      responsive: ["md", "lg"],
-      dataIndex: "supervisedBy",
-      key: "supervisedBy",
-      ...getColumnSearchProps("supervisedBy", "Supervisor"),
     },
     {
       title: "Date Completed",
