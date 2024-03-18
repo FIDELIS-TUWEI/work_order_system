ef8cb726ea929a66caa6b7e51ac14fde17abfb0c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-18 18:15:45 +0300

    route to fetch work with attended tracker status

--- backend/routes/workOrderRoutes.js
+++ backend/routes/workOrderRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createWorkOrder, updateWorkOrder, getAllWorkOrders, getSingleWorkOrder, deleteWorkOrder, queryAllWork, searchWorkByOrderNumber, inAttendanceTracker, inCompleteTracker } = require('../controllers/workOrderController');
+const { createWorkOrder, updateWorkOrder, getAllWorkOrders, getSingleWorkOrder, deleteWorkOrder, queryAllWork, searchWorkByOrderNumber, inAttendanceTracker, inCompleteTracker, attendedTracker } = require('../controllers/workOrderController');
 const { protect, restrict, cacheMiddleware, setVerifiedBy} = require('../middleware/authMiddleware');
 
 
@@ -10,7 +10,7 @@ router.get("/query/all/work", protect, restrict(["admin", "hod", "supervisor", "
 router.get("/single/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "hod", "reviewer", "engineer", "maintenance"]), cacheMiddleware, getSingleWorkOrder);
 router.get("/work-orders/in-attendance", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer", "maintenance"]), cacheMiddleware, inAttendanceTracker);
 router.get("/work-orders/in-complete", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer", "maintenance"]), cacheMiddleware, inCompleteTracker);
-
+router.get("/work-orders/attended", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer", "maintenance"]), cacheMiddleware, attendedTracker);
 router.get("/search/work/:workOrderNumber", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer", "maintenance"]), cacheMiddleware, searchWorkByOrderNumber)
 router.put("/update/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "reviewer", "engineer", "hod", "maintenance"]), setVerifiedBy, updateWorkOrder);
 router.delete("/delete/work/:id", protect, restrict(["admin", "superadmin"]), deleteWorkOrder);
