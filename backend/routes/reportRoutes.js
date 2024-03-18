2662e38a6799a63b2fc29e713c05702ac1b88058
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-18 19:37:12 +0300

    report backend controller modified

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { filterWorkStatus, countWorkStatus, countTotalWork, countReviewed, workCreatedByDate, countWorkTracker } = require('../controllers/report');
+const { filterWorkStatus, countWorkStatus, countTotalWork, workCreatedByDate, countWorkTracker } = require('../controllers/report');
 const { protect, restrict, cacheMiddleware } = require('../middleware/authMiddleware');
 
 router.get("/work", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "maintenance"]), cacheMiddleware, filterWorkStatus);
 router.get("/report/work/count/tracker", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user", "maintenance"]), cacheMiddleware, countWorkTracker);
 router.get("/work/created/date/:selectedDate", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "maintenance"]), cacheMiddleware, workCreatedByDate);
 router.get("/report/work/count/status", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user", "maintenance"]), cacheMiddleware, countWorkStatus);
-router.get("/report/work/total/reviewed", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user", "maintenance"]), cacheMiddleware, countReviewed);
 router.get("/report/total/work/count", protect, restrict(["admin", "hod", "reviewer", "supervisor", "superadmin", "engineer", "user", "maintenance"]), cacheMiddleware, countTotalWork);
 
 
