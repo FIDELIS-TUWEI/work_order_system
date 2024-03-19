2848a54d2ae1dd4c9987637bfb53e2bf705bc049
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-03-19 17:08:54 +0300

    diplay work UI refactor

--- frontend/src/pages/admin/workOrders/AllWorkOrders.jsx
+++ frontend/src/pages/admin/workOrders/AllWorkOrders.jsx
@@ -1,65 +1,21 @@
-import PropTypes from "prop-types";
-import { Button, Input, Typography, message } from "antd"
+import { Button, Typography, message } from "antd"
 import Layout from "@/components/Layout";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import {GrFormNext, GrFormPrevious} from "react-icons/gr";
 
 import Work from "@/pages/admin/workOrders/Work";
-import { useSearchWorkQuery, useWorkOrdersQuery } from "@/features/work/workSlice";
+import { useWorkOrdersQuery } from "@/features/work/workSlice";
 import { selectUserInfo } from "@/features/auth/authSlice";
-import LoadingBox from "@/components/LoadingBox";
-const { Search } = Input;
-
-// Search component
-const Filter = ({ searchTerm, setSearchTerm, handleSearch }) => {
-  return (
-    <Search 
-      type="search"
-      value={searchTerm}
-      onChange={(e) => setSearchTerm(e.target.value)}
-      onSearch={handleSearch}
-      placeholder="Enter work order number to search..."
-    />
-  )
-};
-
-Filter.propTypes = {
-  searchTerm: PropTypes.string,
-  handleSearch: PropTypes.func
-};
-
-// Render searched work
-const SearchedWork = ({ filteredWork }) => {
-  return (
-    <div>
-      {
-        filteredWork?.map(work => (
-          <p key={work._id}>
-            {work.description} {work.workOrderNumber}
-            <Button type="submit">Edit</Button>
-          </p>
-        ))
-      }
-    </div>
-  )
-};
-
-SearchedWork.propTypes = {
-  filteredWork: PropTypes.array,
-}
 
 
 const AllWorkOrders = () => {
   const user = useSelector(selectUserInfo);
   const [filterStatus, setFilterStatus] = useState("");
-  const [searchTerm, setSearchTerm] = useState("");
-  const { dataInfo, isLoading } = useSearchWorkQuery(searchTerm)
   const [page, setPage] = useState(1);
   const { data, isLoading: loading, error, refetch } = useWorkOrdersQuery({ page, status: filterStatus });
 
   const { data: workOrdersArray, pages } = data || {};
-  const { dataInfo: workSearch } = dataInfo || {};
 
  // Handle errors
  useEffect(() => {
@@ -81,26 +37,10 @@ const AllWorkOrders = () => {
    refetch();
  };
 
- const handleSearch = (value) => {
-  setSearchTerm(value);
- }
-
- // Function to filter work by order number
- const filteredWork = workSearch?.filter(work => work.workOrderNumber.toLowerCase().includes(searchTerm.toLowerCase()));
- 
 
   return (
     <Layout>
       <Typography style={{ textAlign: 'center', fontSize: '1.5rem', fontWeight: 'bold' }}>All Work Orders</Typography>
-      <Filter searchTerm={searchTerm} setSearchTerm={setSearchTerm} handleSearch={handleSearch} />
-      { isLoading && <LoadingBox /> }
-      { filteredWork?.map((workOrder) => {
-        <Typography key={workOrder._id}>
-          {workOrder.description} {workOrder.workOrderNumber}
-
-          <Button type="submit">Edit</Button>
-        </Typography>
-      })}
 
       <Work 
         workOrdersArray={workOrdersArray}
