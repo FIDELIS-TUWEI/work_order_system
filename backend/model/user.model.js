ed7e1c130ff29ce5a82798bb5661f2539eb1b30c
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-11 14:58:10 +0300

    user schema model refactor

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,37 +1,33 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
 const { ObjectId } = mongoose.Schema;
-const crypto = require("crypto");
 const validator = require("validator")
 
 // User Schema
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: [true, "Please fill your name"]
+        required: true,
     },
     lastName: {
         type: String,
-        required: [true, "Please fill your name"]
+        required: true,
     },
     email: {
         type: String,
-        required: [true, "Please fill your email"],
-        unique: [true, "Email already exists"],
+        required: true,
+        unique: true,
         lowercase: true,
-        match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, "Please fill a valid email"],
         validate: [validator.isEmail, 'Please Enter a valid email']
     },
     username: {
         type: String,
-        required: [true, "Please enter a Username"],
+        required: true,
         uppercase: true,
-        unique: [true, "Username already exists"],
+        unique: true,
     },
     password: {
         type: String,
-        required: [true, "Password is required"],
-        minLength: [6, "Password must have at least (6) characters"],
+        required: true,
     },
     role: {
         type: String,
@@ -65,32 +61,5 @@ const userSchema = new mongoose.Schema({
     timestamps: { createdAt: "Date_Created", updatedAt: "Date_Updated" }, 
 }
 );
-
-// Encrypt Password before Saving to DB
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        next();
-    }
-    this.password = await bcrypt.hash(this.password, 10);
-});
-
-// Compare user password in the database
-userSchema.methods.comparePassword = async function(enteredPassword) {
-    const isMatch = await bcrypt.compare(enteredPassword, this.password);
-    return isMatch;
-};
-
-// Generate Reset password hash
-userSchema.methods.passordResetHash = function() {
-    // create hash object, then create a sha512 hash of the user's current password and return hash
-    const resetHash = crypto.createHash("sha512").update(this.password).digest("hex");
-    return resetHash;
-};
-
-// Verify Reset password hash
-userSchema.methods.verifyPasswordResetHash = function(resetHash = undefined) {
-    // regenerate hash and check if they match
-    return this.passordResetHash() === resetHash;
-}
 
 module.exports = mongoose.model('User', userSchema);
