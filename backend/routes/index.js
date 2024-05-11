86085dffd2fc633f53ffdd8d679f815c2d6e004b
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-11 13:45:45 +0300

    index file route import

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,26 +1,29 @@
-module.exports = (app) => {
-    const limiter = require("../middleware/limiter");
+const express = require("express");
+const app = require("../app");
+const limiter = require("../middleware/limiter");
 
-    // Import Routes
-    const authRoutes = require("../routes/auth.routes");
-    const userRoutes = require("./user.routes");
-    const workOrderRoutes = require("./work.order.routes");
-    const locationRoutes = require("./location.routes");
-    const reportsRoutes = require("./report.routes");
-    const categoryRoutes = require("../routes/category.routes");
-    const departmentRoutes = require("../routes/department.routes");
-    const designationRoutes = require("../routes/designation.routes");
-    const employeeRoutes = require("../routes/employee.routes");
+// Import Routes
+const authRoutes = require("../routes/auth.routes");
+const userRoutes = require("./user.routes");
+const workOrderRoutes = require("./work.order.routes");
+const locationRoutes = require("./location.routes");
+const reportsRoutes = require("./report.routes");
+const categoryRoutes = require("../routes/category.routes");
+const departmentRoutes = require("../routes/department.routes");
+const designationRoutes = require("../routes/designation.routes");
+const employeeRoutes = require("../routes/employee.routes");
 
+const router = express.Router();
 
-    // Routes Middleware
-    app.use("/api/wos/v2/auth", limiter, authRoutes);
-    app.use("/api/wos/v2/users", limiter, userRoutes);
-    app.use("/api/wos/v2/work-orders", limiter, workOrderRoutes);
-    app.use("/api/wos/v2/locations", limiter, locationRoutes);
-    app.use("/api/wos/v2/reports", limiter, reportsRoutes);
-    app.use("/api/wos/v2/categories", limiter, categoryRoutes);
-    app.use("/api/wos/v2/departments", limiter, departmentRoutes);
-    app.use("/api/wos/v2/designations", limiter, designationRoutes);
-    app.use("/api/wos/v2/employees", limiter, employeeRoutes);
-};
+// Routes Middleware
+router.use("/api/wos/v2/auth", limiter, authRoutes);
+router.use("/api/wos/v2/users", limiter, userRoutes);
+router.use("/api/wos/v2/work-orders", limiter, workOrderRoutes);
+router.use("/api/wos/v2/locations", limiter, locationRoutes);
+router.use("/api/wos/v2/reports", limiter, reportsRoutes);
+router.use("/api/wos/v2/categories", limiter, categoryRoutes);
+router.use("/api/wos/v2/departments", limiter, departmentRoutes);
+router.use("/api/wos/v2/designations", limiter, designationRoutes);
+router.use("/api/wos/v2/employees", limiter, employeeRoutes);
+
+module.exports = router;
