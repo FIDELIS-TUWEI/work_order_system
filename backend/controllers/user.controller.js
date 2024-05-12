730e22ee8dc52e29c2544bb908dd2245c3c57d07
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-12 14:53:22 +0300

    update: user controllers wrapped in try catch block with error being logged

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,30 +2,34 @@ const User = require("../model/user.model");
 const Work = require("../model/work.order.model");
 const sendEmail = require("../utils/email");
 const asyncHandler = require("express-async-handler");
-const CustomError = require("../utils/CustomError");
 const logger = require("../utils/logger");
 
 // Controller function to get all users
 const getAllUsers = asyncHandler (async (req, res, next) => {
-    // Enable Pagination
-    const pageSize = 10;
-    const page = Number(req.query.pageNumber) || 1;
-    const count = await User.find({}).estimatedDocumentCount();
-
-    // Find users in DB
-    const users = await User.find({}).select("-password")
-        .sort({ Date_Created: -1 })
-        .skip(pageSize * (page -1))
-        .limit(pageSize);
+    try {
+        // Enable Pagination
+        const pageSize = 10;
+        const page = Number(req.query.pageNumber) || 1;
+        const count = await User.find({}).estimatedDocumentCount();
     
-    if (!users) return res.status(404).json({ error: "User not found!" });
-
-    res.status(200).json({
-        data: users,
-        page,
-        pages: Math.ceil(count / pageSize),
-        count
-    });
+        // Find users in DB
+        const users = await User.find({}).select("-password")
+            .sort({ Date_Created: -1 })
+            .skip(pageSize * (page -1))
+            .limit(pageSize);
+        
+        if (!users) return res.status(404).json({ error: "User not found!" });
+    
+        res.status(200).json({
+            data: users,
+            page,
+            pages: Math.ceil(count / pageSize),
+            count
+        });
+    } catch (error) {
+        logger.error("Error in getAllUsers controller", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
 });
 
 const getUser = asyncHandler (async (req, res) => {
@@ -84,70 +88,79 @@ const updateUser = asyncHandler (async (req, res) => {
 
 // Controller function to Delete User
 const deleteUser = asyncHandler (async (req, res, next) => {
-    const userId = req.params.id;
-    const user = await User.findByIdAndRemove(userId);
+   try {
+        const userId = req.params.id;
+        const user = await User.findByIdAndRemove(userId);
 
-    if (!user) return res.status(404).json({ errror: `User with ID: ${userId} not found!` })
+        if (!user) return res.status(404).json({ errror: `User with ID: ${userId} not found!` })
 
-    // Check for work requested
-    const workRequested = await Work.find({ requestedBy: userId });
+        // Check for work requested
+        const workRequested = await Work.find({ requestedBy: userId });
 
-    if (workRequested.length > 0) {
-        // Remove associated work orders
-        await Work.deleteMany({ requestedBy: userId });
-    }
+        if (workRequested.length > 0) {
+            // Remove associated work orders
+            await Work.deleteMany({ requestedBy: userId });
+        }
 
-    // Send email notification
-    const recepients = ["fideliofidel9@gmail.com"]
-    const ccEmails = ["fidel.tuwei@holidayinnnairobi.com"];
+        // Send email notification
+        const recepients = ["fideliofidel9@gmail.com"]
+        const ccEmails = ["fidel.tuwei@holidayinnnairobi.com"];
 
-    const emailSubject = `User deleted successfully`;
-    const emailText = `A user with username ${user.username} has been deleted.`;
+        const emailSubject = `User deleted successfully`;
+        const emailText = `A user with username ${user.username} has been deleted.`;
 
-    const emailOptions = {
-        email: recepients,
-        cc: ccEmails,
-        subject: emailSubject,
-        text: emailText
-    };
+        const emailOptions = {
+            email: recepients,
+            cc: ccEmails,
+            subject: emailSubject,
+            text: emailText
+        };
 
-    // Send Email
-    sendEmail(emailOptions);
+        // Send Email
+        sendEmail(emailOptions);
 
-    res.status(200).json({
-        success: true,
-        message: `User with username ${user.username} deleted`
-    });
+        res.status(200).json({
+            success: true,
+            message: `User with username ${user.username} deleted`
+        });
+   } catch (error) {
+        logger.error("Error in deleteUser controller", error);
+        res.status(500).json({ error: "Internal Server Error" });
+   };
 });
 
 // Controller function to count all users
 const countAllUsers = asyncHandler (async (req, res, next) => {
-    const totalUsers = await User.countDocuments();
-
-    if (!totalUsers) {
-        const error = new CustomError("No data for Total users count!", 404)
-        return next(error)
-    }
+    try {
+        const totalUsers = await User.countDocuments();
     
-    res.status(200).json({ 
-        success: true,
-        data: totalUsers 
-    });
+        if (!totalUsers) return res.status(404).json({ error: "No data count for all users found" });
+        
+        res.status(200).json({ 
+            success: true,
+            data: totalUsers 
+        });
+    } catch (error) {
+        logger.error("Error in countAllUsers controller", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
 });
 
 // Controller function to count all users active
 const countActiveUsers = asyncHandler (async (req, res, next) => {
-    const activeUsersCount = await User.countDocuments({ active: true });
-
-    if (!activeUsersCount) {
-        const error = new CustomError("No data for active users count!", 404);
-        return next(error);
-    }
+    try {
+        const activeUsersCount = await User.countDocuments({ active: true });
     
-    res.status(200).json({ 
-        success: true,
-        data: activeUsersCount 
-    });
+        if (!activeUsersCount) return res.status(404).json({ error: "No data count for active users found!" })
+        
+        res.status(200).json({ 
+            success: true,
+            data: activeUsersCount 
+        });
+    } catch (error) {
+        logger.error("Error in countActiveUsers controller", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
 });
 
 
