cd0cebe2e24cba79f6e027756ddd04e6b5511c59
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-12 08:54:45 +0300

    update: login controller logic modified

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,8 @@
-const User = require("../model/user.model");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
+
+const User = require("../model/user.model");
 const sendEmail = require("../utils/email");
-const createSecretToken = require("../utils/generateToken");
-const CustomError = require("../utils/CustomError");
 const asyncErrorHandler = require("../utils/asyncErrorHandler");
 const logger = require("../utils/logger");
 const generateTokenAndSetCookie = require("../utils/generateToken");
@@ -92,7 +91,7 @@ try {
     const user = await User.findOne({ username });
 
     // Check if password is correct with the one in DB
-    isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
     if (!user || !isPasswordCorrect) {
         return res.status(400).json({ error: "Invalid username or password" });
     };
