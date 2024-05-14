12a3519b1c7726032428dbf697fc4a326335c329
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-14 17:44:55 +0300

    work order routes modified/ renamed

--- backend/routes/work.order.routes.js
+++ backend/routes/work.order.routes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { createWorkOrder, updateWorkOrder, getAllWorkOrders, getSingleWorkOrder, deleteWorkOrder, queryAllWork, inAttendanceTracker, inCompleteTracker, attendedTracker } = require('../controllers/work.order.controller');
+const { createWorkOrder, updateWorkOrder, getWorkOrders, getWorkOrder, deleteWorkOrder, displayWork, inAttendanceTracker, inCompleteTracker, attendedTracker } = require('../controllers/work.order.controller');
 const { protect, restrict, cacheMiddleware, setVerifiedBy} = require('../middleware/authMiddleware');
 
 
 router.post("/create/work", protect, restrict(["admin", "hod", "user", "supervisor", "superadmin", "reviewer", "engineer", "maintenance"]), createWorkOrder);
-router.get("/getall/work", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer", "maintenance"]), cacheMiddleware, getAllWorkOrders);
-router.get("/query/all/work", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer", "maintenance"]), cacheMiddleware, queryAllWork);
-router.get("/single/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "hod", "reviewer", "engineer", "maintenance"]), cacheMiddleware, getSingleWorkOrder);
+router.get("/work-orders", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer", "maintenance"]), cacheMiddleware, getWorkOrders);
+router.get("/graph", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer", "maintenance"]), cacheMiddleware, displayWork);
+router.get("/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "hod", "reviewer", "engineer", "maintenance"]), cacheMiddleware, getWorkOrder);
 router.get("/work-orders/in-attendance", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer", "maintenance"]), cacheMiddleware, inAttendanceTracker);
 router.get("/work-orders/in-complete", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer", "maintenance"]), cacheMiddleware, inCompleteTracker);
 router.get("/work-orders/attended", protect, restrict(["admin", "hod", "supervisor", "superadmin", "reviewer", "engineer", "maintenance"]), cacheMiddleware, attendedTracker);
 router.put("/update/work/:id", protect, restrict(["admin", "supervisor", "superadmin", "reviewer", "engineer", "hod", "maintenance"]), setVerifiedBy, updateWorkOrder);
-router.delete("/delete/work/:id", protect, restrict(["admin", "superadmin"]), deleteWorkOrder);
+router.delete("/work/:id", protect, restrict(["admin", "superadmin"]), deleteWorkOrder);
 
 module.exports = router;
