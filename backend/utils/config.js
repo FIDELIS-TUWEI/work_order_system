3054a4ed85a2099f0018cc24e21210386026591f
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-15 15:27:33 +0300

    add: email username/password env config added

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,7 +4,9 @@ let PORT = process.env.PORT;
 let MONGODB_URI = process.env.MONGODB_URI;
 let JWT_SECRET = process.env.JWT_SECRET;
 let NODE_ENV = process.env.NODE_ENV;
+let EMAIL = process.env.EMAIL;
+let PASSWORD = process.env.PASSWORD;
 
 module.exports = {
-    PORT, MONGODB_URI, JWT_SECRET, NODE_ENV
+    PORT, MONGODB_URI, JWT_SECRET, NODE_ENV, EMAIL, PASSWORD
 };
