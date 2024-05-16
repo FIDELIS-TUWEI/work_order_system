15c7abcabe5acf46e4bccb4b7bb32101beca64f8
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-16 11:48:17 +0300

    new work ejs email  logic

---
+++ backend/EmailService/newWork.js
@@ -0,0 +1,27 @@
+const ejs = require("ejs");
+const config = require("../utils/config");
+const sendEmail = require("../helpers/sendEmail");
+const logger = require("../utils/logger");
+
+const SendNewWorkEmail = async (workOrderNumber, description, priority, status, serviceType, username) => {
+    ejs.renderFile(
+        "templates/new.work.ejs",
+        { workOrderNumber, description, priority, status, serviceType, username },
+        async (err, data) => {
+            let messageOption = {
+                from: config.EMAIL,
+                to: "fideliofidel9@gmail.com",
+                subject: "A new work order request has been created",
+                html: data,
+            };
+
+            try {
+                await sendEmail(messageOption);
+            } catch (error) {
+                logger.error(err)
+            }
+        }
+    );
+};
+
+module.exports = { SendNewWorkEmail };
