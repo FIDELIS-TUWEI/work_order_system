cbcca8f3419af337546c188c6674c993447cba2c
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-16 11:47:40 +0300

    assigned work logic modifedwork order controller modified to import email template logic

---
+++ backend/EmailService/assignedWork.js
@@ -0,0 +1,27 @@
+const ejs = require("ejs");
+const config = require("../utils/config");
+const sendEmail = require("../helpers/sendEmail");
+const logger = require("../utils/logger");
+
+const SendAssignedWorkEmail = async (workOrderNumber, description, priority, status, serviceType, username, dateAssigned) => {
+    ejs.renderFile(
+        "../templates/assigned.work.ejs",
+        { workOrderNumber, description, priority, status, serviceType, username, dateAssigned },
+        async (err, data) => {
+            let messageOption = {
+                from: config.EMAIL,
+                to: "fideliofidel9@gmail.com",
+                subject: "Your work order request has been assigned.",
+                html: data,
+            };
+
+            try {
+                await sendEmail(messageOption);
+            } catch (error) {
+                logger.error(err);
+            }
+        }
+    );
+};
+
+module.exports = { SendAssignedWorkEmail }
