58ba0d755efc20c9b252fd48dfb320d9f96f2ac6
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-17 18:53:38 +0300

    Update: logic to send newly created user email working

---
+++ backend/EmailService/welcomeUser.js
@@ -0,0 +1,29 @@
+const ejs = require("ejs");
+const path = require("path");
+const config = require("../utils/config");
+const sendEmail = require("../helpers/sendEmail");
+const logger = require("../utils/logger");
+
+const SendWelcomeUserEmail = async ({firstName, lastName, username}) => {
+    const templatePath = path.join(__dirname, "../templates/welcome.ejs")
+    ejs.renderFile(
+        templatePath,
+        {firstName, lastName, username},
+        async (err, data) => {
+            let messageOption = {
+                from: config.EMAIL,
+                to: "fideliofidel9@gmail.com",
+                subject: "A user has been registered!",
+                html: data,
+            };
+
+            try {
+                await sendEmail(messageOption);
+            } catch (error) {
+                logger.error(err)
+            }
+        }
+    )
+};
+
+module.exports = { SendWelcomeUserEmail };
