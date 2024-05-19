2c26ca0309e98336eb3726119ac76b114c13cfd9
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-19 09:57:00 +0300

    new work email ejs logic modified

--- backend/EmailService/newWork.js
+++ backend/EmailService/newWork.js
@@ -1,6 +1,5 @@
 const ejs = require("ejs");
 const path = require("path");
-const config = require("../utils/config");
 const sendEmail = require("../helpers/sendEmail");
 const logger = require("../utils/logger");
 
